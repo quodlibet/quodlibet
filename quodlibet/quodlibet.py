8ce518f3e3064b6cc17cc155b1021a98b5772941
Joe Wreschnig <joe.wreschnig@gmail.com>
2004-11-08 22:24:22 +0000

    doc corrections

--- quodlibet/quodlibet.py
+++ quodlibet/quodlibet.py
@@ -4,7 +4,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation
+# published by the Free Software Foundation.
 #
 # $Id$
 
