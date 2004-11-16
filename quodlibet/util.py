9d7f9fb3a7d02690b31a6d187ae1a88eda453ec0
Joe Wreschnig <joe.wreschnig@gmail.com>
2004-11-16 05:43:49 +0000

    better disc/track regexen

--- quodlibet/util.py
+++ quodlibet/util.py
@@ -175,7 +175,7 @@ class PatternFromFile(object):
         self.pattern = None
         # patterns look like <tagname> non regexy stuff <tagname> ...
         pieces = sre.split(r'(<[A-Za-z0-9_]+>)', pattern)
-        override = { '<tracknumber>': r'\d+', '<discnumber>': r'\d+' }
+        override = { '<tracknumber>': r'\d\d?', '<discnumber>': r'\d' }
         for i, piece in enumerate(pieces):
             if not piece: continue
             if piece[0]+piece[-1] == '<>' and piece[1:-1].isalnum():
