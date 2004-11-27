0e1f24b337d8804703a0d6cd6cccba3effe7c9af
Joe Wreschnig <joe.wreschnig@gmail.com>
2004-11-27 02:47:09 +0000

    flesh out distutils

--- quodlibet/modplug/setup.py
+++ quodlibet/modplug/setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-setup(name = "modplug", version = "0.1",
+setup(name = "modplug", version = "1.0",
+      url="http://sacredchao.net/~piman/software/python.shtml",
       description = "ModPlug decoder",
       author = "Joe Wreschnig",
       author_email = "piman@sacredchao.net",
+      license = "GNU GPL v2",
+      long_description = """
+This Python module lets you load and decode files supported by
+the ModPlug library (which includes MODs, ITs, XMs, and so on).
+Its API has been chosen to mostly match pyvorbis and pymad.""",
       ext_modules=[
          Extension("modplug",
                    ["modplug.c"],
