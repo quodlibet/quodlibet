5057ddfbf287b97d225a9a7d66300f39b5cab5dd
Joe Wreschnig <joe.wreschnig@gmail.com>
2004-12-17 23:47:11 +0000

    these branches are dead

--- quodlibet-moreglade/const.py
+++
@@ -1,8 +0,0 @@
-import os
-
-HOME    = os.path.expanduser("~")
-DIR     = os.path.join(HOME, ".quodlibet")
-CONTROL = os.path.join(DIR,  "control")
-CONFIG  = os.path.join(DIR,  "config")
-CURRENT = os.path.join(DIR,  "current")
-LIBRARY = os.path.join(DIR,  "songs")
