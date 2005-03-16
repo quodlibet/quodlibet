fdbb0abeba7365ba6e72af792cc0411bcc545a3a
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-03-16 05:30:30 +0000

    test BCI for object leaks (there are none :( )

--- quodlibet/tests/test_leaks.py
+++ quodlibet/tests/test_leaks.py
@@ -5,15 +5,26 @@ import os, gtk, widgets, gc, time
 class TestWidgetLeaks(TestCase):
     def test_AboutWindow(self):
          dummy = gtk.Window()
-         widgets.AboutWindow(dummy).destroy()
+         self.failIfLeaky(widgets.AboutWindow, dummy)
+         dummy.destroy()
+
+    def test_BCI(self):
+         self.failIfLeaky(
+             widgets.BigCenteredImage, "woo",
+             "/home/piman/audio/Ani DiFranco - Dilate/cover.jpg")
+
+    def failIfLeaky(self, Ctr, *args):
+         gc.collect()
+         c0 = len(gc.get_objects())
+         Ctr(*args).destroy()
          while gtk.events_pending(): gtk.main_iteration()
          gc.collect()
          c1 = len(gc.get_objects())
-         widgets.AboutWindow(dummy).destroy()
+         Ctr(*args).destroy()
          while gtk.events_pending(): gtk.main_iteration()
          gc.collect()
          c2 = len(gc.get_objects())
+         self.failUnlessEqual(c0, c1)
          self.failUnlessEqual(c1, c2)
-         dummy.destroy()
 
 registerCase(TestWidgetLeaks)
