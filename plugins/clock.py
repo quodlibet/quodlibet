0943da17d020550dca06ff98457ca6d291642b91
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-04-24 19:56:30 +0000

    Alarm plugin.

--- plugins/clock.py
+++ plugins/clock.py
@@ -10,52 +10,88 @@ import time
 import gobject, gtk
 import player
 
-class Lullaby(object):
-    PLUGIN_NAME = "Lullaby"
-    PLUGIN_DESC = "Fade out your music while you go to sleep."
-
-    def __init__(self):
-        self.__starttime = -1
-        self.__enabled = False
-
-    def plugin_on_song_started(self, song):
-        if self.__enabled:
-            time_ = time.localtime()[3] * 60 + time.localtime()[4]
-            if self.__starttime > 0 and self.__starttime < time_:
-                self.__enabled = False
-                gobject.timeout_add(100, self.__fadeout, player.device.volume)
-
-    def __fadeout(self, volume):
-        player.device.volume = max(player.device.volume - 0.002, 0)
-        if player.device.volume == 0:
-            player.playlist.paused = True
-            player.device.volume = volume
-        else: return True
-
+class ClockPlug(object):
     def __clicked(self, cb, hb):
-        self.__enabled = cb.get_active()
-        hb.set_sensitive(self.__enabled)
+        self._enabled = cb.get_active()
+        hb.set_sensitive(self._enabled)
 
     def __set_time(self, entry):
         t = entry.get_text()
         try: hour, minute = map(int, t.split(":"))
-        except: self.__starttime = -1
-        else: self.__starttime = hour * 60 + minute
+        except: self._starttime = -1
+        else: self._starttime = hour * 60 + minute
 
     def PluginPreferences(self, parent):
         hb = gtk.HBox(spacing=6)
         hb.set_border_width(6)
-        cb = gtk.CheckButton("Fade out at:")
         e = gtk.Entry()
-        if self.__starttime == -1: e.set_text("Enter a time in HH:MM format.")
+        if self._starttime == -1: e.set_text("Enter a time in HH:MM format.")
         else: e.set_text("%d:%02d" % (
-            self.__starttime / 60, self.__starttime % 60))
+            self._starttime / 60, self._starttime % 60))
         e.connect('changed', self.__set_time)
-        cb = gtk.CheckButton("Fade out at:")
-        cb.set_active(self.__enabled)
-        e.set_sensitive(self.__enabled)
+        cb = gtk.CheckButton(self.message)
+        cb.set_active(self._enabled)
+        e.set_sensitive(self._enabled)
         cb.connect('clicked', self.__clicked, e)
         hb.pack_start(cb, expand=False)
         hb.pack_start(e, expand=True)
         hb.show_all()
         return hb
+
+class Alarm(ClockPlug):
+    PLUGIN_NAME = "Alarm"
+    PLUGIN_DESC = "Wake you up with loud music."
+
+    message = "Wake up at:"
+
+    def __init__(self):
+        self._starttime = -1
+        self._enabled = False
+        self.__gid = 0
+
+    def __start_playing(self):
+        gtk.threads_enter()
+        if player.playlist.info.song is None:
+            player.playlist.reset()
+        player.playlist.paused = False
+        gtk.threads_leave()
+
+    def __poll(self):
+        if self._enabled and self._starttime > 0:
+            time_ = time.localtime()[3] * 60 + time.localtime()[4]
+            if self._starttime < time_:
+                self._enabled = False
+                gobject.idle_add(self.__start_playing)
+        return True
+
+    def plugin_on_paused(self):
+        self.__gid = gobject.timeout_add(1000, self.__poll)
+
+    def plugin_on_song_started(self, song):
+        if song is None:
+            self.__gid = gobject.timeout_add(1000, self.__poll)
+        elif self.__gid:
+            gobject.source_remove(self.__gid)
+            self.__gid = 0
+
+    def plugin_on_unpaused(self):
+        if self.__gid:
+            gobject.source_remove(self.__gid)
+            self.__gid = 0
+
+class Lullaby(ClockPlug):
+    PLUGIN_NAME = "Lullaby"
+    PLUGIN_DESC = "Turn off your music after you go to sleep."
+
+    message = "Turn off at:"
+
+    def __init__(self):
+        self._starttime = -1
+        self._enabled = False
+
+    def plugin_on_song_ended(self, song, stopped):
+        if self._enabled and not stopped:
+            time_ = time.localtime()[3] * 60 + time.localtime()[4]
+            if self._starttime > 0 and self._starttime < time_:
+                self._enabled = False
+                player.playlist.paused = True
