f6b27247bd0ea5577a34e675756a709bc0488d5c
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-04-26 22:58:17 +0000

    Alarm:  * Only fire if we're within 2 minutes of the target time.  * Reduce poll interval to 10 seconds. Lullaby: Only fire if we're within 30 minutes of the target time (any song over 30 minutes long will mess this up).

--- plugins/clock.py
+++ plugins/clock.py
@@ -61,17 +61,17 @@ class Alarm(ClockPlug):
     def __poll(self):
         if self._enabled and self._starttime > 0:
             time_ = time.localtime()[3] * 60 + time.localtime()[4]
-            if self._starttime < time_:
+            if self._starttime < time_ and (time_ - self._starttime) < 2:
                 self._enabled = False
                 gobject.idle_add(self.__start_playing)
         return True
 
     def plugin_on_paused(self):
-        self.__gid = gobject.timeout_add(1000, self.__poll)
+        self.__gid = gobject.timeout_add(10000, self.__poll)
 
     def plugin_on_song_started(self, song):
         if song is None:
-            self.__gid = gobject.timeout_add(1000, self.__poll)
+            self.__gid = gobject.timeout_add(10000, self.__poll)
         elif self.__gid:
             gobject.source_remove(self.__gid)
             self.__gid = 0
@@ -96,6 +96,7 @@ class Lullaby(ClockPlug):
     def plugin_on_song_ended(self, song, stopped):
         if self._enabled and not stopped:
             time_ = time.localtime()[3] * 60 + time.localtime()[4]
-            if self._starttime > 0 and self._starttime < time_:
+            if (self._starttime > 0 and self._starttime < time_ and
+                (time_ - self._starttime < 30)):
                 self._enabled = False
                 player.playlist.paused = True
