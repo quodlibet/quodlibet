e69752095574a528e2db77d49697abf6836ce878
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-06-15 17:42:55 +0000

    VorbisProperties wrappers. Tag wrappers (broken, segfaults).

--- python-boost-taglib/taglib.cpp
+++ python-boost-taglib/taglib.cpp
@@ -1,3 +1,5 @@
+#include <Python.h>
+
 #include <boost/python.hpp>
 #include <taglib/taglib.h>
 #include <taglib/fileref.h>
@@ -8,47 +10,100 @@
 #include <taglib/vorbisproperties.h>
 
 using namespace boost::python;
+using namespace TagLib;
+
+struct StringToUnicode {
+    static PyObject* convert(String const& s) {
+      return Py_BuildValue("");
+    }
+};
 
 BOOST_PYTHON_MODULE(taglib) {
-  class_<TagLib::FileRef>("FileRef", init<const char *>())
-    .def("isNull", &TagLib::FileRef::isNull)
-    .def("audioProperties", &TagLib::File::audioProperties,
+  // Enumerations //
+  enum_<AudioProperties::ReadStyle>("ReadStyle")
+    .value("FAST", AudioProperties::Fast)
+    .value("AVERAGE", AudioProperties::Average)
+    .value("ACCURATE", AudioProperties::Accurate)
+    ;
+
+  // Utility classes //
+  class_<ByteVector>("ByteVector", no_init);
+
+  to_python_converter<String, StringToUnicode>();
+
+  class_<FileRef>("FileRef", init<const char *>())
+    .def("save", &FileRef::save)
+    .def("audioProperties", &FileRef::audioProperties,
+	 return_value_policy<reference_existing_object>())
+    .def("tag", &FileRef::tag,
+	 return_value_policy<reference_existing_object>())
+    .def("file", &FileRef::file,
 	 return_value_policy<reference_existing_object>())
     ;
 
-  class_<TagLib::ByteVector>("ByteVector", no_init);
+  // Base abstract classes //
 
-  class_<TagLib::AudioProperties, boost::noncopyable>
+  class_<File, boost::noncopyable>("File", no_init)
+    .add_property("name", &File::name)
+    .add_property("size", &File::length)
+    .def("read", &File::readBlock)
+    .def("seek", &File::seek)
+    .def("tell", &File::tell)
+    .def("save", &File::save)
+    .def("tag", &File::tag,
+	 return_value_policy<reference_existing_object>())
+    .def("audioProperties", &File::audioProperties,
+	 return_value_policy<reference_existing_object>())
+    ;
+
+  class_<AudioProperties, boost::noncopyable>
     ("AudioProperties", no_init)
-    .add_property("length", &TagLib::AudioProperties::length)
-    .add_property("bitrate", &TagLib::AudioProperties::bitrate)
-    .add_property("sampleRate", &TagLib::AudioProperties::sampleRate)
-    .add_property("channels", &TagLib::AudioProperties::channels)
+    .add_property("length", &AudioProperties::length)
+    .add_property("bitrate", &AudioProperties::bitrate)
+    .add_property("sampleRate", &AudioProperties::sampleRate)
+    .add_property("channels", &AudioProperties::channels)
     ;
 
-  class_<TagLib::File, boost::noncopyable>("File", no_init)
-    .add_property("name", &TagLib::File::name)
-    .add_property("size", &TagLib::File::length)
-    .def("read", &TagLib::File::readBlock)
-    .def("seek", &TagLib::File::seek)
-    .def("tell", &TagLib::File::tell)
-    .def("audioProperties", &TagLib::File::audioProperties,
-	 return_value_policy<reference_existing_object>())
+  class_<Tag, boost::noncopyable> ("Tag", no_init)
+    .add_property("title", &Tag::title, &Tag::setTitle)
+    .add_property("artist", &Tag::artist, &Tag::setArtist)
+    .add_property("album", &Tag::album, &Tag::setAlbum)
+    .add_property("genre", &Tag::genre, &Tag::setGenre)
+    .add_property("comment", &Tag::comment, &Tag::setComment)
+    .add_property("year", &Tag::year, &Tag::setYear)
+    .add_property("track", &Tag::track, &Tag::setTrack)
     ;
 
-  class_<TagLib::Ogg::File, boost::noncopyable, bases<TagLib::File> >
+  // Ogg //
+
+  class_<Ogg::File, boost::noncopyable, bases<File> >
     ("OggFile", no_init)
-    .def("__getitem__", &TagLib::Ogg::File::packet)
-    .def("__setitem__", &TagLib::Ogg::File::setPacket)
+    .def("__getitem__", &Ogg::File::packet)
+    .def("__setitem__", &Ogg::File::setPacket)
     ;
 
-  class_<TagLib::Ogg::Vorbis::File, boost::noncopyable,
-    bases<TagLib::Ogg::File> >("VorbisFile", init<const char *>())
+  // Ogg Vorbis //
+
+  class_<Ogg::Vorbis::File, boost::noncopyable,
+    bases<Ogg::File> >("VorbisFile", init<const char *>())
     ;
-  
-  class_<TagLib::Ogg::Vorbis::AudioProperties, boost::noncopyable,
-    bases<TagLib::AudioProperties> >
-    ("VorbisProperties", no_init)
+
+  class_<Vorbis::Properties, boost::noncopyable,
+    bases<AudioProperties> >
+    ("VorbisProperties",
+     init<Vorbis::File *, optional<AudioProperties::ReadStyle> >())
+    .add_property("vorbisVersion",
+		  &Vorbis::Properties::vorbisVersion)
+    .add_property("bitrateMaximum",
+		  &Vorbis::Properties::bitrateMaximum)
+    .add_property("bitrateMinimum",
+		  &Vorbis::Properties::bitrateMinimum)
+    .add_property("bitrateNominal",
+		  &Vorbis::Properties::bitrateNominal)
     ;
-  
+
+  class_<Ogg::XiphComment, boost::noncopyable, bases<Tag> >
+    ("XiphComment", init<>())
+    ;
+
 }
