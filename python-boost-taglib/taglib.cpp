7b5d50ebbc3a8867e378ac3fd7297249acf838b5
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-06-18 07:36:23 +0000

    String::Type, StringList, ID3v2::Frame, ID3v2::TextIdentificationFrame: Wrap. FileRef, File: Clarify memory ownership with return_internal_reference.

--- python-boost-taglib/taglib.cpp
+++ python-boost-taglib/taglib.cpp
@@ -11,10 +11,13 @@
 #include <taglib/mpegfile.h>
 #include <taglib/mpegproperties.h>
 
+#include <taglib/textidentificationframe.h>
+#include <taglib/id3v2framefactory.h>
+
 using namespace boost::python;
 using namespace TagLib;
 
-struct StringUnicodeConv {
+struct StringToUnicode {
   static PyObject *convert(String const &s) {
     const char *ustr = s.toCString();
     return PyUnicode_DecodeUTF8(ustr, strlen(ustr), "strict");
@@ -24,6 +27,15 @@ struct StringUnicodeConv {
 struct ByteVectorToStr {
   static PyObject* convert(ByteVector const &b) {
     return PyString_FromStringAndSize(b.data(), b.size());
+  }
+};
+
+struct StringListToUnicodeList {
+  static PyObject *convert(StringList const &sl) {
+    PyObject *pl = PyList_New((int)sl.size());
+    for (unsigned int i = 0; i < sl.size(); i++)
+      PyList_SetItem(pl, i, StringToUnicode::convert(sl[i]));
+    return pl;
   }
 };
 
@@ -46,7 +58,7 @@ struct StringFromStr {
     if (value == 0) throw_error_already_set();
     void* storage = ((converter::rvalue_from_python_storage<String> *)
 		     data)->storage.bytes;
-    new (storage) String(value);
+    new (storage) String(value, String::UTF8);
     data->convertible = storage;
   }
 };
@@ -68,6 +80,43 @@ struct ByteVectorFromStr {
 		     data)->storage.bytes;
     new (storage) ByteVector(PyString_AsString(o), PyString_Size(o));
     data->convertible = storage;
+  }
+};
+
+struct StringListFromStrList {
+  StringListFromStrList() {
+    converter::registry::push_back(&convertible, &construct,
+				   type_id<StringList>());
+  }
+
+  static void *convertible(PyObject* o) {
+    if (PyList_Check(o)) {
+      int size = PyList_Size(o);
+      for (int i = 0; i < size; i++) {
+	PyObject *s = PyList_GetItem(o, i);
+	if (!(PyString_Check(s) || PyUnicode_Check(s)))
+	  return 0;
+      }
+      return o;
+    } else return 0;
+  }
+
+  static void construct(PyObject* o,
+			converter::rvalue_from_python_stage1_data *data) {
+    void* storage = ((converter::rvalue_from_python_storage<StringList> *)
+		     data)->storage.bytes;
+    int size = PyList_Size(o);
+    new (storage) StringList;
+    data->convertible = storage;
+    StringList *l = (StringList *)(data->convertible);
+    for (int i = 0; i < size; i++) {
+      PyObject *s = PyList_GetItem(o, i);
+      if (PyUnicode_Check(s))
+	s = PyUnicode_AsUTF8String(s);
+      const char *value = PyString_AsString(s);
+      if (value == 0) throw_error_already_set();
+      l->append(String(value, String::UTF8));
+    }
   }
 };
 
@@ -99,20 +148,29 @@ BOOST_PYTHON_MODULE(taglib) {
     .value("ALLTAGS", MPEG::File::AllTags)
     ;
 
+  enum_<String::Type>("Encoding")
+    .value("LATIN1", String::Latin1)
+    .value("UTF16", String::UTF16)
+    .value("UTF16BE", String::UTF16BE)
+    .value("UTF8", String::UTF8)
+    ;
+
   // Utility classes <-> Python types //
   to_python_converter<ByteVector, ByteVectorToStr>();
-  to_python_converter<String, StringUnicodeConv>();
+  to_python_converter<String, StringToUnicode>();
+  to_python_converter<StringList, StringListToUnicodeList>();
   ByteVectorFromStr();
   StringFromStr();
+  StringListFromStrList();
 
   class_<FileRef>("FileRef", init<const char *>())
     .def("save", &FileRef::save)
     .def("audioProperties", &FileRef::audioProperties,
-	 return_value_policy<reference_existing_object>())
+	 return_internal_reference<1>())
     .def("tag", &FileRef::tag,
-	 return_value_policy<reference_existing_object>())
+	 return_internal_reference<1>())
     .def("file", &FileRef::file,
-	 return_value_policy<reference_existing_object>())
+	 return_internal_reference<1>())
     .def("__nonzero__", &FileRef_nonzero)
     ;
 
@@ -125,10 +183,9 @@ BOOST_PYTHON_MODULE(taglib) {
     .def("seek", &File::seek)
     .def("tell", &File::tell)
     .def("save", &File::save)
-    .def("tag", &File::tag,
-	 return_value_policy<reference_existing_object>())
+    .def("tag", &File::tag, return_internal_reference<1>())
     .def("audioProperties", &File::audioProperties,
-	 return_value_policy<reference_existing_object>())
+	 return_internal_reference<1>())
     .add_property("closed", &File_isClosed)
     ;
 
@@ -192,5 +249,28 @@ BOOST_PYTHON_MODULE(taglib) {
     ("MPEGProperties",
      init<MPEG::File *, optional<AudioProperties::ReadStyle> >())
     .add_property("layer", &MPEG::Properties::layer)
+    ;
+
+  // ID3v2 //
+
+  class_<ID3v2::Frame, boost::noncopyable>("_ID3v2Frame", no_init)
+    .add_property("frameID", &ID3v2::Frame::frameID)
+    .add_property("size", &ID3v2::Frame::size)
+    .add_property("text", &ID3v2::Frame::toString, &ID3v2::Frame::setText)
+    .add_property("data", &ID3v2::Frame::render, &ID3v2::Frame::setData)
+    .def("__unicode__", &ID3v2::Frame::toString)
+    ;
+ 
+  void (ID3v2::TextIdentificationFrame::*setTextList)(const StringList &) =
+    &ID3v2::TextIdentificationFrame::setText;
+
+  class_<ID3v2::TextIdentificationFrame, boost::noncopyable,
+    bases<ID3v2::Frame> >
+    ("ID3v2TextFrame", init<ByteVector, String::Type>())
+    .add_property("encoding", &ID3v2::TextIdentificationFrame::textEncoding,
+    &ID3v2::TextIdentificationFrame::setTextEncoding)
+    .add_property("fields", &ID3v2::TextIdentificationFrame::fieldList,
+		  setTextList)
+
     ;
 }
