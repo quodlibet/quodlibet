cec224b5962e899f69ce79fd71addd7a0a232ea2
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-08-06 01:42:19 +0000

    PluginManage#invoke_event: Don't check change/refresh when the signal is 'changed'.

--- quodlibet/plugins.py
+++ quodlibet/plugins.py
@@ -355,7 +355,7 @@ class PluginManager(object):
                         try: handler(*args)
                         except Exception: print_exc()
         finally:
-            if event not in ["removed", "missing"] and args:
+            if event not in ["removed", "missing", "changed"] and args:
                 if isinstance(args[0], list):
                     self.check_change_and_refresh(args[0])
                 else:
