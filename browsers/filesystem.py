86f235bc64b6dd34b458ee3618d961ed0fea3a9f
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-08-19 02:16:18 +0000

    Filesystem:  * Keep a local library until songs are added.  * Implement play-save (a.k.a. crappy) part of Browser API.

--- browsers/filesystem.py
+++ browsers/filesystem.py
@@ -15,30 +15,39 @@
 
 import os
 import gobject, gtk
-import const
 import qltk
-import util
-import parser
 import config
+import player
 import formats
 from efwidgets import DirectoryTree
 
 from browsers.base import Browser
-from library import library
+from library import Library, library as glibrary
 
 class Filesystem(Browser, gtk.ScrolledWindow):
     __gsignals__ = Browser.__gsignals__
     expand = qltk.RHPaned
+    __lib = Library()
 
-    def __init__(self):
+    def __init__(self, play=True, save=True):
         gtk.ScrolledWindow.__init__(self)
         self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         self.set_shadow_type(gtk.SHADOW_IN)
         dt = DirectoryTree(initial=os.environ["HOME"])
         sel = dt.get_selection()
         sel.connect('changed', self.__find_songs)
+        if save: dt.connect('row-activated', self.__play)
         self.add(dt)
+        self.__refresh_library()
         self.show_all()
+
+    def __refresh_library(self):
+        for fn, song in self.__lib.items():
+            if fn in glibrary: self.__lib.remove(song)
+            elif not song.valid(): self.__lib.reload(song)
+
+    def __play(self, *args):
+        player.playlist.next()
 
     def restore(self): pass
 
@@ -49,10 +58,12 @@ class Filesystem(Browser, gtk.ScrolledWindow):
         for dir in dirs:
             for file in filter(formats.filter, os.listdir(dir)):
                 fn = os.path.join(dir, file)
-                if fn in library: songs.append(library[fn])
-                else:
+                if fn in glibrary: songs.append(glibrary[fn])
+                elif fn not in self.__lib:
                     song = formats.MusicFile(fn)
-                    if song is not None: songs.append(song)
+                    if song: self.__lib[song["~filename"]] = song
+                if fn in self.__lib: songs.append(self.__lib[fn])
+
         self.emit('songs-selected', songs, None)
 
 gobject.type_register(Filesystem)
