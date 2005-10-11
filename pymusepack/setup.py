967520efd41cd6259abbb4684c98b533fbdf8236
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-10-11 16:02:08 +0000

    Remove reference to mpc.c. (Closes: #283)

--- pymusepack/setup.py
+++ pymusepack/setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-setup(name = "pymusepack", version = "0.3",
-      url = "http://www.sacredchao.net/quodlibet",
-      description = "Musepack decoder and tagger",
-      author = "Joe Wreschnig",
-      author_email = "piman@sacredchao.net",
-      license = "GNU GPL v2",
-      long_description = """
+setup(name="pymusepack", version="1.0",
+      url="http://www.sacredchao.net/quodlibet",
+      description="Musepack decoder and tagger",
+      author="Joe Wreschnig",
+      author_email="piman@sacredchao.net",
+      license="GNU GPL v2",
+      long_description="""
 This Python module lets you load and decode Musepack (MPC/MP+)
 files using libmpcdec. It resembles the Python MAD, Vorbis,
-and ModPlug interfaces. It also lets you read and edit APEv2 tags.""",
-      packages = ["musepack"],
-      ext_modules=[Extension('musepack.mpc', ['musepack/mpc.c'],
-                             libraries = ['mpcdec'])]
+and ModPlug interfaces. It also lets you read and edit APEv2 tags.
+
+It requires python-ctypes.""",
+      packages = ["musepack"]
     )
