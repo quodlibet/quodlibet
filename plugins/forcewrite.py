967758e14a0f825064e17caf6e9879969928c314
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-10-12 03:51:34 +0000

    Add 'Force Write' plugin.

---
+++ plugins/forcewrite.py
@@ -0,0 +1,16 @@
+# Copyright 2005 Joe Wreschnig
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+# $Id$
+
+class ForceWrite(object):
+    PLUGIN_NAME = "Force Write"
+    PLUGIN_DESC = ("Save the files again. This will make sure play counts "
+                   "and ratings are up-to-date.")
+    PLUGIN_ICON = 'gtk-save'
+    PLUGIN_VERSION = "0.13"
+
+    def plugin_song(self, song): song._needs_write = True
