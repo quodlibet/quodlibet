1302b8d4e324b74b939ee1981a0d4f54b2baab57
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-10-26 05:17:07 +0000

    ReplayGainer.

---
+++ plugins/_subprocobj.py
@@ -0,0 +1,161 @@
+#! /usr/bin/env python
+#
+#    <one line to give the program's name and a brief idea of what it does.>
+#    Copyright (C) 2005  Michael Urman
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of version 2 of the GNU General Public License as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+import gtk, gobject, os, sys
+__all__ = []
+
+class Subprocess(gobject.GObject):
+
+    #SIG_NONE = (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ())
+    SIG_INT = (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (int,))
+    SIG_INTSTR = (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (int, str))
+
+    __gsignals__ = {
+        'output-line': SIG_INTSTR,
+        'output-chunk': SIG_INTSTR,
+        'output-eof': SIG_INT,
+        #'terminate': SIG_NONE,
+    }
+
+    __gproperties__ = {
+        'newlines': (str, 'line splitters',
+                'these characters also case a line to be reported',
+                '\n', gobject.PARAM_READWRITE),
+
+        'process': (object, 'process', 'process name and arguments',
+                gobject.PARAM_READABLE),
+
+        'cmdname': (str, 'cmdname', 'command when name is overriden',
+                '', gobject.PARAM_READABLE),
+
+        'stdin' : (int, 'stdin', "child's stdin file descriptor",
+                ~sys.maxint, sys.maxint, -1, gobject.PARAM_READABLE),
+
+        'stdout' : (int, 'stdout', "child's stdout file descriptor",
+                ~sys.maxint, sys.maxint, -1, gobject.PARAM_READABLE),
+
+        'stderr' : (int, 'stderr', "child's stderr file descriptor",
+                ~sys.maxint, sys.maxint, -1, gobject.PARAM_READABLE),
+    }
+
+    def __init__(self, process=[], cmd='', newlines='\n', stdin=None):
+        gobject.GObject.__init__(self)
+        self.newlines = newlines
+        self.process = process
+        self.cmdname = cmd or process[0]
+        self.__buf = {}
+        if stdin is not None:
+            raise NotImplementedError, "Overriding stdin is not implemented"
+
+    def start(self):
+        from popen2 import Popen3
+        self.__child = Popen3(self.process, capturestderr=True)
+
+        #self.__in_id = gobject.io_add_watch(self.stdin,
+        #       gobject.IO_OUT|gobject.IO_ERR|gobject.IO_HUP|gobject.IO_NVAL,
+        #       self.__on_input)
+        self.__out_id = gobject.io_add_watch(self.stdout,
+                gobject.IO_IN|gobject.IO_ERR|gobject.IO_HUP|gobject.IO_NVAL,
+                self.__on_output)
+        self.__err_id = gobject.io_add_watch(self.stderr,
+                gobject.IO_IN|gobject.IO_ERR|gobject.IO_HUP|gobject.IO_NVAL,
+                self.__on_output)
+
+    stdin = property(lambda s: s.__child.tochild.fileno(),
+            doc='child stdin fd')
+    stdout = property(lambda s: s.__child.fromchild.fileno(),
+            doc='child stdout fd')
+    stderr = property(lambda s: s.__child.childerr.fileno(),
+            doc='child stderr fd')
+    pid = property(lambda s: s.__child.pid, doc='child pid')
+
+    def poll(self):
+        """Return the exit status of the child process, or -1 if unfinished."""
+        return self.__child.poll()
+
+    def wait(self):
+        """Wait for and return the exit status of the child process."""
+        return self.__child.wait()
+
+    def do_get_property(self, property):
+        if property.name in self.__gproperties__:
+            return getattr(self, property.name)
+        else:
+            raise AttributeError, 'unknown property %s' % property.name
+
+    def do_set_property(self, property, value):
+        if property.name in self.__gproperties__:
+            return setattr(self, property.name, value)
+        else:
+            raise AttributeError, 'unknown property %s' % property.name
+
+    def output_chunk(self, fd, chunk):
+        gobject.idle_add(self.emit, 'output-chunk', fd, chunk)
+    def output_line(self, fd, line):
+        gobject.idle_add(self.emit, 'output-line', fd, line)
+    def output_eof(self, fd):
+        gobject.idle_add(self.emit, 'output-eof', fd)
+
+    def __on_output(self, fd, cond):
+        if cond & gobject.IO_IN == gobject.IO_IN:
+            read = os.read(fd, 1024)
+            if read != '':
+               self.output_chunk(fd, read)
+            output = self.__buf.setdefault(fd, '') + read
+
+            lines = [output]
+            for splitter in self.newlines:
+                lines = sum([line.split(splitter) for line in lines], [])
+            self.__buf[fd] = lines.pop()
+
+            for line in lines:
+                self.output_line(fd, line)
+            if read == '':
+                self.output_line(fd, self.__buf.pop(fd))
+                self.output_eof(fd)
+                return False
+            return True
+
+        elif cond & (gobject.IO_ERR|gobject.IO_HUP|gobject.IO_NVAL):
+            self.output_eof(fd)
+            return False
+
+gobject.type_register(Subprocess)
+
+if __name__ == '__main__':
+    import gtk, pango
+    win = gtk.Dialog()
+    win.set_default_size(400,100)
+    label = gtk.Label()
+    label.set_ellipsize(pango.ELLIPSIZE_END)
+    label.show()
+    win.vbox.pack_start(label)
+    print 'create process'
+    process = Subprocess(['vorbisgain', '-a', '-d', '-s'], newlines='\r\n')
+    print 'process created'
+    def update_label(process, fd, line, label):
+        if line:
+            label.set_text(line)
+            print '*', 
+        print repr(line)
+    process.connect('output-line', update_label, label)
+    print 'connected'
+    process.run()
+    print 'running'
+    win.run()
---
+++ plugins/replaygain.py
@@ -0,0 +1,290 @@
+#! /usr/bin/env python
+#
+#    VorbisGain plugin for Quod Libet
+#    Copyright (C) 2005  Michael Urman
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of version 2 of the GNU General Public License as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+
+import gtk, pango, gobject, os, sre
+
+VORBIS_PROGRESS = sre.compile(r'(?P<percent>\d+)% - (?P<file>.+)')
+VORBIS_ALBUM = sre.compile(r'Recommended Album Gain:\s+(?P<albumgain>[-+]?\d+\.\d+)\s+dB')
+VORBIS_TRACK = sre.compile(r'(?P<gain>[-+]?\d\.\d+) dB \|\s+(?P<peak>\d+) \|\s+(?P<scale>\d+\.\d+) \|\s+(?P<newpeak>\d+) \| (?P<file>.+)')
+
+MP3_PROGRESS = sre.compile(r' (?P<file>.+?)\s+(?P<percent>\d+)% done, ETA.+batch')
+MP3_ALBUM = sre.compile(r'(?P<gain>[-+]?\d+\.\d+)dB\s+volume adjustment')
+MP3_TRACK = sre.compile(r'(?P<level>[-+]?\d+\.\d+)dBFS\s+(?P<peak>[-+]?\d+\.\d+)dBFS\s+(?P<gain>[-+]?\d+\.\d+)dB\s+(?P<file>.+)')
+
+__all__ = ['ReplayGain']
+class ReplayGain(object):
+
+    PLUGIN_NAME = 'ReplayGain'
+    PLUGIN_DESC = ('Invokes vorbisgain or normalize-audio on selected '
+                   'songs, grouped by album')
+    PLUGIN_ICON = gtk.STOCK_CDROM
+    PLUGIN_VERSION = "0.15"
+
+    class VorbisGainer(object):
+        def __init__(self, gain):
+            self.gain = gain
+            self.command = ['vorbisgain']
+            self.args = ['--album', '--skip', ]#'--display-only']
+
+        def run(self, songs):
+            from _subprocobj import Subprocess
+            files = [song['~filename'] for song in songs]
+            win = self.gain.get_window()
+            win.process = Subprocess(
+                self.command + self.args + files, newlines='\r\n')
+            win.ids = [
+                win.process.connect(
+                'output-line', self.__output, song('album')),
+                win.process.connect('output-eof', self.__eof),
+            ]
+            self.complete = False
+            win.process.start()
+
+        def __output(self, process, fd, line, album,
+                     progressre=VORBIS_PROGRESS,
+                     trackre=VORBIS_TRACK, albumre=VORBIS_ALBUM):
+
+            match = progressre.search(line)
+            if match:
+                d = match.groupdict()
+                p = d.get('percent','')
+                f = d.get('file', '')
+                self.gain.update_song(f, percent=p)
+
+            match = trackre.search(line)
+            if match:
+                d = match.groupdict()
+                g = d.get('gain', '')
+                f = d.get('file', '')
+                self.gain.update_song(f, gain=g)
+
+            match = albumre.search(line)
+            if match:
+                d = match.groupdict()
+                g = d.get('albumgain')
+                self.gain.update_album(album, gain=g)
+
+        def __eof(self, process, fd):
+            self.gain.set_buttons(True)
+            self.gain.complete = True
+
+    class MP3Gainer(object):
+        def __init__(self, gain):
+            self.gain = gain
+            self.command = ['normalize-audio']
+            self.args1 = ['-n']
+            self.args2 = ['-n', '-b']
+
+        def run(self, songs):
+            from _subprocobj import Subprocess
+            files = [song['~filename'] for song in songs]
+            self.__files = [(song('~basename'), song['~filename'], song)
+                            for song in songs]
+            win = self.gain.get_window()
+            win.process = Subprocess(self.command + self.args1 + files,
+                    newlines='\r\n')
+            win.ids = [
+                win.process.connect(
+                'output-line', self.__output, song('album')),
+                win.process.connect('output-eof', self.__eof1),
+            ]
+            self.__set_album = False
+            self.complete = False
+            win.process.start()
+
+            while not self.complete: gtk.main_iteration()
+            self.complete = 100
+            for id in win.ids: win.process.disconnect(id)
+
+            win.process = Subprocess(
+                self.command + self.args2 + files, newlines='\r\n')
+            win.ids = [
+                win.process.connect(
+                'output-line', self.__output, song('album')),
+                win.process.connect('output-eof', self.__eof2),
+            ]
+            win.process.start()
+
+        def __match_file(self, shortfile):
+            if not shortfile: return shortfile, None
+            for b, f, s in self.__files:
+                if b.startswith(shortfile): return f, s
+            for b, f, s in self.__files:
+                if f == shortfile: return f, s
+            return shortfile, None
+
+        def __output(self, process, fd, line, album, progressre=MP3_PROGRESS,
+                     trackre=MP3_TRACK, albumre=MP3_ALBUM):
+
+            match = progressre.search(line)
+            if match:
+                d = match.groupdict()
+                p = str(int((int(d.get('percent',''))+self.complete)*0.5))
+                f, s = self.__match_file(d.get('file', ''))
+                return self.gain.update_song(f, percent=p)
+            
+            match = trackre.search(line)
+            if match:
+                try:
+                    d = match.groupdict()
+                    g = d.get('gain', '')
+                    f, s = self.__match_file(d.get('file', ''))
+                    self.gain.update_song(f, gain=g)
+                    s['replaygain_track_gain'] = g + ' dB'
+                    s['replaygain_track_peak'] = '0' # unsupported; required
+                except:
+                    from traceback import print_exc
+                    print_exc()
+                return
+
+            match = albumre.search(line)
+            if match:
+                d = match.groupdict()
+                g = d.get('gain')
+                self.__set_album = True
+                self.gain.update_album(album, gain=g)
+                for b, f, s in self.__files:
+                    s['replaygain_album_gain'] = g + ' dB'
+                    s['replaygain_album_peak'] = '0' # unsupported; required
+                return
+
+        def __eof1(self, process, fd):
+            self.complete = True
+
+        def __eof2(self, process, fd):
+            if not self.__set_album:
+                for b, f, s in self.__files:
+                    s['replaygain_album_peak'] = '0'
+                    s['replaygain_album_gain'] = '0'
+            self.gain.set_buttons(True)
+            self.gain.complete = True
+
+    def get_window(self):
+        try: return ReplayGain.win
+        except AttributeError: pass
+
+        win = gtk.Dialog(title='ReplayGain',
+                buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                    gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+        win.set_default_size(400, 300)
+        win.set_border_width(6)
+        swin = gtk.ScrolledWindow()
+        win.vbox.pack_start(swin)
+        swin.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        swin.set_shadow_type(gtk.SHADOW_IN)
+        from qltk import HintedTreeView
+        ReplayGain.model = gtk.ListStore(object, str, str, int, str, str)
+        ReplayGain.view = view = HintedTreeView(ReplayGain.model)
+        swin.add(view)
+
+        columns = [gtk.TreeViewColumn(n.title()) for n in 
+                    ['track', 'progress', 'gain', 'album']]
+        for col in columns:
+            col.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+            col.set_resizable(True)
+            view.append_column(col)
+        columns[0].set_expand(True)
+        columns[0].set_fixed_width(1)
+
+        def add(render, col, **kw):
+            expand = not isinstance(render, gtk.CellRendererPixbuf)
+            col.pack_start(render, expand=expand)
+            if col.get_title() == "Track" and expand:
+                render.set_property('ellipsize', pango.ELLIPSIZE_END)
+            for k, v in kw.items():
+                col.add_attribute(render, k, v)
+
+        add(gtk.CellRendererPixbuf(), columns[0], stock_id=1)
+        add(gtk.CellRendererText(), columns[0], text=2)
+        add(gtk.CellRendererProgress(), columns[1], value=3)
+        add(gtk.CellRendererText(), columns[2], text=4)
+        add(gtk.CellRendererText(), columns[3], text=5)
+
+        ReplayGain.win = win
+        win.ids = []
+        win.connect('response', self.__response)
+        win.connect('delete-event', self.__delete)
+        return win
+
+    def add_song(self, song):
+        if song('~basename').endswith('.ogg'): pixbuf = gtk.STOCK_CDROM
+        elif song('~basename').endswith('.mp3'): pixbuf = gtk.STOCK_CDROM
+        else: pixbuf = gtk.STOCK_DIALOG_WARNING
+        self.model.append(
+            [song, pixbuf, song('~tracknumber~title~version'), 0, 0, 0])
+
+    def update_song(self, song, percent=None, gain=None):
+        if not percent and not gain: return
+        for row in self.model:
+            if row[0]('~filename') == song:
+                self.view.scroll_to_cell(row.path)
+                if percent: row[3] = int(percent)
+                if gain: row[4] = gain
+                break
+
+    def update_album(self, album, gain):
+        for row in self.model:
+            if row[0]('album') == album:
+                row[5] = gain
+
+    def set_buttons(self, to):
+        try:
+            buttons = self.win.vbox.get_children()[2].get_children()
+        except IndexError:
+            pass
+        else:
+            buttons[0].set_sensitive(to)
+            buttons[1].set_sensitive(not to)
+
+    def plugin_album(self, songs):
+        win = self.get_window()
+        win.show_all()
+        self.complete = True
+
+        songs.sort()
+
+        for song in songs: self.add_song(song)
+
+        mp3s = [ song for song in songs if song('~basename').endswith('.mp3')]
+        oggs = [ song for song in songs if song('~basename').endswith('.ogg')]
+
+        if len(mp3s) == len(songs): gainer = self.MP3Gainer(self)
+        elif len(oggs) == len(songs): gainer = self.VorbisGainer(self)
+        else: return
+
+        self.set_buttons(False)
+
+        self.complete = False
+        gainer.run(songs)
+
+        while not self.complete: gtk.main_iteration()
+        
+        self.complete = False
+
+    def __delete(self, win, event):
+        self.__response(win, gtk.RESPONSE_CANCEL)
+        return True
+
+    def __response(self, win, response):
+        win.hide()
+        self.model.clear()
+        if response != gtk.RESPONSE_CLOSE and not self.complete:
+            import signal
+            os.kill(win.process.pid, signal.SIGKILL)
+            win.process.wait()
+        for id in win.ids: win.process.disconnect(id)
+        del win.ids[:]
+        self.complete = True
+
