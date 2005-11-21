18f52e5f6a0be6f51b42cbfccc4693118e497aaa
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-11-21 09:09:34 +0000

    qltk: Adopt LibraryBrowser.

--- quodlibet/qltk/__init__.py
+++ quodlibet/qltk/__init__.py
@@ -825,6 +825,79 @@ class BigCenteredImage(gtk.Window):
     def __destroy(self, event):
         self.destroy()
 
+class LibraryBrowser(Window):
+    def __init__(self, Kind, watcher):
+        super(LibraryBrowser, self).__init__()
+        self.set_border_width(12)
+        self.set_title(_("Library Browser"))
+
+        view = SongList(watcher)
+
+        sw = gtk.ScrolledWindow()
+        sw.set_shadow_type(gtk.SHADOW_IN)
+        sw.add(view)
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
+        self.browser = browser = Kind(watcher, main=False)
+        browser.connect_object('songs-selected', SongList.set_songs, view)
+        if browser.reordered: view.enable_drop()
+
+        if Kind.expand:
+            container = Kind.expand()
+            container.pack1(browser, resize=True)
+            container.pack2(sw, resize=True)
+            self.add(container)
+        else:
+            vbox = gtk.VBox(spacing=6)
+            vbox.pack_start(browser, expand=False)
+            vbox.pack_start(sw)
+            self.add(vbox)
+
+        view.connect('button-press-event', self.__button_press, watcher)
+        view.connect('popup-menu', self.__menu, 3, 0, watcher)
+        view.connect('drag-data-received', self.__drag_data_recv)
+        view.connect('row-activated', self.__enqueue)
+        if browser.headers is not None:
+            view.connect('columns-changed', self.__cols_changed, browser)
+            self.__cols_changed(view, browser)
+        self.set_default_size(500, 300)
+        sw.show_all()
+        self.child.show()
+        self.show()
+
+    def __enqueue(self, view, path, column):
+        from widgets import widget
+        widgets.main.playlist.enqueue([view.get_model()[path][0]])
+
+    def __drag_data_recv(self, view, *args):
+        if callable(self.browser.reordered): self.browser.reordered(view)
+        view.set_sort_by(None, refresh=False)
+
+    def __cols_changed(self, view, browser):
+        for header in view.get_columns():
+            tag = header.header_name
+            if "~" in tag[1:]: tag = filter(None, tag.split("~"))[0]
+            header.set_visible(tag in browser.headers)
+
+    def __button_press(self, view, event, watcher):
+        if event.button != 3: return False
+        x, y = map(int, [event.x, event.y])
+        try: path, col, cellx, celly = view.get_path_at_pos(x, y)
+        except TypeError: return True
+        view.grab_focus()
+        selection = view.get_selection()
+        if not selection.path_is_selected(path):
+            view.set_cursor(path, col, 0)
+        self.__menu(view, event.button, event.time, watcher)
+        return True
+
+    def __menu(self, view, button, time, watcher):
+        path, col = view.get_cursor()
+        header = col.header_name
+        view.Menu(header, self.browser, watcher).popup(
+            None, None, None, button, time)
+        return True
+
 def get_top_parent(widget):
     while widget.parent is not None: widget = widget.parent
     return widget
--- quodlibet/widgets.py
+++ quodlibet/widgets.py
@@ -158,7 +158,8 @@ class FIFOControl(object):
 
     def __open_browser(browsername):
         Kind = browsers.get(browsername)
-        if Kind is not browsers.search.EmptyBar: LibraryBrowser(None, Kind)
+        if Kind is not browsers.search.EmptyBar:
+            qltk.LibraryBrowser(Kind, widgets.watcher)
         else: print "W: Unknown browser type %r." % browsername
 
     callbacks = {
@@ -817,7 +818,7 @@ class QLTrayIcon(object):
         m2 = gtk.Menu()
         for id, label, Kind in browsers.get_browsers():
             i = gtk.MenuItem(label)
-            i.connect('activate', LibraryBrowser, Kind)
+            i.connect_object('activate', qltk.LibraryBrowser, Kind, watcher)
             m2.append(i)
         browse.set_submenu(m2)
 
@@ -1521,7 +1522,8 @@ class MainWindow(gtk.Window):
 
         for id, label, Kind in browsers.get_browsers():
             act = gtk.Action(id, label, None, None)
-            act.connect('activate', LibraryBrowser, Kind)
+            act.connect_object(
+                'activate', qltk.LibraryBrowser, Kind, widgets.watcher)
             ag.add_action(act)
 
         self.ui = gtk.UIManager()
@@ -2156,78 +2158,6 @@ class MainSongList(SongList):
         SongList.set_sort_by(self, *args, **kwargs)
         tag, reverse = self.get_sort_by()
         config.set('memory', 'sortby', "%d%s" % (int(not reverse), tag))
-
-class LibraryBrowser(qltk.Window):
-    def __init__(self, activator, Kind):
-        super(LibraryBrowser, self).__init__()
-        self.set_border_width(12)
-        self.set_title(_("Library Browser"))
-
-        view = SongList(widgets.watcher)
-
-        sw = gtk.ScrolledWindow()
-        sw.set_shadow_type(gtk.SHADOW_IN)
-        sw.add(view)
-        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-
-        self.browser = browser = Kind(widgets.watcher, main=False)
-        browser.connect_object('songs-selected', SongList.set_songs, view)
-        if browser.reordered: view.enable_drop()
-
-        if Kind.expand:
-            container = Kind.expand()
-            container.pack1(browser, resize=True)
-            container.pack2(sw, resize=True)
-            self.add(container)
-        else:
-            vbox = gtk.VBox(spacing=6)
-            vbox.pack_start(browser, expand=False)
-            vbox.pack_start(sw)
-            self.add(vbox)
-
-        view.connect('button-press-event', self.__button_press)
-        view.connect('popup-menu', self.__menu, 3, 0)
-        view.connect('drag-data-received', self.__drag_data_recv)
-        view.connect('row-activated', self.__enqueue)
-        if browser.headers is not None:
-            view.connect('columns-changed', self.__cols_changed, browser)
-            self.__cols_changed(view, browser)
-        self.set_default_size(500, 300)
-        sw.show_all()
-        self.child.show()
-        self.show()
-
-    def __enqueue(self, view, path, column):
-        widgets.main.playlist.enqueue([view.get_model()[path][0]])
-
-    def __drag_data_recv(self, view, *args):
-        if callable(self.browser.reordered): self.browser.reordered(view)
-        view.set_sort_by(None, refresh=False)
-
-    def __cols_changed(self, view, browser):
-        for header in view.get_columns():
-            tag = header.header_name
-            if "~" in tag[1:]: tag = filter(None, tag.split("~"))[0]
-            header.set_visible(tag in browser.headers)
-
-    def __button_press(self, view, event):
-        if event.button != 3: return False
-        x, y = map(int, [event.x, event.y])
-        try: path, col, cellx, celly = view.get_path_at_pos(x, y)
-        except TypeError: return True
-        view.grab_focus()
-        selection = view.get_selection()
-        if not selection.path_is_selected(path):
-            view.set_cursor(path, col, 0)
-        self.__menu(view, event.button, event.time)
-        return True
-
-    def __menu(self, view, button, time):
-        path, col = view.get_cursor()
-        header = col.header_name
-        view.Menu(header, self.browser, widgets.watcher).popup(
-            None, None, None, button, time)
-        return True
 
 def website_wrap(activator, link):
     if not util.website(link):
