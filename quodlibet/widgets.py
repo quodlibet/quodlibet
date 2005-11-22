447c3ee3a9a4f03cae397310732cd0b174adc722
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-11-22 06:57:08 +0000

    Split off PluginWindow into qltk.plugins. widgets.widgets: Remove 'plugins' slot, now handled by PluginWindow itself.

--- quodlibet/widgets.py
+++ quodlibet/widgets.py
@@ -35,7 +35,7 @@ from qltk.songlist import SongList
 # Give us a namespace for now.. FIXME: We need to remove this later.
 # Or, replace it with nicer wrappers!
 class __widgets(object):
-    __slots__ = ["watcher", "preferences", "main", "plugins"]
+    __slots__ = ["watcher", "preferences", "main"]
 widgets = __widgets()
 
 # Provides some files in ~/.quodlibet to indicate what song is playing
@@ -227,154 +227,6 @@ class CountManager(object):
         elif pl.current is not song:
             song["~#skipcount"] = song.get("~#skipcount", 0) + 1
             watcher.changed([song])
-
-class PluginWindow(qltk.Window):
-    def __init__(self, parent):
-        super(PluginWindow, self).__init__()
-        self.set_title(_("Quod Libet Plugins"))
-        self.set_border_width(12)
-        self.set_resizable(False)
-        self.set_transient_for(parent)
-
-        hbox = gtk.HBox(spacing=12)        
-        vbox = gtk.VBox(spacing=6)
-
-        sw = gtk.ScrolledWindow()
-        sw.set_policy(gtk.POLICY_NEVER, gtk.POLICY_ALWAYS)
-        tv = qltk.HintedTreeView()
-        model = gtk.ListStore(object)
-        tv.set_model(model)
-        tv.set_rules_hint(True)
-
-        render = gtk.CellRendererToggle()
-        def cell_data(col, render, model, iter):
-            render.set_active(
-                SongList.pm.enabled(model[iter][0]))
-        render.connect('toggled', self.__toggled, model)
-        column = gtk.TreeViewColumn("enabled", render)
-        column.set_cell_data_func(render, cell_data)
-        tv.append_column(column)
-
-        render = gtk.CellRendererPixbuf()
-        def cell_data(col, render, model, iter):
-            render.set_property(
-                'stock-id', getattr(model[iter][0], 'PLUGIN_ICON',
-                                    gtk.STOCK_EXECUTE))
-        column = gtk.TreeViewColumn("image", render)
-        column.set_cell_data_func(render, cell_data)
-        tv.append_column(column)
-
-        render = gtk.CellRendererText()
-        render.set_property('ellipsize', pango.ELLIPSIZE_END)
-        render.set_property('xalign', 0.0)
-        column = gtk.TreeViewColumn("name", render)
-        def cell_data(col, render, model, iter):
-            render.set_property('text', model[iter][0].PLUGIN_NAME)
-        column.set_cell_data_func(render, cell_data)
-        column.set_expand(True)
-        tv.append_column(column)
-
-        sw.add(tv)
-        sw.set_shadow_type(gtk.SHADOW_IN)
-
-        tv.set_headers_visible(False)
-
-        refresh = gtk.Button(stock=gtk.STOCK_REFRESH)
-        refresh.set_focus_on_click(False)
-        vbox.pack_start(sw)
-        vbox.pack_start(refresh, expand=False)
-        vbox.set_size_request(250, -1)
-        hbox.pack_start(vbox, expand=False)
-
-        selection = tv.get_selection()
-        desc = gtk.Label()
-        desc.set_alignment(0, 0)
-        desc.set_padding(6, 6)
-        desc.set_line_wrap(True)
-        desc.set_size_request(280, -1)
-        selection.connect('changed', self.__description, desc)
-
-        prefs = gtk.Frame()
-        prefs.set_shadow_type(gtk.SHADOW_NONE)
-        lab = gtk.Label()
-        lab.set_markup("<b>%s</b>" % _("Preferences"))
-        prefs.set_label_widget(lab)
-
-        vb2 = gtk.VBox(spacing=12)
-        vb2.pack_start(desc, expand=False)
-        vb2.pack_start(prefs, expand=False)
-        hbox.pack_start(vb2, expand=True)
-
-        self.add(hbox)
-
-        selection.connect('changed', self.__preferences, prefs)
-        refresh.connect('clicked', self.__refresh, tv, desc)
-        tv.get_selection().emit('changed')
-        refresh.clicked()
-        hbox.set_size_request(550, 350)
-
-        self.connect_object('destroy', self.__class__.__destroy, self)
-
-        self.show_all()
-
-    def __destroy(self):
-        try: del(widgets.plugins)
-        except AttributeError: pass
-        else: config.write(const.CONFIG)
-
-    def __description(self, selection, frame):
-        model, iter = selection.get_selected()
-        if not iter: return
-        text = "<big>%s</big>\n" % util.escape(model[iter][0].PLUGIN_NAME)
-        try: text += "<small>%s</small>\n" %(
-            util.escape(model[iter][0].PLUGIN_VERSION))
-        except (TypeError, AttributeError): pass
-
-
-        try: text += "\n" + util.escape(model[iter][0].PLUGIN_DESC)
-        except (TypeError, AttributeError): pass
-
-        frame.set_markup(text)
-
-    def __preferences(self, selection, frame):
-        model, iter = selection.get_selected()
-        if frame.child: frame.child.destroy()
-        if iter and hasattr(model[iter][0], 'PluginPreferences'):
-            try:
-                prefs = model[iter][0].PluginPreferences(self)
-            except:
-                import traceback; traceback.print_exc()
-                frame.hide()
-            else:
-                if isinstance(prefs, gtk.Window):
-                    b = gtk.Button(stock=gtk.STOCK_PREFERENCES)
-                    b.connect_object('clicked', gtk.Window.show, prefs)
-                    b.connect_object('destroy', gtk.Window.destroy, prefs)
-                    frame.add(b)
-                    frame.child.set_border_width(6)
-                else:
-                    frame.add(prefs)
-                frame.show_all()
-        else: frame.hide()
-
-    def __toggled(self, render, path, model):
-        render.set_active(not render.get_active())
-        SongList.pm.enable(model[path][0], render.get_active())
-        SongList.pm.save()
-        model[path][0] = model[path][0]
-
-    def __refresh(self, activator, view, desc):
-        model, sel = view.get_selection().get_selected()
-        if sel: sel = model[sel][0]
-        model.clear()
-        SongList.pm.rescan()
-        plugins = SongList.pm.list()
-        plugins.sort(lambda a, b: cmp(a.PLUGIN_NAME, b.PLUGIN_NAME))
-        for plugin in plugins:
-            it = model.append(row=[plugin])
-            if plugin is sel: view.get_selection().select_iter(it)
-        if not plugins:
-            desc.set_text(_("No plugins found."))
 
 class AboutWindow(gtk.AboutDialog):
     def __init__(self, parent=None):
@@ -1254,9 +1106,8 @@ class MainWindow(gtk.Window):
         widgets.preferences.present()
 
     def __plugins(self, activator):
-        if not hasattr(widgets, 'plugins'):
-            widgets.plugins = PluginWindow(self)
-        widgets.plugins.present()
+        from qltk.plugins import PluginWindow
+        PluginWindow(self, SongList.pm)
 
     def open_location(self, action):
         name = qltk.GetStringDialog(self, _("Add a Location"),
