8e9e069b2613548f61e990416fcd1ea4107486ea
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-12-04 04:30:06 +0000

    AudioFeeds: Allow stopping downloads.

--- quodlibet/qltk/downloader.py
+++ quodlibet/qltk/downloader.py
@@ -11,6 +11,8 @@ import urllib
 import gobject, gtk, pango
 import util
 
+from qltk.views import PrettyDragTreeView
+
 class DownloadWindow(gtk.Window):
     __window = None
 
@@ -26,15 +28,16 @@ class DownloadWindow(gtk.Window):
         self.set_title("Quod Libet - " + _("Downloads"))
         self.set_default_size(300, 150)
         self.set_border_width(12)
-        self.__timeout = None
 
-        model = gtk.ListStore(str, int)
-        tv = gtk.TreeView()
+        model = gtk.ListStore(str, int, int)
+        tv = PrettyDragTreeView()
+        tv.connect('popup-menu', self.__popup_menu)
+        tv.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
         tv.set_model(model)
         tv.set_rules_hint(True)
 
         render = gtk.CellRendererText()
-        render.set_property('ellipsize', pango.ELLIPSIZE_END)
+        render.set_property('ellipsize', pango.ELLIPSIZE_START)
         column = gtk.TreeViewColumn(_("Filename"), render, text=0)
         column.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
         column.set_expand(True)
@@ -57,21 +60,29 @@ class DownloadWindow(gtk.Window):
         self.connect('delete-event', self.__delete_event)
         self.child.show_all()
 
+    def __popup_menu(self, view):
+        selection = view.get_selection()
+        model, paths = selection.get_selected_rows()
+        if model:
+            iters = map(model.get_iter, paths)
+            menu = gtk.Menu()
+            item = gtk.ImageMenuItem(gtk.STOCK_STOP)
+            item.connect_object('activate', self.__stop_download, model, iters)
+            menu.append(item)
+            menu.connect('selection-done', lambda m: m.destroy())
+            menu.show_all()
+            menu.popup(None, None, None, 0, gtk.get_current_event_time())
+            return True
+
+    def __stop_download(self, model, iters):
+        for iter in iters:
+            gobject.source_remove(model[iter][2])
+            os.unlink(model[iter][0])
+            model.remove(iter)
+
     def __delete_event(self, window, event):
-        gobject.handler_disconnect(self.__timeout)
-        self.__timeout = None
         self.hide()
         return True
-
-    def __check_and_update(self):
-        model = self.child.child.get_model()
-        for i in range(len(model)):
-            model.row_changed(i, model.get_iter((i,)))
-        return True
-
-    def present(self):
-        super(DownloadWindow, self).present()
-        self.__timeout = gobject.timeout_add(1000, self.__check_and_update)
 
     def _download(self, source, target):
         fileobj = file(target, "wb")
@@ -79,9 +90,10 @@ class DownloadWindow(gtk.Window):
         sock = url.fp._sock
         sock.setblocking(0)
         model = self.child.child.get_model()
-        iter = model.append(row=[os.path.basename(target), 0])
-        gobject.io_add_watch(sock, gobject.IO_IN|gobject.IO_ERR|gobject.IO_HUP,
-                             self.__got_data, fileobj, model, iter)
+        iter = model.append(row=[target, 0, 0])
+        model[iter][2] = gobject.io_add_watch(
+            sock, gobject.IO_IN|gobject.IO_ERR|gobject.IO_HUP,
+            self.__got_data, fileobj, model, iter)
 
     def __got_data(self, src, condition, fileobj, model, iter):
         if condition in [gobject.IO_ERR, gobject.IO_HUP]:
