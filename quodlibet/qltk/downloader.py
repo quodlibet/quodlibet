896ad60cb752a519a44d50b04890186ed2b2cec9
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-12-04 04:54:30 +0000

    DownloadWindow: Clean up better.

--- quodlibet/qltk/downloader.py
+++ quodlibet/qltk/downloader.py
@@ -11,7 +11,7 @@ import urllib
 import gobject, gtk, pango
 import util
 
-from qltk.views import PrettyDragTreeView
+from qltk.views import HintedTreeView
 
 class DownloadWindow(gtk.Window):
     __window = None
@@ -28,9 +28,9 @@ class DownloadWindow(gtk.Window):
         self.set_title("Quod Libet - " + _("Downloads"))
         self.set_default_size(300, 150)
         self.set_border_width(12)
-
-        model = gtk.ListStore(str, int, int)
-        tv = PrettyDragTreeView()
+        self.__timeout = None
+        model = gtk.ListStore(object, object, int) # fileobjs - source, target
+        tv = HintedTreeView()
         tv.connect('popup-menu', self.__popup_menu)
         tv.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
         tv.set_model(model)
@@ -38,18 +38,21 @@ class DownloadWindow(gtk.Window):
 
         render = gtk.CellRendererText()
         render.set_property('ellipsize', pango.ELLIPSIZE_START)
-        column = gtk.TreeViewColumn(_("Filename"), render, text=0)
+        column = gtk.TreeViewColumn(_("Filename"), render)
         column.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
         column.set_expand(True)
+        def cell_data_name(column, cell, model, iter):
+            cell.set_property('text', model[iter][1].name)
+        column.set_cell_data_func(render, cell_data_name)
         tv.append_column(column)
         
         render = gtk.CellRendererText()
         column = gtk.TreeViewColumn(_("Size"), render)
         column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
-        def cell_data(column, cell, model, iter):
-            size = util.format_size(model[iter][1])
-            render.set_property('text', size)
-        column.set_cell_data_func(render, cell_data)
+        def cell_data_size(column, cell, model, iter):
+            size = util.format_size(model[iter][1].tell())
+            cell.set_property('text', size)
+        column.set_cell_data_func(render, cell_data_size)
         tv.append_column(column)
 
         sw = gtk.ScrolledWindow()
@@ -59,6 +62,13 @@ class DownloadWindow(gtk.Window):
         self.add(sw)
         self.connect('delete-event', self.__delete_event)
         self.child.show_all()
+
+    def __update(self):
+        if self.child is None: return True
+        model = self.child.child.get_model()
+        for i in range(len(model)):
+            model.row_changed((i,), model.get_iter(i))
+        return True
 
     def __popup_menu(self, view):
         selection = view.get_selection()
@@ -77,11 +87,20 @@ class DownloadWindow(gtk.Window):
     def __stop_download(self, model, iters):
         for iter in iters:
             gobject.source_remove(model[iter][2])
-            os.unlink(model[iter][0])
+            model[iter][0].close()
+            model[iter][1].close()
+            os.unlink(model[iter][1].name)
             model.remove(iter)
+
+    def present(self):
+        super(DownloadWindow, self).present()
+        if self.__timeout is None:
+            gobject.timeout_add(1000, self.__update)
 
     def __delete_event(self, window, event):
         self.hide()
+        if self.__timeout is not None: gobject.source_remove(self.__timeout)
+        self.__timeout = None
         return True
 
     def _download(self, source, target):
@@ -90,10 +109,11 @@ class DownloadWindow(gtk.Window):
         sock = url.fp._sock
         sock.setblocking(0)
         model = self.child.child.get_model()
-        iter = model.append(row=[target, 0, 0])
-        model[iter][2] = gobject.io_add_watch(
+        iter = model.append(row=[sock, fileobj, 0])
+        sig_id = gobject.io_add_watch(
             sock, gobject.IO_IN|gobject.IO_ERR|gobject.IO_HUP,
             self.__got_data, fileobj, model, iter)
+        model[iter][2] = sig_id
 
     def __got_data(self, src, condition, fileobj, model, iter):
         if condition in [gobject.IO_ERR, gobject.IO_HUP]:
@@ -103,7 +123,6 @@ class DownloadWindow(gtk.Window):
             return False
         else:
             buf = src.recv(1024*1024)
-            model[iter][1] += len(buf)
             if buf: fileobj.write(buf)
             else:
                 fileobj.close()
