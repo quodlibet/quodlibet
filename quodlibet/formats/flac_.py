2a0e825fd4071fc812c3b5bdac00d6e4069e96d5
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-12-20 05:13:56 +0000

    FLACFile: Use new Mutagen-based FLAC.

--- quodlibet/formats/flac_.py
+++ quodlibet/formats/flac_.py
@@ -11,65 +11,28 @@ import gst
 from formats._vorbis import VCFile
 import util
 
-try: import flac.metadata, flac.decoder
+try: import mutagen.flac
 except: extensions = []
 else:
     if gst.element_factory_make('flacdec'): extensions = [".flac"]
     else: extensions = []
 
 class FLACFile(VCFile):
-
     format = "FLAC"
 
     def __init__(self, filename):
-        if not os.path.exists(filename):
-            raise IOError("%s does not exist" % filename)
-        elif file(filename, "rb").read(4) != "fLaC":
-            raise IOError("%s is not a FLAC file" % filename)
-        chain = flac.metadata.Chain()
-        chain.read(filename)
-        it = flac.metadata.Iterator()
-        it.init(chain)
-        vc = None
-        while True:
-            if it.get_block_type() == flac.metadata.VORBIS_COMMENT:
-                block = it.get_block()
-                vc = flac.metadata.VorbisComment(block)
-            elif it.get_block_type() == flac.metadata.STREAMINFO:
-                info = it.get_block().data.stream_info
-                self["~#length"] = (info.total_samples // info.sample_rate)
-            if not it.next(): break
-
-        if vc:
-            for k in vc.comments:
-                parts = k.split("=")
-                key = parts[0].lower()
-                val = util.decode("=".join(parts[1:]))
-                if key in self: self[key] += "\n" + val
-                else: self[key] = val
+        f = mutagen.flac.FLAC(filename)
+        self["~#length"] = int(f.info.length)
+        for key, value in f.vc.items(): self[key] = "\n".join(value)
         self.sanitize(filename)
 
     def write(self):
-        chain = flac.metadata.Chain()
-        chain.read(self['~filename'])
-        it = flac.metadata.Iterator()
-        it.init(chain)
-        vc = None
-        while True:
-            if it.get_block_type() == flac.metadata.VORBIS_COMMENT:
-                block = it.get_block()
-                vc = flac.metadata.VorbisComment(block)
-                break
-            if not it.next(): break
-
-        if vc:
-            keys = [k.split("=")[0] for k in vc.comments]
-            for k in keys: del(vc.comments[k])
-            for key in self.realkeys():
-                value = self.list(key)
-                for line in value:
-                    vc.comments[key] = util.encode(line)
-            self._prep_write(vc.comments)
-            chain.write(True, True)
+        f = mutagen.flac.FLAC(self["~filename"])
+        if not f.vc: f.add_vorbiscomment()
+        del(f.vc[:])
+        for key in self.realkeys(): f.vc[key] = self.list(key)
+        self._prep_write(f.vc)
+        f.save()
+        self.sanitize()
 
 info = FLACFile
