9ccc0ac39c414f1eb7cd1fd274d0383d900ec4aa
Joe Wreschnig <joe.wreschnig@gmail.com>
2005-12-25 04:19:52 +0000

    qltk.renamefiles: Add tests for filters.

---
+++ quodlibet/tests/test_qltk_renamefiles.py
@@ -0,0 +1,33 @@
+from tests import add, TestCase
+from qltk.renamefiles import SpacesToUnderscores, StripWindowsIncompat, StripDiacriticals, StripNonASCII
+
+class TFilter(TestCase):
+    def setUp(self): self.c = self.Kind()
+    def tearDown(self): self.c.destroy()
+    def test_empty(self): self.failUnlessEqual(self.c.filter(u""), "")
+    def test_safe(self): self.failUnlessEqual(self.c.filter(u"safe"), "safe")
+
+class TSpacesToUnderscores(TFilter):
+    Kind = SpacesToUnderscores
+    def test_conv(self):
+        self.failUnlessEqual(self.c.filter("foo bar "), "foo_bar_")
+add(TSpacesToUnderscores)
+
+class TStripWindowsIncompat(TFilter):
+    Kind = StripWindowsIncompat
+    def test_conv(self):
+        self.failUnlessEqual(self.c.filter('foo\\:*?;"<>|'), "foo_________")
+add(TStripWindowsIncompat)
+
+class TStripDiacriticals(TFilter):
+    Kind = StripDiacriticals
+    def test_conv(self):
+        self.failUnlessEqual(self.c.filter(u"\u00c1 test"), "A test")
+add(TStripDiacriticals)
+
+class TStripNonASCII(TFilter):
+    Kind = StripNonASCII
+    def test_conv(self):
+        self.failUnlessEqual(
+            self.c.filter(u"foo \u00c1 \u1234"), "foo _ _")
+add(TStripNonASCII)
