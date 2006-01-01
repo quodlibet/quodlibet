00491984681dfc9d2c6be8b182dd3a6441e04b6e
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-01-01 05:12:27 +0000

    plugins.editing: TagsFromPath, EditTags plugin types.

--- quodlibet/plugins/editing.py
+++ quodlibet/plugins/editing.py
@@ -31,6 +31,41 @@ class RenameFilesPlugin(object):
     active = False
     def filter(self, value): return value
 
+
+class TagsFromPathPlugin(object):
+    """Plugins of this type must subclass a GTK widget. They will be
+    packed into the TagsFromPath pane (currently a ScrolledWindow hidden
+    with an expander, but that might change).
+
+    The 'filter' function will be called with the tag and proposed value
+    as a unicode object. It should return an appropriate-transformed
+    filename, still as a unicode object.
+
+    The plugin must provide either a 'changed' or 'preview'. 'changed'
+    causes the entire display to be re-previewed (e.g. via a checkbox).
+    'preview' causes the Preview button to made sensitive, and Save
+    to be disabled.
+
+    If the 'active' attribute is false, the filter will not be called.
+    This is particularly useful for gtk.CheckButtons."""
+
+    active = False
+    def filter(self, tag, value): return value
+
+class EditTagsPlugin(gtk.ImageMenuItem):
+    """Plugins of this type are subclasses of gtk.ImageMenuItem.
+    They will be added to the context menu of the EditTags tree view.
+
+    The 'tags' attribute is a list of tags this plugin should appear on,
+    or false if it should appear for all tags.
+
+    The constructor is called with the tag and value for that row. This
+    can be used to set the sensitivity of the menu item, or change its
+    text.
+    """
+
+    tags = []
+
 class EditingPlugins(Manager):
     __PATHS = [os.path.join("./plugins", "editing"),
                os.path.join(const.PLUGINS, "editing")]
@@ -40,3 +75,9 @@ class EditingPlugins(Manager):
 
     def RenamePlugins(self):
         return super(EditingPlugins, self).find_subclasses(RenameFilesPlugin)
+
+    def TagsFromPathPlugins(self):
+        return super(EditingPlugins, self).find_subclasses(TagsFromPathPlugin)
+
+    def EditTagsPlugins(self):
+        return super(EditingPlugins, self).find_subclasses(EditTagsPlugin)
