92f6d5708090a3f28b182ea159b8f0d86eba42de
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-01-15 11:07:36 +0000

    PO tests: Generate subclasses dynamically (like metadata tests).

--- quodlibet/tests/test_po.py
+++ quodlibet/tests/test_po.py
@@ -1,30 +1,32 @@
 import os, glob
 from tests import add, TestCase
 
-class TPO(TestCase):
+class PO(TestCase):
     def test_pos(self):
-        for f in glob.glob("po/*.po"):
-            self.failIf(os.system("msgfmt -c %s > /dev/null" % f))
+        self.failIf(os.system("msgfmt -c po/%s.po > /dev/null" % self.lang))
         try: os.unlink("messages.mo")
         except OSError: pass
 
     def test_gtranslator_blows_goats(self):
-        for f in glob.glob("po/*.po"):
-            for line in file(f):
-                if line.strip().startswith("#"): continue
-                self.failIf(
-                    "\xc2\xb7" in line,
-                    "Broken GTranslator copy/paste in %s:\n%s" % (f, line))
+        for line in file("po/%s.po" % self.lang):
+            if line.strip().startswith("#"): continue
+            self.failIf("\xc2\xb7" in line,
+                        "Broken GTranslator copy/paste in %s:\n%s" % (
+                self.lang, line))
 
     def test_gtk_stock_items(self):
-        for f in glob.glob("po/*.po"):
-            for line in file(f):
-                if line.strip().startswith('msgstr "gtk-'):
-                    parts = line.strip().split()
-                    value = parts[1].strip('"')[4:]
-                    self.failIf(value and value not in [
-                        'media-next', 'media-previous', 'media-play',
-                        'media-pause'],
-                                "Invalid stock translation in %s\n%s" %(
-                        f, line))
-add(TPO)
+        for line in file("po/%s.po" % self.lang):
+            if line.strip().startswith('msgstr "gtk-'):
+                parts = line.strip().split()
+                value = parts[1].strip('"')[4:]
+                self.failIf(value and value not in [
+                    'media-next', 'media-previous', 'media-play',
+                    'media-pause'],
+                            "Invalid stock translation in %s\n%s" %(
+                    self.lang, line))
+
+for fn in glob.glob("po/*.po"):
+    lang = fn[3:-3]
+    testcase = type('PO.' + lang, (PO,), {})
+    testcase.lang = lang
+    add(testcase)
