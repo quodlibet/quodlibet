2734ba89bd8e9e34c7cdf33085d9064822643601
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-02-08 06:08:16 +0000

    Junk.

--- plugins/remote/fifotest.py
+++
@@ -1,7 +0,0 @@
-from plugins.remote import FIFOPlugin
-from qltk.msg import WarningMessage
-
-class FIFOTest(FIFOPlugin):
-    commands = ["test_a", "test_b"]
-    def __init__(self, command, watcher, window, player):
-        print _("You said to %s.") % command
