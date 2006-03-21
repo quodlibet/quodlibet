893d1c236434114fd46971af8dc82e135fb7248d
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-03-21 21:47:01 +0000

    Tests: SongProperties, SongsMenu.

---
+++ quodlibet/tests/test_qltk_properties.py
@@ -0,0 +1,49 @@
+import gtk
+
+from tests import add, TestCase
+from qltk.properties import SongProperties
+from qltk.watcher import SongWatcher
+from formats._audio import AudioFile
+
+class DummyPlugins(object):
+    def rescan(self): pass
+    def find_subclasses(self, *args): return []
+    def TagsFromPathPlugins(self): return []
+    def RenamePlugins(self): return []
+    def EditTagsPlugins(self): return []
+
+class TSongProperties(TestCase):
+    af1 = AudioFile({"title": "woo"})
+    af1.sanitize("invalid")
+    af2 = AudioFile({"title": "bar", "album": "quux"})
+    af2.sanitize("alsoinvalid")
+
+    def setUp(self):
+        SongProperties.plugins = DummyPlugins()
+        self.watcher = SongWatcher()
+
+    def test_onesong(self):
+        self.window = SongProperties(self.watcher, [self.af1])
+
+    def test_twosong(self):
+        self.window = SongProperties(self.watcher, [self.af2, self.af1])
+
+    def test_changed(self):
+        self.test_twosong()
+        self.window.hide()
+        self.watcher.emit('changed', [self.af2])
+        while gtk.events_pending(): gtk.main_iteration()
+
+    def test_removed(self):
+        self.test_twosong()
+        self.window.hide()
+        self.watcher.emit('removed', [self.af2])
+        while gtk.events_pending(): gtk.main_iteration()
+
+    def tearDown(self):
+        try: self.window.destroy()
+        except AttributeError: pass
+        else: del(self.window)
+        self.watcher.destroy()
+        del(SongProperties.plugins)
+add(TSongProperties)
---
+++ quodlibet/tests/test_qltk_songsmenu.py
@@ -0,0 +1,77 @@
+from tests import add, TestCase
+from qltk.songsmenu import SongsMenu
+from qltk.watcher import SongWatcher
+from formats._audio import AudioFile
+
+class TSongsMenu(TestCase):
+    def setUp(self):
+        self.watcher = SongWatcher()
+        self.songs = [AudioFile({"title": x}) for x in
+                      ["song1", "song2", "song3"]]
+        for song in self.songs: song.sanitize(song["title"])
+
+    def test_empty(self):
+        self.menu = SongsMenu(self.watcher, self.songs, plugins=False,
+                              playlists=False, queue=False, remove=False,
+                              delete=False, edit=False)
+        self.failUnlessEqual(0, len(self.menu))
+
+    def test_simple(self):
+        self.menu = SongsMenu(self.watcher, self.songs, plugins=False)
+
+    def test_playlists(self):
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=True,
+            queue=False, remove=False, delete=False, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failUnless(self.menu.get_children()[0].props.sensitive)
+
+        self.songs[0].can_add = False
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=True,
+            queue=False, remove=False, delete=False, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failIf(self.menu.get_children()[0].props.sensitive)
+
+    def test_queue(self):
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=False,
+            queue=True, remove=False, delete=False, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failUnless(self.menu.get_children()[0].props.sensitive)
+
+        self.songs[0].can_add = False
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=False,
+            queue=True, remove=False, delete=False, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failIf(self.menu.get_children()[0].props.sensitive)
+
+    def test_remove(self):
+        # FIXME: Mock a fake library to test should-be-sensitive case.
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=False,
+            queue=False, remove=True, delete=False, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failIf(self.menu.get_children()[0].props.sensitive)
+
+    def test_delete(self):
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=False,
+            queue=False, remove=False, delete=True, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failUnless(self.menu.get_children()[0].props.sensitive)
+
+        self.songs[0].is_file = False
+        self.menu = SongsMenu(
+            self.watcher, self.songs, plugins=False, playlists=False,
+            queue=False, remove=False, delete=True, edit=False)
+        self.failUnlessEqual(1, len(self.menu))
+        self.failIf(self.menu.get_children()[0].props.sensitive)
+
+    def tearDown(self):
+        self.watcher.destroy()
+        try: self.menu.destroy()
+        except AttributeError: pass
+        else: del(self.menu)
+add(TSongsMenu)
