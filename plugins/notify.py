9db18c78c03c7e012c3ffefac7281c948a0e38e5
David Kågedal <davidk@lysator.liu.se>
2006-03-22 10:58:27 +0000

    Patch from Pablo Aranha: Added support for cover image rescaling, and enabled the cover image.

--- plugins/notify.py
+++ plugins/notify.py
@@ -1,15 +1,18 @@
 from parse import XMLFromPattern
 import dbus
+import gtk.gdk
+import const
+import os.path
 
-print "[notify] loading"
+# print "[notify] loading"
 
 class Notify(object):
     PLUGIN_NAME = "Notify"
     PLUGIN_DESC = "Display a notification when the song changes."
-    PLUGIN_VERSION = "0.2"
+    PLUGIN_VERSION = "0.3"
 
     def __init__(self):
-        print "[notify] connecting to D-Bus session bus"
+        # print "[notify] connecting to D-Bus session bus"
         bus = dbus.SessionBus()
         obj = bus.get_object(
             "org.freedesktop.Notifications",
@@ -20,7 +23,7 @@ class Notify(object):
     # david's default settings - this needs to be configurable
     class conf(object):
         timeout = 4000
-        show_icon = False
+        show_icon = True
         single_notification = False
         string = r'''<album|\<b\><album>\</b\><discnumber| - Disc <discnumber>><part| - \<b\><part>\</b\>><tracknumber| - <tracknumber>>
 >\<span weight='bold' size='large'\><title>\</span\> - <~length><version|
@@ -38,10 +41,26 @@ by <~people>>'''
 
         icon = ""
         if self.conf.show_icon:
-            # This should perhaps scale the icon somehow
             iconf = song.find_cover()
-            if iconf:
-                icon = iconf.name
+            if iconf is not None:
+                try:
+                    iconf = gtk.gdk.pixbuf_new_from_file_at_size(
+                        iconf.name, 46, 46)
+                except:
+                        print "[notify] failed to create small cover image"
+                else:
+                    # add a black outline, using the same style from AlbumList
+                    w, h = iconf.get_width(), iconf.get_height()
+                    newcover = gtk.gdk.Pixbuf(
+                        gtk.gdk.COLORSPACE_RGB, True, 8, w + 2, h + 2)
+                    newcover.fill(0x000000ff)
+                    iconf.copy_area(0, 0, w, h, newcover, 1, 1)
+                    try:
+                        coverart = os.path.join(const.DIR, "cover.png")
+                        newcover.save(coverart, "png", {})
+                        icon = coverart
+                    except:
+                        print "[notify] failed to save small cover image"
 
         if self.conf.single_notification and self.last_id is not None:
             last_id = self.last_id
