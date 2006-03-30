5edc4abcb68b1d52a6724da45cbbe55fa45fa0f2
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-03-30 06:38:52 +0000

    reset: Reset play data.

---
+++ plugins/songsmenu/reset.py
@@ -0,0 +1,21 @@
+# Copyright 2006 Joe Wreschnig
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+# $Id$
+
+from plugins.songsmenu import SongsMenuPlugin
+
+class ResetLibrary(SongsMenuPlugin):
+    PLUGIN_NAME = "Reset Library Data"
+    PLUGIN_VERSION = "1"
+    PLUGIN_DESC = "Reset ratings, play counts, skip counts, and play times."
+    PLUGIN_ICON = 'gtk-refresh'
+
+    def plugin_song(self, song):
+        song["~#rating"] = 0.5
+        for key in ["~#playcount", "~#skipcount", "~#lastplayed",
+                    "~#laststarted"]:
+            song[key] = 0
