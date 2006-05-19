f95d0fd5ef8045c8f0a3f6af74665daa11517b8e
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-05-19 00:39:34 +0000

    Iconv: Rewrite by Nikolai Prokoschenko; don't display codec names, avoid duplicate entries, add more codecs. (Closes: #648)

--- plugins/editing/iconv.py
+++ plugins/editing/iconv.py
@@ -10,10 +10,19 @@
 
 import gtk
 import util
+import locale
 from plugins.editing import EditTagsPlugin
 
-ENCODINGS = "Shift-JIS Big5 CP1251 EUC-KR EUC-JP UTF16BE UTF16LE ISO-2022-JP GB2312 EUC-CN EUC-TW GBK".split()
+ENCODINGS = """\
+big5 cp1250 cp1251 cp1252 cp1253 cp1254 cp1255 cp1256 cp1257 cp1258
+euc_jp euc_jis_2004 euc_jisx0213 euc_kr gb2312 gbk gb18030 iso2022_jp
+iso2022_kr iso8859_2 iso8859_3 iso8859_4 iso8859_5 iso8859_6 iso8859_7
+iso8859_8 iso8859_9 iso8859_10 iso8859_13 iso8859_14 iso8859_15 johab
+koi8_r koi8_u ptcp154 shift_jis utf_16_be utf_16_le""".split()
+
 if util.fscoding not in ENCODINGS + ["utf-8", "latin1"]:
+    ENCODINGS.append(util.fscoding)
+if locale.getpreferredencoding() not in ENCODINGS + ["utf-8", "latin1"]:
     ENCODINGS.append(util.fscoding)
 
 class Iconv(EditTagsPlugin):
@@ -22,36 +31,36 @@ class Iconv(EditTagsPlugin):
     PLUGIN_ICON = gtk.STOCK_CONVERT
 
     def __init__(self, tag, value):
-        super(Iconv, self).__init__("_Convert From...")
+        super(Iconv, self).__init__("_Convert Encoding...")
         self.set_image(
             gtk.image_new_from_stock(gtk.STOCK_CONVERT, gtk.ICON_SIZE_MENU))
         submenu = gtk.Menu()
-        sizes = gtk.SizeGroup(gtk.SIZE_GROUP_HORIZONTAL)
+
+        items = []
+
+        # Ok, which encodings do work on this string?
+
         for enc in ENCODINGS:
-            item = gtk.MenuItem()
-            try: new = value.encode('latin1').decode(enc, 'replace')
-            except UnicodeEncodeError:
-                new = _("[Invalid Encoding]")
-                item.set_sensitive(False)
-            except LookupError:
-                item.destroy()
-                continue
+            try:
+                new = value.encode('latin1').decode(enc)
+            except (UnicodeEncodeError, UnicodeDecodeError, LookupError):
+                   continue
             else:
-                if new == value: item.set_sensitive(False)
-                else:
-                    item.value = new
-                    item.connect('activate', self.__convert)
-            vb = gtk.HBox(spacing=3)
-            encoding = gtk.Label()
-            encoding.set_markup("<b>_%s:</b>" % enc)
-            encoding.set_alignment(0.0, 0.5)
-            sizes.add_widget(encoding)
-            result = gtk.Label(new)
-            result.set_alignment(0.0, 0.5)
-            vb.pack_start(encoding, expand=False)
-            vb.pack_start(result)
-            item.add(vb)
-            encoding.set_use_underline(True)
+                if new == value:
+                    continue
+                if not new in items:
+                    items.append(new)
+        
+        if not items:
+            self.set_sensitive(False)
+        
+        for i in items:
+            item = gtk.MenuItem()
+            item.value = i
+            item_label = gtk.Label(i)
+            item_label.set_alignment(0.0, 0.5)
+            item.add(item_label)
+            item.connect('activate', self.__convert)
             submenu.append(item)
         self.set_submenu(submenu)
 
