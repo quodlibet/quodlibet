19114bf164647bb3f475c49ce7a1bcbb0d08869f
Joe Wreschnig <joe.wreschnig@gmail.com>
2006-10-08 22:10:55 +0000

    QuodLibetWindow#__update_title: Just update whenever a song changes. (Closes: #808)

--- quodlibet/qltk/quodlibet.py
+++ quodlibet/qltk/quodlibet.py
@@ -567,11 +567,10 @@ class QuodLibetWindow(gtk.Window):
                 self.__set_time()
 
     def __update_title(self, player, songs):
-        if player.song in songs:
-            song = player.info
-            if song:
-                self.set_title("Quod Libet - " + song.comma("~title~version"))
-            else: self.set_title("Quod Libet")
+        song = player.info
+        if song:
+            self.set_title("Quod Libet - " + song.comma("~title~version"))
+        else: self.set_title("Quod Libet")
 
     def __song_started(self, player, song):
         if song is None:
