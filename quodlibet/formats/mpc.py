624611b191339b045e16805b7ecc9ea2c13b16e4
Joe Wreschnig <joe.wreschnig@gmail.com>
2007-04-26 16:52:10 +0000

    MPCFile: Allow APEv2 tags to override Musepack stream settings.

--- quodlibet/formats/mpc.py
+++ quodlibet/formats/mpc.py
@@ -24,10 +24,6 @@ else:
 class MPCFile(APEv2File):
     format = "Musepack"
 
-    IGNORE = APEv2File.IGNORE + [
-        "replaygain_track_peak", "replaygain_album_peak",
-        "replaygain_track_gain", "replaygain_album_gain"]
-
     def __init__(self, filename):
         audio = Musepack(filename)
         super(MPCFile, self).__init__(filename, audio)
@@ -35,14 +31,18 @@ class MPCFile(APEv2File):
         self["~#bitrate"] = int(audio.info.bitrate)
 
         try:
-            track_g = u"%+0.2f dB" % audio.info.title_gain
-            album_g = u"%+0.2f dB" % audio.info.album_gain
-            track_p = unicode(audio.info.title_peak * 2)
-            album_p = unicode(audio.info.album_peak * 2)
-            self["replaygain_track_gain"] = track_g
-            self["replaygain_track_peak"] = track_p
-            self["replaygain_album_gain"] = album_g
-            self["replaygain_album_peak"] = album_p
+            if audio.info.title_gain:
+                track_g = u"%+0.2f dB" % audio.info.title_gain
+                self.setdefault("replaygain_track_gain", track_g)
+            if audio.info.album_gain:
+                album_g = u"%+0.2f dB" % audio.info.album_gain
+                self.setdefault("replaygain_album_gain", album_g)
+            if audio.info.title_peak:
+                track_p = unicode(audio.info.title_peak * 2)
+                self.setdefault("replaygain_album_peak", album_p)
+            if audio.info.album_peak:
+                album_p = unicode(audio.info.album_peak * 2)
+                self.setdefault("replaygain_track_peak", track_p)
         except AttributeError:
             pass
 
