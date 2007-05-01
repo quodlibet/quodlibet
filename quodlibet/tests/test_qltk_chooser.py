3fb0812e94515f0b42b28f0b2a6e8b28df78a60e
Joe Wreschnig <joe.wreschnig@gmail.com>
2007-05-01 02:37:51 +0000

    TFolderChooser: GTK+ devs say that you can't actually get a set filename from a file selector.

--- quodlibet/tests/test_qltk_chooser.py
+++ quodlibet/tests/test_qltk_chooser.py
@@ -7,12 +7,10 @@ class TFolderChooser(TestCase):
     Kind = FolderChooser
     def test_init_nodir(self):
         f = self.Kind(None, "A file chooser")
-        self.assertEqual(f.get_current_folder(), os.path.realpath("."))
         f.destroy()
 
     def test_init_dir(self):
         f = self.Kind(None, "A file chooser", initial_dir="/home")
-        self.assertEqual(f.get_current_folder(), "/home")
         f.destroy()
 add(TFolderChooser)
 
