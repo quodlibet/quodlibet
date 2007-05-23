965f3dacaef340ac18e9cc080b6734dd8889d027
Joe Wreschnig <joe.wreschnig@gmail.com>
2007-05-23 07:14:46 +0000

    SongsMenu#__enqueue: Fix import.

--- quodlibet/quodlibet/qltk/songsmenu.py
+++ quodlibet/quodlibet/qltk/songsmenu.py
@@ -135,7 +135,7 @@ class SongsMenu(gtk.Menu):
     def __enqueue(self, item, songs):
         songs = filter(lambda s: s.can_add, songs)
         if songs:
-            from widgets import main
+            from quodlibet.widgets import main
             main.playlist.enqueue(songs)
 
     def __delete(self, item, songs, library):
