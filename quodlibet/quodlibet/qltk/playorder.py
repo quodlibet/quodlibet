3f8e0a2a32cba1c65f3acf4414c011dfd6c54940
Joe Wreschnig <joe.wreschnig@gmail.com>
2007-08-19 09:01:15 +0000

    First pass skeleton for playlist order plugins; convert InOrder and OneSong to subclasses of new 'Order' type.

--- quodlibet/quodlibet/qltk/playorder.py
+++ quodlibet/quodlibet/qltk/playorder.py
@@ -19,11 +19,117 @@ _TRANS = {"inorder": _("In Order"),
 
 # Canonical accelerated versions, in case we need them (e.g. the tray
 # icon uses them right now).
-
-_("_In Order")
 _("_Shuffle")
 _("_Weighted")
-_("_One Song")
+
+class Order(object):
+    name = "unknown_order"
+    display_name = _("Unknown")
+
+    def __init__(self, playlist, iter):
+        self.playlist = playlist
+
+    # Not called directly, but the default implementation of
+    # next_explicit and next_implicit both just call this.
+    def next(self, playlist, iter):
+        raise NotImplementedError
+
+    # Not called directly, but the default implementation of
+    # previous_explicit calls this. Right now there is no such thing
+    # as previous_implicit.
+    def previous(self, playlist, iter):
+        raise NotImplementedError
+
+    # Not called directly, but the default implementation of
+    # set_explicit calls this. Right now there is no such thing as
+    # set_implicit.
+    def set(self, playlist, iter):
+        return iter
+
+    # Called when the user presses a "Next" button.
+    def next_explicit(self, playlist, iter):
+        return self.next(playlist, iter)
+
+    # Called when a song ends passively, e.g. it runs out of time, or
+    # the file could not be found. By default, do the same thing as an
+    # explicit end.
+    def next_implicit(self, playlist, iter):
+        return self.next(playlist, iter)
+
+    # Called when the user presses a "Previous" button.
+    def previous_explicit(self, playlist, iter):
+        return self.previous(playlist, iter)
+
+    # Called when the user manually selects a song (at iter).
+    # If desired the play order can override that, or just
+    # log it and return the iter again.
+    def set_explicit(self, playlist, iter):
+        return self.set(playlist, iter)
+
+    def reset(self, playlist):
+        pass
+
+class OrderInOrder(Order):
+    name = "inorder"
+    display_name = _("In Order")
+    accelerated_name = _("_In Order")
+
+    def next(self, playlist, iter):
+        if iter is None:
+            return playlist.get_iter_first()
+        else:
+            next = playlist.iter_next(iter)
+            if next is None and playlist.repeat:
+                next = self.get_iter_first()
+            return next
+
+    def previous(self, playlist, iter):
+        if len(playlist) == 0:
+            return None
+        elif iter is None:
+            return playlist[(len(playlist) - 1,)].iter
+        else:
+            path = playlist.get_path(iter)[0]
+            try: return playlist.get_iter((path - 1,))
+            except ValueError:
+                if playlist.repeat:
+                    return playlist[(len(playlist) - 1,)].iter
+        return None
+
+class OrderOneSong(OrderInOrder):
+    name = "onesong"
+    display_name = _("One Song")
+    accelerated_name = _("_One Song")
+
+    def next_implicit(self, playlist, iter):
+        if playlist.repeat:
+            return iter
+        else:
+            return None
+
+class OrderRemembered(Order):
+    # Shared class for all the shuffle modes that keep a memory
+    # of their previously played songs.
+
+    # A list of previously played paths.
+    _played = []
+
+    def next(self, playlist, iter):
+        if iter is not None:
+            self._played.append(playlist.get_path(iter)[0])
+
+    def previous(self, playlist, iter):
+        try: path = self._played.pop()
+        except IndexError: return None
+        else: return playlist.get_iter(path)
+
+    def set(self, playlist, iter):
+        if iter is not None:
+            self._played.append(playlist.get_path(iter)[0])
+        return iter
+
+    def reset(self):
+        del(self.played[:])
 
 class PlayOrder(gtk.ComboBox):
     def __init__(self, model, player):
