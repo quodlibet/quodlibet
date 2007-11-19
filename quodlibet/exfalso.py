2e22aa0310a78ba94536b0991ea7df1cfb71350d
Joe Wreschnig <joe.wreschnig@gmail.com>
2007-11-19 06:36:13 +0000

    exfalso: Add nullbe backend for audio playback, use that instead of the lame fakegst hack.

--- quodlibet/exfalso.py
+++ quodlibet/exfalso.py
@@ -11,24 +11,6 @@
 import os
 import sys
 
-class fakegst(object):
-    URI_SRC = 0
-    PluginNotFoundError = ValueError
-
-    def element_factory_make(self, element_name):
-        if element_name in ["monkeysdec", "mikmod", "modplug", "wavparse",
-                            "spcdec"]:
-            raise self.PluginNotFoundError("unsupported fake module")
-
-    def element_make_from_uri(self, type_, uri, arg):
-        return None
-
-    def registry_get_default(self):
-        return self
-
-    def find_plugin(self, plugin):
-        return plugin not in ["wavparse", "modplug"]
-
 def main(argv):
     import quodlibet
     from quodlibet import util
@@ -38,18 +20,15 @@ def main(argv):
         "Ex Falso", const.VERSION,
         _("an audio tag editor"), "[%s]" % _("directory"))
 
-    from quodlibet import config
-    config.init(const.CONFIG)
-
-    backend, library, player = quodlibet.init(icon="exfalso")
-
-    sys.modules["gst"] = fakegst()
-
     sys.argv.append(os.path.abspath("."))
     opts, args = opts.parse()
     args[0] = os.path.realpath(args[0])
+
+    from quodlibet import config
+    config.init(const.CONFIG)
+    backend, library, player = quodlibet.init(icon="exfalso", player="nullbe")
+
     from quodlibet.qltk.exfalsowindow import ExFalsoWindow
-    from quodlibet.library import SongFileLibrary
     w = ExFalsoWindow(library, args[0])
     quodlibet.main(w)
     config.write(const.CONFIG)
---
+++ quodlibet/quodlibet/player/nullbe.py
@@ -0,0 +1,82 @@
+# Copyright 2004 Joe Wreschnig, Michael Urman
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+# $Id$
+
+import gobject
+
+from quodlibet import config
+from quodlibet import const
+
+from quodlibet.util import fver
+from quodlibet.player import error as PlayerError
+from quodlibet.player._base import BasePlayer
+
+class NullPlayer(BasePlayer):
+    __gproperties__ = BasePlayer._gproperties_
+    __gsignals__ = BasePlayer._gsignals_
+    version_info = "Null Player Backend"
+
+    def __init__(self, sinkname, librarian=None):
+        super(NullPlayer, self).__init__()
+        self._set_paused(True)
+
+    def get_position(self):
+        """Return the current playback position in milliseconds,
+        or 0 if no song is playing."""
+        return 0
+        
+    def _set_paused(self, paused):
+        if paused != self._paused:
+            self._paused = paused
+            if self.song:
+                self.emit((paused and 'paused') or 'unpaused')
+            elif paused is True:
+                # Something wants us to pause between songs, or when
+                # we've got no song playing (probably StopAfterMenu).
+                self.emit('paused')
+    def _get_paused(self): return self._paused
+    paused = property(_get_paused, _set_paused)
+
+    def do_set_property(self, property, v):
+        if property.name == 'volume':
+            self._volume = v
+        else:
+            raise AttributeError
+
+    def error(self, message, lock):
+        self.emit('error', self.song, message, lock)
+        if not self.paused:
+            self.next()
+
+    def seek(self, pos):
+        """Seek to a position in the song, in milliseconds."""
+        if self.song:
+            self.emit('seek', self.song, pos)
+
+    def _end(self, stopped):
+        # We need to set self.song to None before calling our signal
+        # handlers. Otherwise, if they try to end the song they're given
+        # (e.g. by removing it), then we get in an infinite loop.
+        song = self.song
+        self.song = self.info = None
+        self.emit('song-ended', song, stopped)
+
+        # Then, set up the next song.
+        self.song = self.info = self._source.current
+        self.emit('song-started', self.song)
+
+        if self.song is not None:
+            self._length = self.song["~#length"] * 1000
+        else:
+            self.paused = True
+
+def can_play_uri(uri):
+    return False
+
+def init(librarian):
+    return NullPlayer(librarian)
+
