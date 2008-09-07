339319bb2231a6a2bd1604bf0c1d451c000510e1
Joe Wreschnig <joe.wreschnig@gmail.com>
2008-09-07 10:11:56 +0000

    Port pluginwin.

--- quodlibet/tests/test_qltk_pluginwin.py
+++ quodlibet/tests/test_qltk_pluginwin.py
@@ -1,9 +1,11 @@
 from tests import TestCase, add
 
 from quodlibet.qltk.pluginwin import PluginWindow
+import quodlibet.config
 
 class TPluginWindow(TestCase):
     def setUp(self):
+        quodlibet.config.init()
         self.win = PluginWindow(None)
 
     def test_ctr(self):
@@ -11,4 +13,5 @@ class TPluginWindow(TestCase):
 
     def tearDown(self):
         self.win.destroy()
+        quodlibet.config.quit()
 add(TPluginWindow)
