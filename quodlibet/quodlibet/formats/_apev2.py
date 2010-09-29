aff651252771b6c695ab4c6180f91d06fc40996e
Christoph Reiter <reiter.christoph@gmail.com>
2010-09-29 16:03:51 +0200

    Add APEv2 tests and fix a bunch of bugs.
    Merge different cased tags; Don't allow invalid tag keys; Don't delete ignored tags.

--- quodlibet/quodlibet/formats/_apev2.py
+++ quodlibet/quodlibet/formats/_apev2.py
@@ -38,28 +38,46 @@ class APEv2File(AudioFile):
                 key not in self.IGNORE):
                 self[key] = "\n".join(list(value))
 
+        self.sanitize(filename)
+
+    @staticmethod
+    def __titlecase(key):
+        if key.lower() in ["isrc", "isbn", "ean/upc"]: return key.upper()
+        else: return key.title()
+
     def can_change(self, key=None):
         if key is None: return True
         else: return (super(APEv2File, self).can_change(key) and
-                      key not in self.IGNORE and key not in self.TRANS)
+                      key.lower() not in self.IGNORE and
+                      key.lower() not in self.TRANS and
+                      mutagen.apev2.is_valid_apev2_key(self.__titlecase(key)))
 
     def write(self):
         try: tag = mutagen.apev2.APEv2(self['~filename'])
         except mutagen.apev2.APENoHeaderError:
             tag = mutagen.apev2.APEv2()
 
-        keys = tag.keys()
-        for key in keys:
-            # remove any text keys we read in
+        # Remove any text keys we read in
+        for key in tag.iterkeys():
             value = tag[key]
-            if (value.kind == mutagen.apev2.TEXT and key not in self.IGNORE):
+            if (value.kind == mutagen.apev2.TEXT and
+                key.lower() not in self.IGNORE):
                 del(tag[key])
+
+        # Merge all case differing keys together and titlecase them
         for key in self.realkeys():
-            if key in self.IGNORE: continue
             value = self[key]
-            key = self.SNART.get(key, key)
-            if key in ["isrc", "isbn", "ean/upc"]: key = key.upper()
-            else: key = key.title()
-            tag[key] = value.split("\n")
+            key = self.SNART.get(key.lower(), key.lower())
+            if key in self.IGNORE: continue
+            key = self.__titlecase(key)
+            if key in tag:
+                old_val = tag[key]
+                if old_val.kind == mutagen.apev2.TEXT:
+                    tag[key] = list(old_val) + value.split("\n")
+                else: # FIXME: We overwrite data here.
+                    tag[key] = value.split("\n")
+            else:
+                tag[key] = value.split("\n")
+
         tag.save(self["~filename"])
         self.sanitize()
---
+++ quodlibet/tests/test_formats_apev2.py
@@ -0,0 +1,81 @@
+from tests import TestCase, add
+
+import os
+import shutil
+import tempfile
+
+import mutagen
+
+from mutagen.apev2 import BINARY, APEValue
+from quodlibet.formats._apev2 import APEv2File
+
+class TAPEv2File(TestCase):
+    def setUp(self):
+        self.f = tempfile.mkstemp(".mpc")[1]
+        shutil.copy(os.path.join('tests', 'data', 'silence-44-s.mpc'), self.f)
+        self.s = APEv2File(self.f)
+
+    def test_can_change(self):
+        self.failUnlessEqual(self.s.can_change(), True)
+        self.failUnlessEqual(self.s.can_change("~"), False)
+        self.failUnlessEqual(self.s.can_change("a"), False)
+        self.failUnlessEqual(self.s.can_change("OggS"), True)
+        self.failUnlessEqual(self.s.can_change("\xc3\xa4\xc3\xb6"), False)
+        self.failUnlessEqual(self.s.can_change("sUbtitle"), False)
+        self.failUnlessEqual(self.s.can_change("indeX"), False)
+        self.failUnlessEqual(self.s.can_change("yEar"), False)
+
+    def test_trans_keys(self):
+        self.s["date"] = "2010"
+        self.s.write()
+        m = mutagen.apev2.APEv2(self.f)
+        self.failUnlessEqual(m["Year"], "2010")
+        m["yEar"] = "2011"
+        m.save()
+        self.s.reload()
+        self.failUnlessEqual(self.s["date"], "2011")
+
+    def test_ignore(self):
+        for tag in ["inDex", "index"]:
+            m = mutagen.apev2.APEv2(self.f)
+            m[tag] = "foobar"
+            m.save()
+            self.s.reload()
+            self.failUnlessEqual(self.s.get(tag), None)
+            m = mutagen.apev2.APEv2(self.f)
+            self.failUnlessEqual(m[tag], "foobar")
+
+    def test_multi_case(self):
+        self.s["AA"] = "B"
+        self.s["aa"] = "C"
+        self.s["BB"] = "D"
+        self.s["Aa"] = "E"
+        self.s.write()
+        self.s.reload()
+        self.failUnlessEqual(set(self.s["aa"].split()), set(["C", "B", "E"]))
+
+    def test_binary_ignore(self):
+        m = mutagen.apev2.APEv2(self.f)
+        m["foo"] = APEValue("bar", BINARY)
+        m.save()
+        self.s.reload()
+        self.failUnlessEqual(self.s.get("foo"), None)
+        self.s.write()
+        m = mutagen.apev2.APEv2(self.f)
+        self.failUnlessEqual("foo" in m, True)
+
+    def test_titlecase(self):
+        self.s["isRc"] = "1234"
+        self.s["fOoBaR"] = "5678"
+        self.s.write()
+        self.s.reload()
+        self.failUnlessEqual("isrc" in self.s, True)
+        self.failUnlessEqual("foobar" in self.s, True)
+        m = mutagen.apev2.APEv2(self.f)
+        self.failUnlessEqual("ISRC" in m, True)
+        self.failUnlessEqual("Foobar" in m, True)
+
+    def tearDown(self):
+        os.unlink(self.f)
+
+add(TAPEv2File)
