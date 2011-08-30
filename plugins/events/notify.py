e2cd1fde32f40c76e3043301058113264d4416af
Christoph Reiter <reiter.christoph@gmail.com>
2011-08-30 22:01:50 +0200

    Clean up notification plugin preferences.

--- plugins/events/notify.py
+++ plugins/events/notify.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2010 Felix Krull <f_krull@gmx.de>
+#               2011 Christoph Reiter <christoph.reiter@gmx.at>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
@@ -13,7 +14,6 @@
 
 from xml.sax.saxutils import unescape
 import os.path
-import tempfile
 
 import dbus
 import gtk
@@ -23,7 +23,8 @@ from quodlibet.plugins.events import EventPlugin
 from quodlibet.parse import XMLFromPattern
 from quodlibet.widgets import main as qlmainwindow
 from quodlibet.player import playlist as qlplayer
-
+from quodlibet.qltk.textedit import TextView, TextBuffer
+from quodlibet.qltk.entry import UndoEntry
 
 # configuration stuff
 DEFAULT_CONFIG = {
@@ -51,39 +52,41 @@ get_conf_int = lambda name: get_conf_value(name, "getint")
 def set_conf_value(name, value):
     config.set("plugins", "notify_%s" % name, unicode(value))
 
-
-class PaddedWidget(gtk.Alignment):
-    def __init__(self, child, top=0, bottom=0, left=0, right=0):
-        gtk.Alignment.__init__(self)
-        self.add(child)
-        self.set_padding(top, bottom, left, right)
-
-
 class PreferencesWidget(gtk.VBox):
     def __init__(self, parent, plugin_instance):
         gtk.VBox.__init__(self, spacing=12)
-        self.set_border_width(6)
         self.plugin_instance = plugin_instance
 
         # notification text settings
-        text_box = gtk.VBox(spacing=3)
-        text_frame = qltk.Frame(_("Notification text"), child=text_box)
+        table = gtk.Table(2, 3)
+        table.set_col_spacings(6)
+        table.set_row_spacings(6)
 
-        title_label = gtk.Label(_("Notification title:"))
-        title_label.set_alignment(0, 0.1)
-        text_box.pack_start(PaddedWidget(title_label, top=3, left=3))
+        text_frame = qltk.Frame(_("Notification text"), child=table)
 
-        title_entry = gtk.Entry()
+        title_entry = UndoEntry()
         title_entry.set_text(get_conf_value("titlepattern"))
         title_entry.connect("focus-out-event", self.on_entry_unfocused,
                             "titlepattern")
-        text_box.pack_start(title_entry)
+        table.attach(title_entry, 1, 2, 0, 1)
 
-        body_label = gtk.Label(_("Notification body:"))
-        body_label.set_alignment(0, 0.1)
-        text_box.pack_start(PaddedWidget(body_label, top=3, left=3))
+        title_label = gtk.Label(_("_Title:"))
+        title_label.set_use_underline(True)
+        title_label.set_alignment(0, 0.5)
+        title_label.set_mnemonic_widget(title_entry)
+        table.attach(title_label, 0, 1, 0, 1, xoptions=gtk.FILL | gtk.SHRINK)
 
-        body_textview = gtk.TextView()
+        title_revert = gtk.Button()
+        title_revert.add(gtk.image_new_from_stock(
+            gtk.STOCK_REVERT_TO_SAVED, gtk.ICON_SIZE_MENU))
+        title_revert.set_tooltip_text(_("Revert to default pattern"))
+        title_revert.connect_object(
+            "clicked", title_entry.set_text, DEFAULT_CONFIG["titlepattern"])
+        table.attach(title_revert, 2, 3, 0, 1, xoptions=gtk.SHRINK)
+
+        body_textbuffer = TextBuffer()
+        body_textview = TextView(body_textbuffer)
+        body_textview.set_size_request(-1, 85)
         body_textview.get_buffer().set_text(get_conf_value("bodypattern"))
         body_textview.connect("focus-out-event", self.on_textview_unfocused,
                               "bodypattern")
@@ -91,34 +94,74 @@ class PreferencesWidget(gtk.VBox):
         body_scrollarea.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         body_scrollarea.set_shadow_type(gtk.SHADOW_ETCHED_OUT)
         body_scrollarea.add(body_textview)
-        text_box.pack_start(body_scrollarea)
+        table.attach(body_scrollarea, 1, 2, 1, 2)
+
+        body_label = gtk.Label(_("_Body:"))
+        body_label.set_padding(0, 3)
+        body_label.set_use_underline(True)
+        body_label.set_alignment(0, 0)
+        body_label.set_mnemonic_widget(body_textview)
+        table.attach(body_label, 0, 1, 1, 2, xoptions=gtk.SHRINK)
+
+        revert_align = gtk.Alignment()
+        body_revert = gtk.Button()
+        body_revert.add(gtk.image_new_from_stock(
+                        gtk.STOCK_REVERT_TO_SAVED, gtk.ICON_SIZE_MENU))
+        body_revert.set_tooltip_text(_("Revert to default pattern"))
+        body_revert.connect_object(
+            "clicked", body_textbuffer.set_text, DEFAULT_CONFIG["bodypattern"])
+        revert_align.add(body_revert)
+        table.attach(
+            revert_align, 2, 3, 1, 2,
+            xoptions=gtk.SHRINK, yoptions=gtk.FILL | gtk.SHRINK)
+
+        # preview button
+        preview_button = qltk.Button(
+            _("_Show notification now"), gtk.STOCK_EXECUTE)
+        preview_button.set_sensitive(qlplayer.info is not None)
+        preview_button.connect("clicked", self.on_preview_button_clicked)
+        self.qlplayer_connected_signals = [
+            qlplayer.connect("paused", self.on_player_state_changed,
+                             preview_button),
+            qlplayer.connect("unpaused", self.on_player_state_changed,
+                             preview_button),
+        ]
+
+        table.attach(
+            preview_button, 0, 3, 2, 3, xoptions=gtk.FILL | gtk.SHRINK)
 
         self.pack_start(text_frame)
 
         # notification display settings
-        display_box = gtk.VBox()
+        display_box = gtk.VBox(spacing=12)
         display_frame = qltk.Frame(_("Show notifications"), child=display_box)
 
+        radio_box = gtk.VBox(spacing=6)
+        display_box.pack_start(radio_box)
+
         only_user_radio = gtk.RadioButton(label=_(
-            "Only on manual song changes"
+            "Only on <i>_manual</i> song changes"
         ))
+        only_user_radio.child.set_use_markup(True)
         only_user_radio.connect("toggled", self.on_radiobutton_toggled,
                                 "show_notifications", "user")
-        display_box.pack_start(only_user_radio)
+        radio_box.pack_start(only_user_radio)
 
         only_auto_radio = gtk.RadioButton(only_user_radio, label=_(
-            "Only on automatic song changes"
+            "Only on <i>_automatic</i> song changes"
         ))
+        only_auto_radio.child.set_use_markup(True)
         only_auto_radio.connect("toggled", self.on_radiobutton_toggled,
                                 "show_notifications", "auto")
-        display_box.pack_start(only_auto_radio)
+        radio_box.pack_start(only_auto_radio)
 
         all_radio = gtk.RadioButton(only_user_radio, label=_(
-            "On all song changes"
+            "On <i>a_ll</i> song changes"
         ))
+        all_radio.child.set_use_markup(True)
         all_radio.connect("toggled", self.on_radiobutton_toggled,
                           "show_notifications", "all")
-        display_box.pack_start(all_radio)
+        radio_box.pack_start(all_radio)
 
         try:
             {
@@ -131,26 +174,13 @@ class PreferencesWidget(gtk.VBox):
             set_conf_value("show_notifications", "all")
 
         focus_check = gtk.CheckButton(_("Only when the main window is not "
-                                        "focused"))
+                                        "_focused"))
         focus_check.set_active(get_conf_bool("show_only_when_unfocused"))
         focus_check.connect("toggled", self.on_checkbutton_toggled,
                             "show_only_when_unfocused")
-        display_box.pack_start(PaddedWidget(focus_check, top=6))
+        display_box.pack_start(focus_check)
 
         self.pack_start(display_frame)
-
-        # preview button
-        preview_button = gtk.Button(_("Show notification now"))
-        preview_button.set_sensitive(qlplayer.info is not None)
-        preview_button.connect("clicked", self.on_preview_button_clicked)
-        self.qlplayer_connected_signals = [
-            qlplayer.connect("paused", self.on_player_state_changed,
-                             preview_button),
-            qlplayer.connect("unpaused", self.on_player_state_changed,
-                             preview_button),
-        ]
-
-        self.pack_start(preview_button)
 
         self.show_all()
         self.connect("destroy", self.on_destroyed)
@@ -183,12 +213,14 @@ class PreferencesWidget(gtk.VBox):
         for sig in self.qlplayer_connected_signals:
             qlplayer.disconnect(sig)
         self.qlplayer_connected_signals = []
+        self.plugin_instance = None
 
 
 class Notify(EventPlugin):
     PLUGIN_ID = "Notify"
-    PLUGIN_NAME = _("Notify")
+    PLUGIN_NAME = _("Song Notifications")
     PLUGIN_DESC = _("Display a notification when the song changes.")
+    PLUGIN_ICON = gtk.STOCK_DIALOG_INFO
     PLUGIN_VERSION = "1.0"
 
     # quirks
