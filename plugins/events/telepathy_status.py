dd9eb53b15dae2e5948a47decc0755fc0d939d4e
Christoph Reiter <reiter.christoph@gmail.com>
2012-07-19 00:52:22 +0200

    Get rid of the gir bindings... mixing them with the static ones wont work.
    Use the dbus interface directly instead.

--- plugins/events/telepathy_status.py
+++ plugins/events/telepathy_status.py
@@ -9,9 +9,8 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
 
+import dbus
 import gtk
-from gi.repository import TelepathyGLib as TGL
-from gi.repository import GObject
 
 from quodlibet.parse._pattern import Pattern
 from quodlibet.qltk.entry import UndoEntry
@@ -21,6 +20,32 @@ from quodlibet import util
 from quodlibet import config
 from quodlibet.plugins.events import EventPlugin
 from quodlibet.plugins import PluginManager as PM
+
+
+AM_PATH = "/org/freedesktop/Telepathy/AccountManager"
+AM_NAME = "org.freedesktop.Telepathy.AccountManager"
+AM_IFACE = "org.freedesktop.Telepathy.AccountManager"
+AC_IFACE = "org.freedesktop.Telepathy.Account"
+PROPS_IFACE = "org.freedesktop.DBus.Properties"
+CONN_PRESENCE_TYPE_AVAILABLE = 2
+
+
+def get_active_account_paths():
+    bus = dbus.SessionBus()
+    bus_object = bus.get_object(AM_NAME, AM_PATH)
+    bus_iface = dbus.Interface(bus_object, dbus_interface=PROPS_IFACE)
+    return bus_iface.Get(AM_IFACE, "ValidAccounts")
+
+
+def set_accounts_requested_presence(paths, status, message):
+    bus = dbus.SessionBus()
+    for path in paths:
+        bus_object = bus.get_object(AM_NAME, path)
+        bus_iface = dbus.Interface(bus_object, dbus_interface=PROPS_IFACE)
+        presence_type = dbus.UInt32(CONN_PRESENCE_TYPE_AVAILABLE)
+        value = dbus.Struct([presence_type, status, message])
+        bus_iface.Set(AC_IFACE, "RequestedPresence", value)
+
 
 class PluginConfigMixin(object):
     """Mixin for storage and editing of plugin config in a standard way"""
@@ -73,8 +98,12 @@ class TelepathyStatusPlugin(EventPlugin, PluginConfigMixin):
     def _set_status(self, text):
         print_d("Setting status to \"%s\"" % text)
         self.status = text
-        self.acct_manager.set_all_requested_presences(
-            TGL.ConnectionPresenceType.AVAILABLE, 'available', text)
+        try:
+            accounts = get_active_account_paths()
+            set_accounts_requested_presence(accounts, "available", text)
+        except dbus.DBusException:
+            print_d("setting failed")
+            util.print_exc()
 
     def plugin_on_song_started(self, song):
         self.song = song
@@ -93,17 +122,14 @@ class TelepathyStatusPlugin(EventPlugin, PluginConfigMixin):
     def plugin_on_unpaused(self):
         self.plugin_on_song_started(self.song)
 
-    def on_quit(self):
-        if self.status: self._set_status(self.cfg_get(self.CFG_STATUS_SONGLESS))
+    def disabled(self):
+        if self.status:
+            self._set_status(self.cfg_get(self.CFG_STATUS_SONGLESS))
 
     def enabled(self):
         print_d("Setting up Telepathy hooks...")
         self.song = None
         self.status = ""
-        am = self.acct_manager = TGL.AccountManager.dup()
-        loop = GObject.MainLoop()
-        am.prepare_async(None, lambda *args: loop.quit(), None)
-        gtk.quit_add(0, self.on_quit)
 
     def PluginPreferences(self, parent):
         outer_vb = gtk.VBox(spacing=12)
