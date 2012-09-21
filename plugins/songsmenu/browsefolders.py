b36af85a7ef5ba20e004615ce975ae0dd92be052
Nick B <nick.boultbee@gmail.com>
2012-09-22 00:32:38 +0100

    Tweaks to browse folders plugin: tidied up logging, fixed typo, and refactored common functionality out into a mixin.

--- plugins/songsmenu/browsefolders.py
+++ plugins/songsmenu/browsefolders.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Christoph Reiter
+# Copyright 2012 Christoph Reiter, Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -21,6 +21,7 @@ except ImportError:
 from quodlibet.plugins.songsmenu import SongsMenuPlugin
 from quodlibet.util.uri import URI
 from quodlibet.qltk.msg import ErrorMessage
+from quodlibet.util.dprint import print_d
 
 
 def get_startup_id():
@@ -105,77 +106,70 @@ def browse_files_win_explorer(songs):
         subprocess.call(["Explorer", "/select,", song("~filename")])
 
 
-class BrowseFolters(SongsMenuPlugin):
+class HandlingMixin(object):
+    def plugin_handles(self, songs):
+        # By default, any single song being a file is good enough
+        for song in songs:
+            if song.is_file:
+                return True
+        return False
+
+    def handle(self, songs):
+        """
+        Uses the first successful handler in callable list `_HANDLERS`
+        to handle `songs`
+        Returns False if none could be used
+        """
+        if not hasattr(self, "_HANDLERS"): return False
+        for handler in self._HANDLERS:
+            name = handler.__name__
+            try:
+                print_d("Trying %r..." % name)
+                handler(songs)
+            except (dbus.DBusException, EnvironmentError):
+                print_d("...failed.")
+                # TODO: caching of failures (re-order list maybe)
+            else:
+                print_d("...success!")
+                return True
+        print_d("No handlers could be used." )
+        return False
+
+
+class BrowseFolders(SongsMenuPlugin, HandlingMixin):
     PLUGIN_ID = 'Browse Folders'
     PLUGIN_NAME = _('Browse Folders')
     PLUGIN_DESC = "View the songs' folders in a file manager"
     PLUGIN_ICON = gtk.STOCK_OPEN
-    PLUGIN_VERSION = '1'
+    PLUGIN_VERSION = '1.1'
 
     _HANDLERS = [browse_folders_fdo, browse_folders_thunar,
                  browse_folders_xdg_open, browse_folders_gnome_open,
                  browse_folders_win_explorer]
 
-    def plugin_handles(self, songs):
-        handle = False
-        for song in songs:
-            if song.is_file:
-                return True
-        return handle
-
     def plugin_songs(self, songs):
         songs = [s for s in songs if s.is_file]
-        print_d("Try to browse folders")
-        for handler in self._HANDLERS:
-            name = handler.__name__
-            try:
-                print_d("Try %r" % name)
-                handler(songs)
-            except (dbus.DBusException, EnvironmentError):
-                print_d("%r failed" % name)
-                pass
-            else:
-                print_d("OK")
-                return
-
-        ErrorMessage(self.plugin_window,
-                     _("Unable to open folders"),
-                     _("No program available to open folders.")).run()
+        print_d("Trying to browse folders...")
+        if not self.handle(songs):
+            ErrorMessage(self.plugin_window,
+                         _("Unable to open folders"),
+                         _("No program available to open folders.")).run()
 
 
-class BrowseFiles(SongsMenuPlugin):
+class BrowseFiles(SongsMenuPlugin, HandlingMixin):
     PLUGIN_ID = 'Browse Files'
     PLUGIN_NAME = _('Show File')
     PLUGIN_DESC = "View the song's file in a file manager"
     PLUGIN_ICON = gtk.STOCK_OPEN
-    PLUGIN_VERSION = '1'
+    PLUGIN_VERSION = '1.1'
 
     _HANDLERS = [browse_files_fdo, browse_files_thunar,
                  browse_files_win_explorer]
 
-    def plugin_handles(self, songs):
-        handle = False
-        for song in songs:
-            if song.is_file:
-                return True
-        return handle
-
     def plugin_single_song(self, song):
         songs = [s for s in [song] if s.is_file]
-
-        print_d("Try to browse files")
-        for handler in self._HANDLERS:
-            name = handler.__name__
-            try:
-                print_d("Try %r" % name)
-                handler(songs)
-            except (dbus.DBusException, EnvironmentError):
-                print_d("%r failed" % name)
-                pass
-            else:
-                print_d("OK")
-                return
-
-        ErrorMessage(self.plugin_window,
-                     _("Unable to browse files"),
-                     _("No program available to browse files.")).run()
+        print_d("Trying to browse files...")
+        if not self.handle(songs):
+            ErrorMessage(self.plugin_window,
+                         _("Unable to browse files"),
+                         _("No program available to browse files.")).run()
