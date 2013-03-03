93a12cce520e2860cae3caa18877fbd6eae93eb8
Christoph Reiter <reiter.christoph@gmail.com>
2013-03-03 11:45:13 +0100

    pep8

--- quodlibet/quodlibet/parse/_pattern.py
+++ quodlibet/quodlibet/parse/_pattern.py
@@ -17,13 +17,22 @@ from quodlibet.parse._scanner import Scanner
 # Token types.
 (OPEN, CLOSE, TEXT, COND, EOF) = range(5)
 
-class error(ValueError): pass
-class ParseError(error): pass
-class LexerError(error): pass
+
+class error(ValueError):
+    pass
+
+
+class ParseError(error):
+    pass
+
+
+class LexerError(error):
+    pass
+
 
 class PatternLexeme(object):
-    _reverse = { OPEN: "OPEN", CLOSE: "CLOSE", TEXT: "TEXT", COND: "COND",
-                 EOF: "EOF" }
+    _reverse = {OPEN: "OPEN", CLOSE: "CLOSE", TEXT: "TEXT", COND: "COND",
+                EOF: "EOF"}
 
     def __init__(self, typ, lexeme):
         self.type = typ
@@ -35,6 +44,7 @@ class PatternLexeme(object):
                 str(self._reverse[self.type]) +
                 "), lexeme=" + repr(self.lexeme) + ">")
 
+
 class PatternLexer(Scanner):
     def __init__(self, s):
         self.string = s.strip()
@@ -45,14 +55,18 @@ class PatternLexer(Scanner):
 
     def text(self, scanner, string):
         return PatternLexeme(TEXT, re.sub(r"\\(.)", r"\1", string))
+
     def table(self, scanner, string):
         return PatternLexeme(
             {"|": COND, "<": OPEN, ">": CLOSE}[string], string)
 
     def __iter__(self):
         s = self.scan(self.string)
-        if s[1] != "": raise LexerError("characters left over in string")
-        else: return iter(s[0] + [PatternLexeme(EOF, "")])
+        if s[1] != "":
+            raise LexerError("characters left over in string")
+        else:
+            return iter(s[0] + [PatternLexeme(EOF, "")])
+
 
 class PatternNode(object):
     def __init__(self):
@@ -61,12 +75,14 @@ class PatternNode(object):
     def __repr__(self):
         return "Pattern(%s)" % (", ".join(map(repr, self.children)))
 
+
 class TextNode(object):
     def __init__(self, text):
         self.text = text
 
     def __repr__(self):
         return "Text(\"%s\")" % self.text
+
 
 class ConditionNode(object):
     def __init__(self, tag, ifcase, elsecase):
@@ -78,12 +94,14 @@ class ConditionNode(object):
         t, i, e = self.tag, repr(self.ifcase), repr(self.elsecase)
         return "Condition(tag: \"%s\", if: %s, else: %s)" % (t, i, e)
 
+
 class TagNode(object):
     def __init__(self, tag):
         self.tag = tag
 
     def __repr__(self):
         return "Tag(\"%s\")" % self.tag
+
 
 class PatternParser(object):
     def __init__(self, tokens):
@@ -108,7 +126,8 @@ class PatternParser(object):
         # fix bad tied tags
         if tag[:1] != "~" and "~" in tag:
                 tag = "~" + tag
-        try: self.match(TEXT)
+        try:
+            self.match(TEXT)
         except ParseError:
             while self.lookahead.type not in [CLOSE, EOF]:
                 self.match(self.lookahead.type)
@@ -119,17 +138,20 @@ class PatternParser(object):
             if self.lookahead.type == COND:
                 self.match(COND)
                 elsecase = self.Pattern()
-            else: elsecase = None
+            else:
+                elsecase = None
             nodes.append(ConditionNode(tag, ifcase, elsecase))
 
-            try: self.match(CLOSE)
+            try:
+                self.match(CLOSE)
             except ParseError:
                 nodes.pop(-1)
                 while self.lookahead.type not in [EOF, OPEN]:
                     self.match(self.lookahead.type)
         else:
             nodes.append(TagNode(tag))
-            try: self.match(CLOSE)
+            try:
+                self.match(CLOSE)
             except ParseError:
                 nodes.pop(-1)
                 while self.lookahead.type not in [EOF, OPEN]:
@@ -144,10 +166,11 @@ class PatternParser(object):
             if self.lookahead.type in tokens:
                 self.lookahead = self.tokens.next()
             else:
-                raise ParseError("The token '%s' is not the type exected." %(
+                raise ParseError("The token '%s' is not the type exected." % (
                     self.lookahead.lexeme))
         except StopIteration:
             self.lookahead = PatternLexeme(EOF, "")
+
 
 class PatternFormatter(object):
     _formatters = []
@@ -157,11 +180,14 @@ class PatternFormatter(object):
         self.__func = func
         self.__list_func = list_func
         self.tags = set(tags)
-        self.format(self.Dummy()) # Validate string
+        self.format(self.Dummy())  # Validate string
 
     class Dummy(dict):
-        def comma(self, *args): return u"_"
-        def list_separate(self, *args): return [u""]
+        def comma(self, *args):
+            return u"_"
+
+        def list_separate(self, *args):
+            return [u""]
 
     class SongProxy(object):
         def __init__(self, realsong, formatters):
@@ -186,7 +212,8 @@ class PatternFormatter(object):
                 if isinstance(value, float):
                     value = "%.2f" % value
                 values = [unicode(value)]
-            else: values = self.__song.list_separate(key)
+            else:
+                values = self.__song.list_separate(key)
             for f in self.__formatters:
                 values = [f(key, v) for v in values]
             return values
@@ -211,6 +238,7 @@ class PatternFormatter(object):
         return set(vals)
 
     __mod__ = format
+
 
 class PatternCompiler(object):
     def __init__(self, root):
@@ -275,6 +303,7 @@ class PatternCompiler(object):
                 text.extend(self.__tag(child, scope, tags))
         return map("  ".__add__, text)
 
+
 def Pattern(string, Kind=PatternFormatter, MAX_CACHE_SIZE=100, cache={}):
     if (Kind, string) not in cache:
         if len(cache) > MAX_CACHE_SIZE:
@@ -285,18 +314,27 @@ def Pattern(string, Kind=PatternFormatter, MAX_CACHE_SIZE=100, cache={}):
         cache[(Kind, string)] = Kind(func, list_func, tags)
     return cache[(Kind, string)]
 
+
 def _number(key, value):
     if key == "tracknumber":
         parts = value.split("/")
-        try: decimals = len(str(int(parts[1])))
-        except (IndexError, ValueError): decimals = 2
-        try: return "%0*d" % (max(2, decimals), int(parts[0]))
-        except (TypeError, ValueError): return value
+        try:
+            decimals = len(str(int(parts[1])))
+        except (IndexError, ValueError):
+            decimals = 2
+        try:
+            return "%0*d" % (max(2, decimals), int(parts[0]))
+        except (TypeError, ValueError):
+            return value
     elif key == "discnumber":
         parts = value.split("/")
-        try: return "%02d" % int(parts[0])
-        except (TypeError, ValueError): return value
-    else: return value
+        try:
+            return "%02d" % int(parts[0])
+        except (TypeError, ValueError):
+            return value
+    else:
+        return value
+
 
 class _FileFromPattern(PatternFormatter):
     _formatters = [_number,
@@ -310,7 +348,8 @@ class _FileFromPattern(PatternFormatter):
             fn = song.get("~filename", ".")
             ext = fn[fn.rfind("."):].lower()
             val_ext = value[-len(ext):].lower()
-            if not ext == val_ext: value += ext.lower()
+            if not ext == val_ext:
+                value += ext.lower()
             if os.name == "nt":
                 value = util.strip_win32_incompat_from_path(value)
 
@@ -322,13 +361,14 @@ class _FileFromPattern(PatternFormatter):
             path = map(util.fsnative, path.split(os.sep))
             limit = [255] * len(path)
             limit[-1] -= len(util.fsnative(ext))
-            elip = lambda (p, l): (len(p) > l and p[:l-2] + "..") or p
+            elip = lambda (p, l): (len(p) > l and p[:l - 2] + "..") or p
             path = os.sep.join(map(elip, zip(path, limit)))
             value = util.fsdecode(path) + ext
 
             if os.sep in value and not os.path.isabs(value):
                 raise ValueError("Pattern is not rooted")
         return value
+
 
 class _XMLFromPattern(PatternFormatter):
     _formatters = [lambda k, s: util.escape(s)]
@@ -341,6 +381,7 @@ def FileFromPattern(string):
     if os.name == 'nt':
         string = string.replace("\\", r"\\")
     return Pattern(string, _FileFromPattern)
+
 
 def XMLFromPattern(string):
     return Pattern(string, _XMLFromPattern)
