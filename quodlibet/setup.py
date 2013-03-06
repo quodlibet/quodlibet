ca0947806b6f8a963e639b3c5f31c259f5bb7733
Christoph Reiter <reiter.christoph@gmail.com>
2013-03-06 12:32:43 +0100

    fix build: debug package is gone

--- quodlibet/setup.py
+++ quodlibet/setup.py
@@ -17,7 +17,7 @@ from gdist import GDistribution
 from gdist.clean import clean as gdist_clean
 
 PACKAGES = ("browsers devices formats library parse plugins qltk "
-            "util player debug").split()
+            "util player").split()
 
 # TODO: link this better to the app definitions
 MIN_PYTHON_VER = (2,6)
