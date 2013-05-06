92db0147596913ae17ae07021db3f9441c78b403
Christoph Reiter <reiter.christoph@gmail.com>
2013-05-06 11:29:38 +0200

    theme switcher: Add option to prefer dark theme version.

--- plugins/events/themeswitcher.py
+++ plugins/events/themeswitcher.py
@@ -11,6 +11,7 @@ from gi.repository import Gtk
 from quodlibet import qltk
 from quodlibet import config
 from quodlibet import const
+from quodlibet.qltk.ccb import ConfigCheckButton
 from quodlibet.plugins.events import EventPlugin
 
 
@@ -20,6 +21,9 @@ class ThemeSwitcher(EventPlugin):
     PLUGIN_DESC = ("Change the active GTK+ theme.")
 
     __enabled = False
+
+    CONFIG_THEME = PLUGIN_ID + "_theme"
+    CONFIG_DARK = PLUGIN_ID + "_prefer_dark"
 
     def PluginPreferences(self, *args):
         hb = Gtk.HBox(spacing=6)
@@ -39,17 +43,32 @@ class ThemeSwitcher(EventPlugin):
         combo.set_active(select)
         combo.connect('changed', self.__changed)
 
+        dark_button = ConfigCheckButton(
+            _("Prefer dark theme version"),
+            "plugins", self.CONFIG_DARK)
+        dark_button.set_active(
+            config.getboolean("plugins", self.CONFIG_DARK, False))
+
+        def dark_cb(button):
+            self.__set_dark(button.get_active())
+
+        dark_button.connect('toggled', dark_cb)
+
         label.set_mnemonic_widget(combo)
         label.set_use_underline(True)
         hb.pack_start(label, False, True, 0)
         hb.pack_start(combo, False, True, 0)
 
-        return qltk.Frame(_("Preferences"), child=hb)
+        vbox = Gtk.VBox(spacing=6)
+        vbox.pack_start(hb, False, True, 0)
+        vbox.pack_start(dark_button, False, True, 0)
+
+        return qltk.Frame(_("Preferences"), child=vbox)
 
     def __changed(self, combo):
         index = combo.get_active()
         name = (index and combo.get_active_text()) or ""
-        config.set("plugins", __name__, name)
+        config.set("plugins", self.CONFIG_THEME, name)
         self.__set_theme(name)
 
     def __get_themes(self):
@@ -78,16 +97,29 @@ class ThemeSwitcher(EventPlugin):
 
         settings.set_property('gtk-theme-name', name)
 
+    def __set_dark(self, value):
+        if not self.__enabled:
+            return
+        settings = Gtk.Settings.get_default()
+        if value is None:
+            value = self.__default_dark
+        settings.set_property('gtk-application-prefer-dark-theme', value)
+
     def enabled(self):
         self.__enabled = True
 
         settings = Gtk.Settings.get_default()
         self.__default_theme = settings.get_property('gtk-theme-name')
+        self.__default_dark = settings.get_property(
+            'gtk-application-prefer-dark-theme')
 
-        theme = config.get("plugins", __name__, None)
-
+        theme = config.get("plugins", self.CONFIG_THEME, None)
         self.__set_theme(theme)
+
+        is_dark = config.getboolean("plugins", self.CONFIG_DARK, False)
+        self.__set_dark(is_dark)
 
     def disabled(self):
         self.__set_theme(None)
+        self.__set_dark(None)
         self.__enabled = False
