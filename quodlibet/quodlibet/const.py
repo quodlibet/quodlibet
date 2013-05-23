5407cfa70c5c0df0cd049f094035919596604a8d
Christoph Reiter <reiter.christoph@gmail.com>
2013-05-23 10:50:43 +0200

    Adjust branch name

--- quodlibet/quodlibet/const.py
+++ quodlibet/quodlibet/const.py
@@ -63,7 +63,7 @@ LIBRARY_SAVE_PERIOD_SECONDS = 15 * 60
 DEFAULT_RATING = 0.5
 
 # entry point for the user guide / wiki
-BRANCH_NAME = "pygobject-port"
+BRANCH_NAME = "default"
 DOCS_BASE_URL = "https://quodlibet.readthedocs.org/en/%s"
 DOCS_BASE_URL %= BRANCH_NAME if BRANCH_NAME != "default" else "latest"
 ONLINE_HELP = DOCS_BASE_URL + "/guide/index.html"
