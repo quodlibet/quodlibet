b03e887dd4fa18535986c5e081560eea5881312e
Christoph Reiter <reiter.christoph@gmail.com>
2013-06-05 20:07:39 +0200

    version bump

--- quodlibet/quodlibet/const.py
+++ quodlibet/quodlibet/const.py
@@ -29,7 +29,7 @@ class MinVersions(object):
     PYTHON = Version(2, 6)
     MUTAGEN = Version(1, 14)
 
-VERSION_TUPLE = Version(2, 9, 92)
+VERSION_TUPLE = Version(2, 9, 92, -1)
 VERSION = str(VERSION_TUPLE)
 
 PROCESS_TITLE_QL = "quodlibet"
