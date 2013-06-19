530903bd71aa2f35965e21d23cf9874defb12623
Christoph Reiter <reiter.christoph@gmail.com>
2013-06-20 00:10:57 +0200

    Monkey patch Gtk.TreePath.__getitem__ for PyGObject 3.2

--- quodlibet/quodlibet/__init__.py
+++ quodlibet/quodlibet/__init__.py
@@ -103,6 +103,12 @@ def _gtk_init(icon=None):
 
     from gi.repository import Gtk, GObject, GLib
 
+    # add Gtk.TreePath.__getitem__ for PyGObject 3.2
+    try:
+        Gtk.TreePath()[0]
+    except TypeError:
+        Gtk.TreePath.__getitem__ = lambda self, index: list(self)[index]
+
     # We don't depend on Gst overrides, so make sure it's initialized.
     try:
         gi.require_version("Gst", "1.0")
