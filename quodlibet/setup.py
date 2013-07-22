421e6653ff99113ac4fa3e8ab6adc12ff51358b8
Christoph Reiter <reiter.christoph@gmail.com>
2013-07-22 13:40:27 +0200

    Add new package.. we need tests for this..

--- quodlibet/setup.py
+++ quodlibet/setup.py
@@ -22,7 +22,7 @@ from gdist import GDistribution
 from gdist.clean import clean as gdist_clean
 
 PACKAGES = ("browsers devices formats library parse plugins qltk "
-            "util player browsers.albums").split()
+            "util player browsers.albums util.string").split()
 
 # TODO: link this better to the app definitions
 MIN_PYTHON_VER = (2,6)
