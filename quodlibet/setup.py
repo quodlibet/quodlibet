150573cc03bb4f390894842f23204f9e449fcac8
Christoph Reiter <reiter.christoph@gmail.com>
2013-10-25 22:00:50 +0200

    update package list

--- quodlibet/setup.py
+++ quodlibet/setup.py
@@ -21,7 +21,7 @@ from gdist import GDistribution
 from gdist.clean import clean as gdist_clean
 
 PACKAGES = ("browsers devices formats library parse plugins qltk "
-            "util player browsers.albums util.string").split()
+            "util player browsers.albums browsers.paned util.string").split()
 
 # TODO: link this better to the app definitions
 MIN_PYTHON_VER = (2, 6)
