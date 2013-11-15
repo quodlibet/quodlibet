70650f15a11c450c68af11660909c53df7e0da29
Christoph Reiter <reiter.christoph@gmail.com>
2013-11-15 09:09:09 +0100

    tests: fix (fails on drone.io)

--- quodlibet/tests/test_qltk_ratingsmenu.py
+++ quodlibet/tests/test_qltk_ratingsmenu.py
@@ -1,16 +1,24 @@
-# Copyright 2012 Christoph Reiter
+# Copyright 2012,2013 Christoph Reiter
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
 
 from tests import TestCase, add
+from quodlibet import config
 from quodlibet.qltk.ratingsmenu import RatingsMenuItem
 from quodlibet.library import SongLibrary, SongLibrarian
 from quodlibet.formats._audio import AudioFile
 
 
 class TRatingsMenuItem(TestCase):
+
+    def setUp(self):
+        config.init()
+
+    def tearDown(self):
+        config.quit()
+
     def test_menuitem(self):
         library = SongLibrary()
         library.librarian = SongLibrarian()
