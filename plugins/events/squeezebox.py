161a776aaf872f1756d3f5b5476355696337ee7c
Christoph Reiter <reiter.christoph@gmail.com>
2013-12-17 16:43:48 +0100

    win: another failing plugin

--- plugins/events/squeezebox.py
+++ plugins/events/squeezebox.py
@@ -6,7 +6,12 @@
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
-#
+
+import os
+
+if os.name == "nt":
+    from quodlibet.plugins import PluginNotSupportedError
+    raise PluginNotSupportedError
 
 from telnetlib import Telnet
 from threading import Thread
