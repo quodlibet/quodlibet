05eecd0ef1d0b5fd52a80adb0a47640d5540610f
Nick B <nick.boultbee@gmail.com>
2014-01-10 21:20:10 +0000

    Fix i18n scanners for new browser structure.

--- quodlibet/setup.py
+++ quodlibet/setup.py
@@ -20,9 +20,10 @@ from distutils.archive_util import make_archive
 from gdist import GDistribution
 from gdist.clean import clean as gdist_clean
 
-PACKAGES = ("browsers devices formats library parse plugins qltk "
-            "util player browsers.albums browsers.paned util.string "
-            "util.cover player.gstbe player.xinebe").split()
+PACKAGES = ("browsers devices formats library parse plugins qltk util player "
+            "browsers.albums browsers.paned browsers.playlists "
+            "util.string util.cover "
+            "player.gstbe player.xinebe").split()
 
 # TODO: link this better to the app definitions
 MIN_PYTHON_VER = (2, 6)
