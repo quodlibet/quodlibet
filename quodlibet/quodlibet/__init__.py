b0ba68cda13a094ee60fb00f5a0a947a874fa83f
Christoph Reiter <reiter.christoph@gmail.com>
2014-02-22 16:05:18 +0100

    Force gtk-button-images and gtk-menu-images. If anyone doesn't like it please file a bug report.

--- quodlibet/quodlibet/__init__.py
+++ quodlibet/quodlibet/__init__.py
@@ -120,6 +120,13 @@ def _gtk_init(icon=None):
         Gdk.BUTTON_MIDDLE = 2
         Gdk.BUTTON_SECONDARY = 3
 
+    # Force menu/button image related settings. We might show too many atm
+    # but this makes sure we don't miss cases where we forgot to force them
+    # per widget.
+    settings = Gtk.Settings.get_default()
+    settings.set_property("gtk-button-images", True)
+    settings.set_property("gtk-menu-images", True)
+
     # Make sure PyGObject includes support for foreign cairo structs
     some_window = Gtk.OffscreenWindow()
     some_window.show()
