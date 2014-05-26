1ad834e699f84bd60522f67a6b17f5e0b0686897
Christoph Reiter <reiter.christoph@gmail.com>
2014-05-26 19:07:52 +0200

    Merge 'Browse File' plugin into the 'Browse Folders' plugin.
    Depending on the interfaces available it will (in that order):
    
     * open directories and select all files for all selected songs
     * open directories and select one file which is the first of the selected songs for each directory
     * only open directories

--- quodlibet/quodlibet/ext/songsmenu/browsefolders.py
+++ quodlibet/quodlibet/ext/songsmenu/browsefolders.py
@@ -13,12 +13,7 @@ from gi.repository import Gtk
 try:
     import dbus
 except ImportError:
-    class FakeDbus(object):
-        def __getattribute__(self, name):
-            if name == "DBusException":
-                return Exception
-            raise Exception
-    dbus = FakeDbus()
+    dbus = None
 
 from quodlibet.plugins.songsmenu import SongsMenuPlugin
 from quodlibet.util.uri import URI
@@ -27,77 +22,98 @@ from quodlibet.util.dprint import print_d
 from quodlibet.util.path import is_fsnative, normalize_path
 
 
+class BrowseError(Exception):
+    pass
+
+
+def group_songs(songs):
+    """Groups a sequence of songs per dirname.
+
+    The value order is the same as with the passed in songs.
+    """
+
+    dirs = {}
+    for s in songs:
+        dirs.setdefault(s("~dirname"), []).append(s)
+    return dirs
+
+
 def get_startup_id():
     from quodlibet import app
     app_name = type(app.window).__name__
     return "%s_TIME%d" % (app_name, Gtk.get_current_event_time())
 
 
-# http://www.freedesktop.org/wiki/Specifications/file-manager-interface
-FDO_PATH = "/org/freedesktop/FileManager1"
-FDO_NAME = "org.freedesktop.FileManager1"
-FDO_IFACE = "org.freedesktop.FileManager1"
-
-
 def browse_folders_fdo(songs):
-    bus = dbus.SessionBus()
-    bus_object = bus.get_object(FDO_NAME, FDO_PATH)
-    bus_iface = dbus.Interface(bus_object, dbus_interface=FDO_IFACE)
-    uris = map(URI.frompath, set([s("~dirname") for s in songs]))
-    bus_iface.ShowFolders(uris, get_startup_id())
+    # http://www.freedesktop.org/wiki/Specifications/file-manager-interface
+    FDO_PATH = "/org/freedesktop/FileManager1"
+    FDO_NAME = "org.freedesktop.FileManager1"
+    FDO_IFACE = "org.freedesktop.FileManager1"
 
+    if not dbus:
+        raise BrowseError("no dbus")
 
-def browse_files_fdo(songs):
-    bus = dbus.SessionBus()
-    bus_object = bus.get_object(FDO_NAME, FDO_PATH)
-    bus_iface = dbus.Interface(bus_object, dbus_interface=FDO_IFACE)
-    uris = [s("~uri") for s in songs]
-    bus_iface.ShowItems(uris, get_startup_id())
+    try:
+        bus = dbus.SessionBus()
+        bus_object = bus.get_object(FDO_NAME, FDO_PATH)
+        bus_iface = dbus.Interface(bus_object, dbus_interface=FDO_IFACE)
 
-
-# http://git.xfce.org/xfce/thunar/tree/thunar/thunar-dbus-service-infos.xml
-XFCE_PATH = "/org/xfce/FileManager"
-XFCE_NAME = "org.xfce.FileManager"
-XFCE_IFACE = "org.xfce.FileManager"
+        # open each folder and select the first file we have selected
+        for dirname, sub_songs in group_songs(songs).items():
+            print sub_songs[0]("~uri"), get_startup_id()
+            bus_iface.ShowItems([sub_songs[0]("~uri")], get_startup_id())
+    except dbus.DBusException as e:
+        raise BrowseError(e)
 
 
 def browse_folders_thunar(songs, display=""):
-    bus = dbus.SessionBus()
-    bus_object = bus.get_object(XFCE_NAME, XFCE_PATH)
-    bus_iface = dbus.Interface(bus_object, dbus_interface=XFCE_IFACE)
-    uris = map(URI.frompath, set([s("~dirname") for s in songs]))
-    for uri in uris:
-        bus_iface.DisplayFolder(uri, display, get_startup_id())
+    # http://git.xfce.org/xfce/thunar/tree/thunar/thunar-dbus-service-infos.xml
+    XFCE_PATH = "/org/xfce/FileManager"
+    XFCE_NAME = "org.xfce.FileManager"
+    XFCE_IFACE = "org.xfce.FileManager"
 
+    if not dbus:
+        raise BrowseError("no dbus")
 
-def browse_files_thunar(songs, display=""):
-    bus = dbus.SessionBus()
-    bus_object = bus.get_object(XFCE_NAME, XFCE_PATH)
-    bus_iface = dbus.Interface(bus_object, dbus_interface=XFCE_IFACE)
-    for song in songs:
-        dirname = song("~dirname")
-        basename = song("~basename")
-        bus_iface.DisplayFolderAndSelect(URI.frompath(dirname), basename,
-                                         display, get_startup_id())
+    try:
+        bus = dbus.SessionBus()
+        bus_object = bus.get_object(XFCE_NAME, XFCE_PATH)
+        bus_iface = dbus.Interface(bus_object, dbus_interface=XFCE_IFACE)
+
+        # open each folder and select the first file we have selected
+        for dirname, sub_songs in group_songs(songs).items():
+            bus_iface.DisplayFolderAndSelect(
+                URI.frompath(dirname),
+                sub_songs[0]("~basename"),
+                display,
+                get_startup_id())
+    except dbus.DBusException as e:
+        raise BrowseError(e)
 
 
 def browse_folders_gnome_open(songs):
-    dirs = list(set([s("~dirname") for s in songs]))
-    for dir_ in dirs:
-        if subprocess.call(["gnome-open", dir_]) != 0:
-            raise EnvironmentError
+    try:
+        for dir_ in group_songs(songs).keys():
+            if subprocess.call(["gnome-open", dir_]) != 0:
+                raise EnvironmentError("gnome-open error return status")
+    except EnvironmentError as e:
+        raise BrowseError(e)
 
 
 def browse_folders_xdg_open(songs):
-    dirs = list(set([s("~dirname") for s in songs]))
-    for dir_ in dirs:
-        if subprocess.call(["xdg-open", dir_]) != 0:
-            raise EnvironmentError
+    try:
+        for dir_ in group_songs(songs).keys():
+            if subprocess.call(["xdg-open", dir_]) != 0:
+                raise EnvironmentError("xdg-open error return status")
+    except EnvironmentError as e:
+        raise BrowseError(e)
 
 
 def show_files_win32(path, files):
     """Takes a path to a directory and a list of filenames in that directory
     to display.
+
+    Returns True on success.
     """
 
     assert os.name == "nt"
@@ -122,68 +138,21 @@ def show_files_win32(path, files):
                 items.append(item)
         shell.SHOpenFolderAndSelectItems(folder_pidl, items, 0)
     except pywintypes.com_error:
-        return
-
-
-def group_files(file_paths):
-    """Groups a sequence of absolute file paths into their dirname and a
-    list of entry names.
-
-    The entry order is the same as with the passed in paths.
-    """
-
-    dirs = {}
-    for p in file_paths:
-        assert os.path.isabs(p)
-        dirname = os.path.dirname(p)
-        basename = os.path.basename(p)
-        dirs.setdefault(dirname, []).append(basename)
-    return dirs
-
-
-def browse_files_win_explorer(songs):
-    file_paths = [song("~filename") for song in songs]
-    for path, names in group_files(file_paths).items():
-        show_files_win32(path, names)
+        return False
+    else:
+        return True
 
 
 def browse_folders_win_explorer(songs):
-    # as an added bonus, this also selects the files in the folders
-    browse_files_win_explorer(songs)
+    if os.name != "nt":
+        raise BrowseError("windows only")
+
+    for path, sub_songs in group_songs(songs).items():
+        if not show_files_win32(path, [s("~basename") for s in sub_songs]):
+            raise BrowseError
 
 
-class HandlingMixin(object):
-    def plugin_handles(self, songs):
-        # By default, any single song being a file is good enough
-        for song in songs:
-            if song.is_file:
-                return True
-        return False
-
-    def handle(self, songs):
-        """
-        Uses the first successful handler in callable list `_HANDLERS`
-        to handle `songs`
-        Returns False if none could be used
-        """
-        if not hasattr(self, "_HANDLERS"):
-            return False
-        for handler in self._HANDLERS:
-            name = handler.__name__
-            try:
-                print_d("Trying %r..." % name)
-                handler(songs)
-            except (dbus.DBusException, EnvironmentError):
-                print_d("...failed.")
-                # TODO: caching of failures (re-order list maybe)
-            else:
-                print_d("...success!")
-                return True
-        print_d("No handlers could be used.")
-        return False
-
-
-class BrowseFolders(SongsMenuPlugin, HandlingMixin):
+class BrowseFolders(SongsMenuPlugin):
     PLUGIN_ID = 'Browse Folders'
     PLUGIN_NAME = _('Browse Folders')
     PLUGIN_DESC = "View the songs' folders in a file manager"
@@ -197,26 +166,34 @@ class BrowseFolders(SongsMenuPlugin, HandlingMixin):
     def plugin_songs(self, songs):
         songs = [s for s in songs if s.is_file]
         print_d("Trying to browse folders...")
-        if not self.handle(songs):
+        if not self._handle(songs):
             ErrorMessage(self.plugin_window,
                          _("Unable to open folders"),
                          _("No program available to open folders.")).run()
 
+    def plugin_handles(self, songs):
+        # By default, any single song being a file is good enough
+        for song in songs:
+            if song.is_file:
+                return True
+        return False
 
-class BrowseFiles(SongsMenuPlugin, HandlingMixin):
-    PLUGIN_ID = 'Browse Files'
-    PLUGIN_NAME = _('Show File')
-    PLUGIN_DESC = "View the song's file in a file manager"
-    PLUGIN_ICON = Gtk.STOCK_OPEN
-    PLUGIN_VERSION = '1.1'
+    def _handle(self, songs):
+        """
+        Uses the first successful handler in callable list `_HANDLERS`
+        to handle `songs`
+        Returns False if none could be used
+        """
 
-    _HANDLERS = [browse_files_fdo, browse_files_thunar,
-                 browse_files_win_explorer]
-
-    def plugin_single_song(self, song):
-        songs = [s for s in [song] if s.is_file]
-        print_d("Trying to browse files...")
-        if not self.handle(songs):
-            ErrorMessage(self.plugin_window,
-                         _("Unable to browse files"),
-                         _("No program available to browse files.")).run()
+        for handler in self._HANDLERS:
+            name = handler.__name__
+            try:
+                print_d("Trying %r..." % name)
+                handler(songs)
+            except BrowseError as e:
+                print_d("...failed: %r" % e)
+            else:
+                print_d("...success!")
+                return True
+        print_d("No handlers could be used.")
+        return False
