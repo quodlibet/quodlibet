89750630ea9e378fa4c0533b2293deff72f946d6
Christoph Reiter <reiter.christoph@gmail.com>
2014-07-07 20:32:22 +0200

    remove the render test, in never draws on the buildbot and hangs

--- quodlibet/tests/test_qltk_image.py
+++ quodlibet/tests/test_qltk_image.py
@@ -41,37 +41,3 @@ class TImageUtils(TestCase):
         newpb = GdkPixbuf.Pixbuf.new(rgb, True, 8, 10, 10)
         set_image_from_pbosf(image, newpb)
         self.assertTrue(image.props.pixbuf)
-
-    def test_render(self):
-
-        class Custom(Gtk.Image):
-
-            def __init__(self, pbosf):
-                super(Custom, self).__init__()
-                self.pbosf = pbosf
-                self.started = False
-                self.drawn = False
-
-            def do_draw(self, cairo_context):
-                self.started = True
-                style_context = self.get_style_context()
-                pbosf_render(style_context, cairo_context, self.pbosf, 0, 0)
-                self.drawn = True
-
-            def wait_for_draw(self):
-                self.queue_draw()
-                while not self.started:
-                    if not Gtk.main_iteration_do(False):
-                        break
-
-        rgb = GdkPixbuf.Colorspace.RGB
-        newpb = GdkPixbuf.Pixbuf.new(rgb, True, 8, 10, 10)
-        with visible(Custom(newpb)) as w:
-            w.wait_for_draw()
-            self.assertTrue(w.drawn)
-
-        if hasattr(Gtk.Image, "new_from_surface"):
-            sf = cairo.ImageSurface(cairo.FORMAT_RGB24, 10, 10)
-            with visible(Custom(sf)) as w:
-                w.wait_for_draw()
-                self.assertTrue(w.drawn)
