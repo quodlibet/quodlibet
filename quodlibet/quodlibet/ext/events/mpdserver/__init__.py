75977958286ad03eacf3a2102d53f42e306e1da7
Christoph Reiter <reiter.christoph@gmail.com>
2014-07-18 13:46:53 +0200

    mpd: disable on Windows

--- quodlibet/quodlibet/ext/events/mpdserver/__init__.py
+++ quodlibet/quodlibet/ext/events/mpdserver/__init__.py
@@ -4,6 +4,13 @@
 # it under the terms of version 2 of the GNU General Public License as
 # published by the Free Software Foundation.
 
+import os
+
+if os.name == "nt":
+    from quodlibet.plugins import PluginNotSupportedError
+    # we are missing socket.fromfd on Windows
+    raise PluginNotSupportedError
+
 import socket
 import threading
 
