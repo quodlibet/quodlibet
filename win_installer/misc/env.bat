1f699800a0e7c81ea5619e77f6f0a60a2206f913
Christoph Reiter <reiter.christoph@gmail.com>
2014-07-18 14:12:58 +0200

    winsdk env: set PYTHONDONTWRITEBYTECODE

--- win_installer/misc/env.bat
+++ win_installer/misc/env.bat
@@ -1,4 +1,5 @@
 set GI_TYPELIB_PATH=%~dp0deps\lib\girepository-1.0
 set PATH=%~dp0\deps;%~dp0python;%PATH%
 set QUODLIBET_USERDIR=%~dp0_ql_config
+set PYTHONDONTWRITEBYTECODE=1
 cd %~dp0quodlibet\quodlibet
