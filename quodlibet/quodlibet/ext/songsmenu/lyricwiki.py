eea58d585214b7841f8c4c41aaaf2bd3fb454efb
Christoph Reiter <reiter.christoph@gmail.com>
2014-08-26 15:25:34 +0200

    lyricwiki: threading & error handling

--- quodlibet/quodlibet/ext/songsmenu/lyricwiki.py
+++ quodlibet/quodlibet/ext/songsmenu/lyricwiki.py
@@ -4,8 +4,11 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
 
-import sys
 import urllib
+import urllib2
+import threading
+import socket
+import Queue
 from xml.dom import minidom
 
 from quodlibet.plugins import PluginImportException
@@ -16,12 +19,101 @@ try:
 except ValueError as e:
     raise PluginImportException("GObject Introspection: " + str(e))
 
-from gi.repository import WebKit, Gtk
+from gi.repository import WebKit, Gtk, GLib
 
 from quodlibet import app
 from quodlibet.util import DeferredSignal, escape
 from quodlibet.qltk.window import Window
 from quodlibet.plugins.songsmenu import SongsMenuPlugin
+
+
+class SearchThread(threading.Thread):
+    TIMEOUT = 4.0
+
+    def __init__(self):
+        super(SearchThread, self).__init__()
+        self.daemon = True
+        self._queue = Queue.Queue()
+        self._stopped = False
+        self._current = None
+
+    def search_song(self, song, done_cb):
+        """Trigger a new search, old ones will be canceled"""
+
+        assert song
+        assert done_cb
+
+        if not self.is_alive():
+            self.start()
+
+        self._current = song
+        while not self._queue.empty():
+            self._queue.get_nowait()
+        self._queue.put((song, done_cb))
+
+    def stop(self):
+        """Stop all acitve searchs, no callback will be called after this
+           returns.
+        """
+
+        self._stopped = True
+        self._queue.put((None, None))
+
+    def __idle(self, song, func, *args, **kwargs):
+        def delayed():
+            if self._stopped or song is not self._current:
+                return
+            func(song, *args, **kwargs)
+
+        GLib.idle_add(delayed)
+
+    def _do_search(self, song):
+        """Returns a URL or None"""
+
+        artist, title = song("artist"), song("title")
+        artist = urllib.quote(artist.encode('utf-8'))
+        title = urllib.quote(title.encode('utf-8'))
+
+        url = ("http://lyrics.wikia.com/api.php?client=QuodLibet&func=getSong"
+               "&artist=%s&song=%s&fmt=xml")
+
+        fetch_url = url % (artist, title)
+
+        try:
+            response = urllib2.urlopen(fetch_url, timeout=self.TIMEOUT)
+        except (urllib2.URLError, socket.timeout):
+            return
+
+        try:
+            xml = response.read()
+        except IOError:
+            return
+
+        try:
+            dom = minidom.parseString(xml)
+        except Exception:
+            # who knows...
+            return
+
+        page_id_nodes = dom.getElementsByTagName("page_id")
+        if not page_id_nodes or not page_id_nodes[0].hasChildNodes():
+            return
+
+        url_nodes = dom.getElementsByTagName("url")
+        try:
+            page = url_nodes[0].firstChild.data
+        except (IndexError, AttributeError):
+            return
+        else:
+            return page
+
+    def run(self):
+        while not self._stopped:
+            song, cb = self._queue.get()
+            if self._stopped:
+                break
+            result = self._do_search(song)
+            self.__idle(song, cb, result)
 
 
 class LyricWikiWindow(Window):
@@ -31,7 +123,8 @@ class LyricWikiWindow(Window):
             default_width=500, default_height=500, dialog=False)
         self.set_transient_for(parent)
 
-        self._defer_set = DeferredSignal(self._set_song, owner=self)
+        self._thread = SearchThread()
+        self.connect("destroy", lambda *x: self._thread.stop())
 
         sw = Gtk.ScrolledWindow()
         self.add(sw)
@@ -54,10 +147,25 @@ class LyricWikiWindow(Window):
         sw.add(view)
         sw.show_all()
 
+    def set_song(self, song):
+        """Display lyrics for the given song"""
+
+        if song is None:
+            message = _("No active song")
+            self._set_html(message)
+            self._set_title(message)
+            return
+
+        self._thread.search_song(song, self._callback)
+
     def enable_tracking(self, player):
+        """Follow the active playing song"""
+
+        # for tracking wait a bit to not produce lots of http requests
+        defer_set = DeferredSignal(self.set_song, owner=self, timeout=50)
 
         def next_handler(player, song):
-            self.set_song(song)
+            defer_set(song)
 
         id_ = player.connect("song-started", next_handler)
         self.connect("destroy", lambda *x: player.disconnect(id_))
@@ -69,44 +177,17 @@ class LyricWikiWindow(Window):
 
         self._view.load_html_string(html, "http://foo.bar")
 
-    def set_song(self, song):
-        # FIXME: threading
-
-        self._defer_set(song)
-
-    def _set_title(self, message):
-        self.set_title(_("Lyrics:") + " " + message)
-
-    def _set_song(self, song):
-        if song is None:
-            message = _("No active song")
-            self._set_html(message)
-            self._set_title(message)
-            return
-
-        artist, title = song("artist"), song("title")
-        artist = urllib.quote(artist.encode('utf-8'))
-        title = urllib.quote(title.encode('utf-8'))
-
-        url = ("http://lyrics.wikia.com/api.php?client=QuodLibet&func=getSong"
-               "&artist=%s&song=%s&fmt=xml")
-
-        fetch_url = url % (artist, title)
-        xml = urllib.urlopen(fetch_url).read()
-        dom = minidom.parseString(xml)
-        lyrics_node = dom.getElementsByTagName("lyrics")
-        url_node = dom.getElementsByTagName("url")
-
-        if not lyrics_node or not url_node or \
-                lyrics_node[0].firstChild.data == "Not found":
+    def _callback(self, song, page):
+        if page is None:
             message = _("No lyrics found")
             self._set_html(message, song("~artist~title"))
             self._set_title(message)
-            return
+        else:
+            self._view.load_uri(page)
+            self._set_title(song("title"))
 
-        page = url_node[0].firstChild.data
-        self._view.load_uri(page)
-        self._set_title(song("title"))
+    def _set_title(self, message):
+        self.set_title(_("Lyrics:") + " " + message)
 
 
 class LyricWiki(SongsMenuPlugin):
