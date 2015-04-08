31af1f57fa180b847d6b3e095c83de367c996cfb
Christoph Reiter <reiter.christoph@gmail.com>
2015-04-08 23:23:59 +0200

    New 3.4 stable branch

--- quodlibet/quodlibet/const.py
+++ quodlibet/quodlibet/const.py
@@ -67,7 +67,7 @@ LIBRARY = os.path.join(USERDIR, "songs")
 LOGDIR = os.path.join(USERDIR, "logs")
 
 # entry point for the user guide / wiki
-BRANCH_NAME = "master"
+BRANCH_NAME = "quodlibet-3.4"
 DOCS_BASE_URL = "https://quodlibet.readthedocs.org/en/%s"
 DOCS_LATEST = DOCS_BASE_URL % "latest"
 DOCS_BASE_URL %= BRANCH_NAME if BRANCH_NAME != "master" else "latest"
