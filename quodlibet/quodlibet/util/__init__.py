4ca0305b03a3f39dec5ca670703bacaaed9293f7
Christoph Reiter <reiter.christoph@gmail.com>
2015-05-01 17:14:54 +0200

    iconv: fix import

--- quodlibet/quodlibet/util/__init__.py
+++ quodlibet/quodlibet/util/__init__.py
@@ -32,12 +32,13 @@ from quodlibet.util.string.titlecase import title
 
 from quodlibet.const import SUPPORT_EMAIL, COPYRIGHT
 from quodlibet.util.dprint import print_d, print_
-from .misc import environ, argv, cached_func, get_locale_encoding
+from .misc import environ, argv, cached_func, get_locale_encoding, \
+    get_fs_encoding
 from .environment import *
 
 
 # pyflakes
-environ, argv, cached_func, get_locale_encoding
+environ, argv, cached_func, get_locale_encoding, get_fs_encoding
 
 
 class InstanceTracker(object):
