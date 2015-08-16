53df87647122891cb77cf5bf7bd92c1480a59cc9
Christoph Reiter <reiter.christoph@gmail.com>
2015-08-16 14:48:17 +0200

    osx: jhbuild sync

--- osx_bundle/fetch_modules.sh
+++ osx_bundle/fetch_modules.sh
@@ -4,5 +4,5 @@ source env.sh
 
 git clone git://git.gnome.org/gtk-osx _gtk-osx
 cp -R _gtk-osx/modulesets-stable/. modulesets/
-cp _gtk-osx/jhbuildrc-gtk-osx config/gtk-osx-jhbuildrc
+cp _gtk-osx/jhbuildrc-gtk-osx misc/gtk-osx-jhbuildrc
 rm -Rf _gtk-osx
