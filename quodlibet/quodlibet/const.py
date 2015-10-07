3d6b1d80a7c30075792dd424cb080e9a89fdde91
Christoph Reiter <reiter.christoph@gmail.com>
2015-10-07 20:40:06 +0200

    version bump

--- quodlibet/quodlibet/const.py
+++ quodlibet/quodlibet/const.py
@@ -25,7 +25,7 @@ class MinVersions(object):
     PYTHON = Version(2, 7)
     MUTAGEN = Version(1, 27)
 
-VERSION_TUPLE = Version(3, 5, 0)
+VERSION_TUPLE = Version(3, 5, 0, -1)
 VERSION = str(VERSION_TUPLE)
 
 # entry point for the user guide / wiki
