db82b1570c20f39e88918b58a5491ebba9d450a4
elfalem <elfalem@users.noreply.github.com>
2015-10-26 21:19:57 -0400

    Synchronized Lyrics plugin - follow PEP8, use config mixin

--- quodlibet/quodlibet/ext/events/synchronizedlyrics.py
+++ quodlibet/quodlibet/ext/events/synchronizedlyrics.py
@@ -17,12 +17,13 @@ from gi.repository import Gtk, Gdk, GLib
 
 from quodlibet import app
 from quodlibet import qltk
-from quodlibet import config
+
+from quodlibet.plugins import PluginConfigMixin
 
 from quodlibet.plugins.events import EventPlugin
 
 
-class SynchronizedLyrics(EventPlugin):
+class SynchronizedLyrics(EventPlugin, PluginConfigMixin):
 
     PLUGIN_ID = 'SynchronizedLyrics'
     PLUGIN_NAME = _('Synchronized Lyrics')
@@ -31,16 +32,20 @@ as the track.')
 
     SYNC_PERIOD = 3000
 
-    _defaultBgColor = '#343428282C2C'
-    _defaultTxtColor = '#FFFFFFFFFFFF'
-    _defaultFontSize = 25
+    DEFAULT_BGCOLOR = '#343428282C2C'
+    DEFAULT_TXTCOLOR = '#FFFFFFFFFFFF'
+    DEFAULT_FONTSIZE = 25
+
+    CFG_BGCOLOR_KEY = "backgroundColor"
+    CFG_TXTCOLOR_KEY = "textColor"
+    CFG_FONTSIZE_KEY = "fontSize"
 
     _lines = []
     _timers = []
 
     _currentLrc = ""
 
-    def PluginPreferences(self, window):
+    def PluginPreferences(cls, window):
         vb = Gtk.VBox(spacing=6)
         vb.set_border_width(6)
 
@@ -57,20 +62,20 @@ as the track.')
         t.attach(l, 0, 1, 1, 2, xoptions=Gtk.AttachOptions.FILL)
 
         c = Gdk.RGBA()
-        c.parse(self.getTextColor())
+        c.parse(cls._get_text_color())
         b = Gtk.ColorButton(rgba=c)
         t.attach(b, 1, 2, 1, 2)
-        b.connect('color-set', self.setTextColor)
+        b.connect('color-set', cls._set_text_color)
 
         l = Gtk.Label(label=_("Background:"))
         l.set_alignment(xalign=1.0, yalign=0.5)
         t.attach(l, 0, 1, 2, 3, xoptions=Gtk.AttachOptions.FILL)
 
         c = Gdk.RGBA()
-        c.parse(self.getBgColor())
+        c.parse(cls._get_background_color())
         b = Gtk.ColorButton(rgba=c)
         t.attach(b, 1, 2, 2, 3)
-        b.connect('color-set', self.setBgColor)
+        b.connect('color-set', cls._set_background_color)
 
         fontSection = Gtk.Label()
         fontSection.set_markup("<b>" + _("Font") + "</b>")
@@ -80,43 +85,39 @@ as the track.')
         l.set_alignment(xalign=1.0, yalign=0.5)
         t.attach(l, 0, 1, 4, 5, xoptions=Gtk.AttachOptions.FILL)
 
-        a = Gtk.Adjustment.new(self.getFontSize(), 6, 36, 1, 3, 0)
+        a = Gtk.Adjustment.new(cls._get_font_size(), 6, 36, 1, 3, 0)
         s = Gtk.SpinButton(adjustment=a)
         s.set_numeric(True)
-        s.set_text(str(self.getFontSize()))
+        s.set_text(str(cls._get_font_size()))
         t.attach(s, 1, 2, 4, 5)
-        s.connect('value-changed', self.setFontSize)
+        s.connect('value-changed', cls._set_font_size)
 
         vb.pack_start(t, False, False, 0)
         return vb
 
-    def getTextColor(self):
-        v = config.get("plugins", self.PLUGIN_ID + "_txt",
-                       self._defaultTxtColor)
+    def _get_text_color(self):
+        v = self.config_get(self.CFG_TXTCOLOR_KEY, self.DEFAULT_TXTCOLOR)
         return v[:3] + v[5:7] + v[9:11]
 
-    def getBgColor(self):
-        v = config.get("plugins", self.PLUGIN_ID + "_bg", self._defaultBgColor)
+    def _get_background_color(self):
+        v = self.config_get(self.CFG_BGCOLOR_KEY, self.DEFAULT_BGCOLOR)
         return v[:3] + v[5:7] + v[9:11]
 
-    def getFontSize(self):
-        return int(config.get("plugins", self.PLUGIN_ID + "_fsize",
-                              self._defaultFontSize))
+    def _get_font_size(self):
+        return int(self.config_get(self.CFG_FONTSIZE_KEY,
+                                   self.DEFAULT_FONTSIZE))
 
-    def setTextColor(self, button):
-        config.set("plugins", self.PLUGIN_ID + "_txt",
-                   button.get_color().to_string())
-        self.styleLyricsWindow()
+    def _set_text_color(self, button):
+        self.config_set(self.CFG_TXTCOLOR_KEY, button.get_color().to_string())
+        self._style_lyrics_window()
 
-    def setBgColor(self, button):
-        config.set("plugins", self.PLUGIN_ID + "_bg",
-                   button.get_color().to_string())
-        self.styleLyricsWindow()
+    def _set_background_color(self, button):
+        self.config_set(self.CFG_BGCOLOR_KEY, button.get_color().to_string())
+        self._style_lyrics_window()
 
-    def setFontSize(self, sButton):
-        config.set("plugins", self.PLUGIN_ID + "_fsize",
-                   sButton.get_value_as_int())
-        self.styleLyricsWindow()
+    def _set_font_size(self, sButton):
+        self.config_set(self.CFG_FONTSIZE_KEY, sButton.get_value_as_int())
+        self._style_lyrics_window()
 
     def enabled(self):
         self.scrolled_window = Gtk.ScrolledWindow()
@@ -137,17 +138,23 @@ as the track.')
         app.window.get_child().reorder_child(self.scrolled_window, 2)
 
         self.textview.set_name("syncLyricsWindow")
-        self.styleLyricsWindow()
+        self._style_lyrics_window()
 
         self.adjustment.set_value(0)
 
         self.scrolled_window.show()
 
         self._syncTimer = GLib.timeout_add(self.SYNC_PERIOD, self._sync)
-        self.buildData()
-        self.timerControl()
+        self._build_data()
+        self._timer_control()
 
-    def styleLyricsWindow(self):
+    def disabled(self):
+        self._clear_timers()
+        GLib.source_remove(self._syncTimer)
+        self.textview.destroy()
+        self.scrolled_window.destroy()
+
+    def _style_lyrics_window(self):
         qltk.add_css(self.textview, """
             #syncLyricsWindow {{
                 background-color: {0};
@@ -155,18 +162,13 @@ as the track.')
                 font-size: {2}px;
                 font-weight: bold;
             }}
-        """.format(self.getBgColor(), self.getTextColor(), self.getFontSize()))
+        """.format(self._get_background_color(), self._get_text_color(),
+                   self._get_font_size()))
 
-    def curPosition(self):
+    def _cur_position(self):
         return app.player.get_position()
 
-    def disabled(self):
-        self.clearTimers()
-        GLib.source_remove(self._syncTimer)
-        self.textview.destroy()
-        self.scrolled_window.destroy()
-
-    def buildData(self):
+    def _build_data(self):
         self.textbuffer.set_text("")
         if app.player.song is not None:
             #check in same location as track
@@ -175,10 +177,10 @@ as the track.')
             if self._currentLrc != newLrc:
                 self._lines = []
                 if os.path.exists(newLrc):
-                    self.parseLrcFile(newLrc)
+                    self._parse_lrc_file(newLrc)
             self._currentLrc = newLrc
 
-    def parseLrcFile(self, filename):
+    def _parse_lrc_file(self, filename):
         rawFile = ""
         with open(filename, 'r') as lrcfile:
             rawFile = lrcfile.read()
@@ -222,10 +224,10 @@ as the track.')
         del keys
         del tmp_dict
 
-    def setTimers(self):
+    def _set_timers(self):
         if len(self._timers) == 0:
-            curTime = self.curPosition()
-            curIndex = self.greater(self._lines, curTime)
+            curTime = self._cur_position()
+            curIndex = self._greater(self._lines, curTime)
             if curIndex != -1:
                 while (curIndex < len(self._lines) and
                        self._lines[curIndex][0] < curTime + self.SYNC_PERIOD):
@@ -239,48 +241,48 @@ as the track.')
 
     def _sync(self):
         if not app.player.paused:
-            self.clearTimers()
-            self.setTimers()
+            self._clear_timers()
+            self._set_timers()
         return True
 
-    def timerControl(self):
+    def _timer_control(self):
         if app.player.paused:
-            self.clearTimers()
+            self._clear_timers()
         else:
-            self.setTimers()
+            self._set_timers()
         return False
 
-    def clearTimers(self):
+    def _clear_timers(self):
         curIndex = 0
         while curIndex < len(self._timers):
             GLib.source_remove(self._timers[curIndex][1])
             curIndex += 1
         self._timers = []
 
-    def plugin_on_song_started(self, song):
-        self.buildData()
-        #delay so that current position is for current track, not previous one
-        GLib.timeout_add(5, self.timerControl)
-
-    def plugin_on_song_ended(self, song, stopped):
-        self.clearTimers()
-
-    def plugin_on_paused(self):
-        self.timerControl()
-
     def _show(self, line):
         self.textbuffer.set_text(line)
         return False
 
+    def plugin_on_song_started(self, song):
+        self._build_data()
+        #delay so that current position is for current track, not previous one
+        GLib.timeout_add(5, self.timerControl)
+
+    def plugin_on_song_ended(self, song, stopped):
+        self._clear_timers()
+
+    def plugin_on_paused(self):
+        self._timer_control()
+
     def plugin_on_unpaused(self):
-        self.timerControl()
+        self._timer_control()
 
     def plugin_on_seek(self, song, msec):
         if not app.player.paused:
-            self.clearTimers()
-            self.setTimers()
+            self._clear_timers()
+            self._set_timers()
 
-    def greater(self, array, probe):
+    def _greater(self, array, probe):
         length = len(array)
         if length == 0:
             return -1
@@ -289,9 +291,9 @@ as the track.')
         elif probe >= array[length - 1][0]:
             return length
         else:
-            return self.search(array, probe, 0, length - 1)
+            return self._search(array, probe, 0, length - 1)
 
-    def search(self, array, probe, lower, upper):
+    def _search(self, array, probe, lower, upper):
         if lower == upper:
             if array[lower][0] <= probe:
                 return lower + 1
@@ -300,6 +302,6 @@ as the track.')
         else:
             middle = int((lower + upper) / 2)
             if array[middle][0] <= probe:
-                return self.search(array, probe, middle + 1, upper)
+                return self._search(array, probe, middle + 1, upper)
             else:
-                return self.search(array, probe, lower, middle)
+                return self._search(array, probe, lower, middle)
