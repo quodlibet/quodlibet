9b64d1ce78abd68cbb5b7d0f2e46388329d96815
Peter F. Patel-Schneider <pfpschneider@gmail.com>
2016-01-14 09:18:47 -0800

    Add space after , to make pep8 happy

--- quodlibet/quodlibet/qltk/songlist.py
+++ quodlibet/quodlibet/qltk/songlist.py
@@ -159,8 +159,8 @@ def get_sort_tag(tag):
         for key, value in replace_order.iteritems():
             tag = tag.replace("<%s>" % key, "<%s>" % value)
         for key, value in TAG_TO_SORT.iteritems():
-            tag = tag.replace("<%s>" % key,
-                              "<{1}|<{1}>|<{0}>>".format(key,value))
+            tag = tag.replace("<%s>" % key, 
+                              "<{1}|<{1}>|<{0}>>".format(key, value))
         tag = Pattern(tag).format
     else:
         tags = util.tagsplit(tag)
