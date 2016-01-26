caaf356c7c6a9955607439e5f0befdc9bb1b3cca
faubiguy <faubiguy0@gmail.com>
2016-01-26 06:24:25 -0600

    Fix error in numeric expression parsing

--- quodlibet/quodlibet/query/_parser.py
+++ quodlibet/quodlibet/query/_parser.py
@@ -165,7 +165,7 @@ class QueryParser(object):
         elif allow_date and self.accept_re(DATE):
             # Parse sequences of numbers that looks like dates as either dates
             # or numbers
-            return match.NumexprNumberOrDate(self.last_match)
+            expr = match.NumexprNumberOrDate(self.last_match)
         elif self.accept_re(DIGITS):
             number = float(self.last_match)
             if self.accept(':'):
