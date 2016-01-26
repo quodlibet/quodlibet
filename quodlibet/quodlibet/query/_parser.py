edb145d05194665be8e81445a14aa8a389d2c719
faubiguy <faubiguy0@gmail.com>
2016-01-26 06:16:03 -0600

    Fix date regex

--- quodlibet/quodlibet/query/_parser.py
+++ quodlibet/quodlibet/query/_parser.py
@@ -25,7 +25,7 @@ SINGLE_STRING = re.compile(r"([^'\\]|\\.)*")
 DOUBLE_STRING = re.compile(r'([^"\\]|\\.)*')
 MODIFIERS = re.compile(r'[cisld]*')
 TEXT = re.compile(r'[^=)|&#/<>!@,]+')
-DATE = re.compile(r'(\d{4})(?:\s*-\s*(\d{1,2})(?:\s*-\s*(\d{1,2}))?)?')
+DATE = re.compile(r'\d{4}(-\d{1,2}(-\d{1,2})?)?')
 
 
 class QueryParser(object):
