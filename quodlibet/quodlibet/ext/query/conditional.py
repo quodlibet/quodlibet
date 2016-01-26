26b343b78d38ba8b6346b84a475a0870561c9c8b
faubiguy <faubiguy0@gmail.com>
2016-01-25 20:00:41 -0600

    Add conditional query plugin

---
+++ quodlibet/quodlibet/ext/query/conditional.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from quodlibet.plugins.query import QueryPlugin, ParseError
+from quodlibet.query._parser import QueryParser
+
+class ConditionalQuery(QueryPlugin):
+    PLUGIN_ID = "conditional_query"
+    PLUGIN_NAME = _("Conditional Query")
+    PLUGIN_DESC _("Chooses the query to match based on a condition query. "
+                  "Syntax is '@(if: condition, then, else)'")
+    key = 'if'
+    
+    def search(self, data, body):
+        if body[0].search(data):
+            return body[1].search(data)
+        else:
+            return body[2].search(data)
+        
+    def parse_body(self, body):
+        parser = QueryParser(body)
+        queries = parser.match_list(parser.Query)
+        if len(queries) != 3:
+            raise ParseError
+        return queries
