1019b355bf0e38445ea01d164d34da19fda4222c
Christoph Reiter <reiter.christoph@gmail.com>
2016-02-01 12:14:41 +0100

    tests: fix TPot

--- quodlibet/quodlibet/ext/query/pythonexpression.py
+++ quodlibet/quodlibet/ext/query/pythonexpression.py
@@ -11,7 +11,7 @@ from quodlibet.plugins.query import QueryPlugin, QueryPluginError
 class PythonQuery(QueryPlugin):
     PLUGIN_ID = "python_query"
     PLUGIN_NAME = _("Python Query")
-    PLUGIN_DESC = _("Use python expressions in queries. Syntax is '@(python: "
+    PLUGIN_DESC = _("Use Python expressions in queries. Syntax is '@(python: "
                   "expression)'. The variable 's' is the song being matched.")
     key = 'python'
 
