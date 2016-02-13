8177cf53dbaa5277be994a2c415f9ce15f77c034
Christoph Reiter <reiter.christoph@gmail.com>
2016-02-13 02:55:50 +0100

    osx bundle: symlink the jhbuildrc files
    makes it easier to test things after bootstrap

--- osx_bundle/bootstrap.sh
+++ osx_bundle/bootstrap.sh
@@ -5,7 +5,7 @@ source env.sh
 mkdir -p "$HOME"
 git clone git://git.gnome.org/jhbuild _jhbuild
 (cd _jhbuild && ./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
-cp misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
-cp misc/quodlibet-jhbuildrc-custom "$HOME/.jhbuildrc-custom"
+ln -s misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
+ln -s misc/quodlibet-jhbuildrc-custom "$HOME/.jhbuildrc-custom"
 git clone git://git.gnome.org/gtk-mac-bundler _bundler
 (cd _bundler && make install)
