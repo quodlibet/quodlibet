037fc2d47c2e306197ce4ff850d928cc13555c6e
CreamyCookie <CreamyCookie@users.noreply.github.com>
2016-02-18 19:40:50 +0100

    Media playback in the WebView now requires user interaction

--- quodlibet/quodlibet/ext/events/lyricswindow.py
+++ quodlibet/quodlibet/ext/events/lyricswindow.py
@@ -146,6 +146,7 @@ class LyricsWebViewWindow(Window):
             ("Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) "
              "AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0"
              "Chrome/43.0.2357.65 Mobile Safari/537.36"))
+        settings.set_media_playback_requires_user_gesture(True)
 
         def scroll_tp_lyrics(view, load_event):
             if load_event != WebKit2.LoadEvent.COMMITTED:
