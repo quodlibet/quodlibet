c7b23ff9954d95d48d7ef55a13e8f32ff1da1d43
Nick B <nick@declension.net>
2016-03-19 17:44:52 +0000

    Songrwapper: log when (force) writing goes wrong.

--- quodlibet/quodlibet/util/songwrapper.py
+++ quodlibet/quodlibet/util/songwrapper.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2005 Michael Urman
+#           2016 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -97,13 +98,14 @@ def check_wrapper_changed(library, parent, songs):
         for song in needs_write:
             try:
                 song._song.write()
-            except Exception:
+            except Exception as e:
                 qltk.ErrorMessage(
                     None, _("Unable to edit song"),
                     _("Saving <b>%s</b> failed. The file "
                       "may be read-only, corrupted, or you "
                       "do not have permission to edit it.") %
                     util.escape(song('~basename'))).run()
+                print_d("Couldn't save song %s (%s)" % (song("~filename"), e))
 
             if win.step():
                 break
