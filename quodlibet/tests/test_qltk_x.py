a8bb9ee28c0f20f6182f7568dfd766644c083aa1
Christoph Reiter <reiter.christoph@gmail.com>
2016-08-01 08:24:08 +0200

    rpaned: make the relative position independent of the child min size
    max-position depends on the min size of all children if they are
    packed to not shrink. As we want to save and restore the relative
    position independend of which child is visible use the allocation
    instead.

--- quodlibet/tests/test_qltk_x.py
+++ quodlibet/tests/test_qltk_x.py
@@ -3,14 +3,12 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
 
-from tests import TestCase
-from .helper import visible
-
 from gi.repository import Gtk
 
 from quodlibet.qltk import x
 from quodlibet.qltk import Icons
-from quodlibet import config
+
+from . import TestCase
 
 
 class Notebook(TestCase):
@@ -51,70 +49,6 @@ class MenuItem(TestCase):
 class Button(TestCase):
     def test_ctr(self):
         self.failUnless(x.Button("foo", Icons.EDIT_FIND))
-
-
-class RPaned(object):
-    Kind = None
-
-    def test_ctr(self):
-        self.Kind().destroy()
-
-    def test_pre_alloc(self):
-        p = self.Kind()
-        p.set_relative(0.25)
-        self.failUnlessEqual(p.get_relative(), 0.25)
-
-    def test_visible_no_setup(self):
-        p = self.Kind()
-        with visible(p):
-            pass
-
-    def test_visible_pre_setup_children(self):
-        p = self.Kind()
-        p.pack1(Gtk.Button())
-        p.pack2(Gtk.Button())
-        p.set_relative(0.75)
-        self.failUnlessAlmostEqual(p.get_relative(), 0.75)
-        with visible(p, width=200, height=200) as p:
-            self.failUnlessAlmostEqual(p.get_relative(), 0.75, 2)
-
-    def test_visible_pre_setup_empty(self):
-        p = self.Kind()
-        p.set_relative(0.75)
-        self.failUnlessEqual(p.get_relative(), 0.75)
-        with visible(p) as p:
-            self.failUnlessAlmostEqual(p.get_relative(), 0.75, 2)
-
-
-class RHPaned(TestCase, RPaned):
-    Kind = x.RHPaned
-
-
-class RVPaned(TestCase, RPaned):
-    Kind = x.RVPaned
-
-
-class TConfigRPaned(TestCase):
-    def setUp(self):
-        config.init()
-
-    def tearDown(self):
-        config.quit()
-
-    def test_basic(self):
-        self.failUnless(config.get("memory", "foobar", None) is None)
-
-        p = x.ConfigRVPaned("memory", "foobar", 0.75)
-        p.pack1(Gtk.Button())
-        p.pack2(Gtk.Button())
-
-        with visible(p, width=200, height=200) as p:
-            self.failUnlessAlmostEqual(p.get_relative(), 0.75, 2)
-            p.props.position = 20
-            self.failUnlessAlmostEqual(p.get_relative(), 0.10, 2)
-
-        config_value = config.getfloat("memory", "foobar")
-        self.failUnlessAlmostEqual(config_value, 0.10, 2)
 
 
 class TAlign(TestCase):
