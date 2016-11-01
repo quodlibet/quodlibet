1f5ede18ebd37383fb54e73c4089409da746c596
Christoph Reiter <reiter.christoph@gmail.com>
2016-11-01 09:57:51 +0100

    tests: some windows filesystem related fixes
    Only use allowed filenames; Close files before trying to delete them
    This sometimes worked under wine, but not under real Windows.

--- quodlibet/tests/test_util_cover.py
+++ quodlibet/tests/test_util_cover.py
@@ -6,21 +6,16 @@
 
 import glob
 import os
+import shutil
 
 from senf import fsnative
 
-from quodlibet import config
 from quodlibet.formats import AudioFile
 from quodlibet.util.cover.manager import CoverManager
 from quodlibet.util.path import normalize_path
 
-from tests import TestCase, DATA_DIR
+from tests import TestCase, mkdtemp
 
-
-quux = AudioFile({
-    "~filename": os.path.join(DATA_DIR, "asong.ogg"),
-    "album": u"Quuxly",
-})
 
 bar_2_1 = AudioFile({
     "~filename": fsnative(u"does not/exist"),
@@ -34,25 +29,25 @@ bar_2_1 = AudioFile({
 class TCoverManager(TestCase):
 
     def setUp(self):
-        config.init()
         self.manager = CoverManager()
 
-        self.dir = os.path.realpath(quux("~dirname"))
+        self.dir = mkdtemp()
+        self.song = AudioFile({
+            "~filename": os.path.join(self.dir, "asong.ogg"),
+            "album": u"Quuxly",
+        })
+
         # Safety check
-        self.failIf(glob.glob(self.dir + "/*.jpg"))
-        self.files = [self.full_path("12345.jpg"),
-                      self.full_path("nothing.jpg")
-                      ]
-        for f in self.files:
+        self.failIf(glob.glob(os.path.join(self.dir + "*.jpg")))
+        files = [self.full_path("12345.jpg"), self.full_path("nothing.jpg")]
+        for f in files:
             open(f, "w").close()
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
 
     def _find_cover(self, song):
         return self.manager.get_cover(song)
-
-    def tearDown(self):
-        for f in self.files:
-            os.unlink(f)
-        config.quit()
 
     def full_path(self, path):
         return os.path.join(self.dir, path)
@@ -61,20 +56,20 @@ class TCoverManager(TestCase):
         self.failIf(self._find_cover(bar_2_1))
 
     def test_nothing(self):
-        self.failIf(self._find_cover(quux))
+        self.failIf(self._find_cover(self.song))
 
     def test_labelid(self):
-        quux["labelid"] = "12345"
-        self.failUnlessEqual(os.path.abspath(self._find_cover(quux).name),
+        self.song["labelid"] = "12345"
+        self.failUnlessEqual(os.path.abspath(self._find_cover(self.song).name),
                              self.full_path("12345.jpg"))
-        del(quux["labelid"])
+        del(self.song["labelid"])
 
     def test_regular(self):
         for fn in ["cover.png", "folder.jpg", "frontcover.jpg",
                    "front_folder_cover.gif", "jacket_cover.front.folder.jpeg"]:
             f = self.add_file(fn)
             self.failUnlessEqual(
-                os.path.abspath(self._find_cover(quux).name), f)
+                os.path.abspath(self._find_cover(self.song).name), f)
         self.test_labelid() # labelid must work with other files present
 
     def test_file_encoding(self):
@@ -82,12 +77,12 @@ class TCoverManager(TestCase):
             return
 
         f = self.add_file("\xff\xff\xff\xff - cover.jpg")
-        self.assertTrue(isinstance(quux("album"), unicode))
-        h = self._find_cover(quux)
+        self.assertTrue(isinstance(self.song("album"), unicode))
+        h = self._find_cover(self.song)
         self.assertEqual(h.name, normalize_path(f))
 
     def test_intelligent(self):
-        song = quux
+        song = self.song
         song["artist"] = "Q-Man"
         song["title"] = "First Q falls hardest"
         fns = ["Quuxly - back.jpg", "Quuxly.jpg", "q-man - quxxly.jpg",
@@ -135,12 +130,11 @@ class TCoverManager(TestCase):
     def add_file(self, fn):
         f = self.full_path(fn)
         open(f, "w").close()
-        self.files.append(f)
         return f
 
     def test_multiple_people(self):
         song = AudioFile({
-            "~filename": fsnative(os.path.join(self.dir, u"asong.ogg")),
+            "~filename": os.path.join(self.dir, "asong.ogg"),
             "album": "foobar",
             "title": "Ode to Baz",
             "performer": "The Performer",
@@ -149,15 +143,17 @@ class TCoverManager(TestCase):
         })
         for fn in ["foobar.jpg",
                    "The Performer - foobar.jpg",
-                   "The Composer: The Performer - foobar.jpg",
-                   "The Composer: The Conductor, The Performer - foobar.jpg"]:
+                   "The Composer - The Performer - foobar.jpg",
+                   "The Composer - The Conductor, The Performer - foobar.jpg"]:
             f = self.add_file(fn)
             cover = self._find_cover(song)
             self.failUnless(cover)
             actual = os.path.abspath(cover.name)
+            cover.close()
             self.failUnlessEqual(
                     actual, f, "\"%s\" should trump \"%s\"" % (f, actual))
 
     def test_get_thumbnail(self):
-        self.assertTrue(self.manager.get_pixbuf(quux, 10, 10) is None)
-        self.assertTrue(self.manager.get_pixbuf_many([quux], 10, 10) is None)
+        self.assertTrue(self.manager.get_pixbuf(self.song, 10, 10) is None)
+        self.assertTrue(
+            self.manager.get_pixbuf_many([self.song], 10, 10) is None)
