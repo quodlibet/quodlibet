aedd1a78e1521c908a53111c5f986f3286af889e
Christoph Reiter <reiter.christoph@gmail.com>
2016-11-04 12:58:03 +0100

    osx bundle: include setuptools

--- osx_bundle/bundle.sh
+++ osx_bundle/bundle.sh
@@ -19,11 +19,10 @@ git clone ../ "$CLONE"
 if [ $# -eq 1 ]; then
     (cd "$CLONE"; git checkout "$1";)
 fi
-jhbuild run "$CLONE"/quodlibet/setup.py install --prefix="$APP_PREFIX" --record="$QL_OSXBUNDLE_BUNDLE_DEST"/_install_log.txt
+jhbuild run "$CLONE"/quodlibet/setup.py install --old-and-unmanageable --prefix="$APP_PREFIX" --record="$QL_OSXBUNDLE_BUNDLE_DEST"/_install_log.txt
 rm -Rf "$CLONE"
 
 # kill some useless files
-rm -f "$APP_PREFIX"/lib/python2.7/site-packages/*.egg-info
 rm -f "$APP_PREFIX"/lib/python2.7/config/libpython2.7.a
 rm -Rf "$APP_PREFIX"/lib/python2.7/*/test
 rm -f "$APP"/Contents/MacOS/_launcher-bin
