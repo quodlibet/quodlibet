fdc30c21391af5f42be8d9cc25bd199225ceb7e1
Jason Heard <jasonpheard@gmail.com>
2017-01-15 10:01:11 -0700

    Add configuration options

--- quodlibet/quodlibet/ext/playorder/skip_zeros.py
+++ quodlibet/quodlibet/ext/playorder/skip_zeros.py
@@ -1,24 +1,55 @@
 # -*- coding: utf-8 -*-
-# Copyright 2010 Christoph Reiter
-#           2016 Nick Boultbee
+# Copyright 2016 Nick Boultbee
 #           2017 Jason Heard
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation.
 
-from quodlibet import _, print_d
+from gi.repository import Gtk, Pango
+
+from quodlibet import _, print_d, qltk
 from quodlibet.order import OrderInOrder
+from quodlibet.plugins import PluginConfigMixin
 from quodlibet.plugins.playorder import ShufflePlugin
 from quodlibet.qltk import Icons
+from quodlibet.qltk.ccb import ConfigCheckButton
 
 
-class SkipZeros(ShufflePlugin, OrderInOrder):
-    PLUGIN_ID = "skip_zeros"
-    PLUGIN_NAME = _("Skip Zero Rated")
+class SkipZeros(ShufflePlugin, OrderInOrder, PluginConfigMixin):
+    PLUGIN_ID = "skip_songs"
+    PLUGIN_NAME = _("Skip Songs")
     PLUGIN_ICON = Icons.GO_JUMP
-    PLUGIN_DESC = _("Playback skips over songs with a rating of zero unless "
-                    "explicitly played.")
+    PLUGIN_DESC = _("Playback skips over songs with a rating of zero or with "
+                    "a non-empty 'skip' tag unless they are explicitly "
+                    "played.")
+
+
+    _CFG_SKIP_BY_RATING = 'skip_by_rating'
+    _CFG_SKIP_BY_TAG = 'skip_by_tag'
+
+
+    @classmethod
+    def PluginPreferences(self, window):
+        vb = Gtk.VBox(spacing=10)
+        vb.set_border_width(0)
+
+        # Matching Option
+        toggles = [
+            (self._CFG_SKIP_BY_RATING, _("Skip Songs with _Rating of Zero")),
+            (self._CFG_SKIP_BY_TAG, _("Skip Songs with a Non-Empty 'skip' _Tag")),
+        ]
+        vb2 = Gtk.VBox(spacing=6)
+        for key, label in toggles:
+            ccb = ConfigCheckButton(label, 'plugins', self._config_key(key))
+            ccb.set_active(self.config_get_bool(key))
+            vb2.pack_start(ccb, True, True, 0)
+
+        frame = qltk.Frame(label=_("Skipping options"), child=vb2)
+        vb.pack_start(frame, True, True, 0)
+
+        vb.show_all()
+        return vb
 
 
     def next(self, playlist, current):
@@ -45,10 +76,10 @@ class SkipZeros(ShufflePlugin, OrderInOrder):
         rating = song("~#rating")
 
         shouldSkip = False
-        if rating <= 0:
+        if self.config_get_bool(self._CFG_SKIP_BY_RATING) and rating <= 0:
             shouldSkip = True
             print_d("Rating is %f; skipping..." % (rating))
-        elif song("skip") <> '':
+        elif self.config_get_bool(self._CFG_SKIP_BY_TAG) and song("skip") <> '':
             shouldSkip = True
             print_d("Skip tag present; skipping...")
 
