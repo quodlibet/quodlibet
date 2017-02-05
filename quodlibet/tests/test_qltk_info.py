20807a01cf581e0783a6428fbf6c6dd9834d1374
Nick B <nick@declension.net>
2017-02-05 16:51:07 +0000

    PY3: write bytes to display pattern
     * Write all bytes in PY3, not a mixture of `str` / `bytes`
     * Also trap and warn about any errors, useful if say it deletes your pattern by not saving...
     * Add a test to exercise this part of the code
    
     Fixes #2219

--- quodlibet/tests/test_qltk_info.py
+++ quodlibet/tests/test_qltk_info.py
@@ -3,19 +3,38 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
 
-from tests import TestCase
+import os
+
+from quodlibet import app
+from tests import TestCase, destroy_fake_app, init_fake_app
 
 from quodlibet.player.nullbe import NullPlayer
 from quodlibet.qltk.info import SongInfo
 from quodlibet.library import SongLibrary
 
+FILENAME = "test-pattern"
+SOME_PATTERN = "foo\n[big]<title>[/big] - <artist>"
+
+
+class FakePatternEdit(object):
+    @property
+    def text(self):
+        return SOME_PATTERN
+
 
 class TSongInfo(TestCase):
     def setUp(self):
-        self.info = SongInfo(SongLibrary(), NullPlayer(), "")
+        init_fake_app()
+        self.info = SongInfo(SongLibrary(), NullPlayer(), FILENAME)
 
-    def test_ctr(self):
-        pass
+    def test_save(self):
+        fake_edit = FakePatternEdit()
+        self.info._on_set_pattern(None, fake_edit, app.player)
+        with open(FILENAME, "r") as f:
+            contents = f.read()
+            self.failUnlessEqual(contents, SOME_PATTERN + "\n")
+        os.unlink(FILENAME)
 
     def tearDown(self):
+        destroy_fake_app()
         self.info.destroy()
