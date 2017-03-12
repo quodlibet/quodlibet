67c1fafeb7559f321b738c4bfe8c468608039ad6
Fredrik Strupe <fredrik@strupe.net>
2017-03-12 17:46:10 +0100

    Update copyright header for qltk/paned.py

--- quodlibet/quodlibet/qltk/paned.py
+++ quodlibet/quodlibet/qltk/paned.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2005 Joe Wreschnig, Michael Urman
+#           2017 Fredrik Strupe
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
