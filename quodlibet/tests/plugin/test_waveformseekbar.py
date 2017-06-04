04b93e499a72886663bf45e6c2c4c91da37bd18d
Christoph Reiter <reiter.christoph@gmail.com>
2017-06-04 13:07:44 +0200

    tests: Add a failing test for waveformseekbar. See #2387

---
+++ quodlibet/tests/plugin/test_waveformseekbar.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Christoph Reiter
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from unittest import expectedFailure
+
+from tests.plugin import PluginTestCase
+from tests.helper import visible
+
+from quodlibet.player.nullbe import NullPlayer
+from quodlibet.formats import AudioFile
+
+
+class TWaveformSeekBar(PluginTestCase):
+
+    def setUp(self):
+        self.mod = self.modules["WaveformSeekBar"]
+
+    def tearDown(self):
+        del self.mod
+
+    @expectedFailure
+    def test_main(self):
+        WaveformScale = self.mod.WaveformScale
+
+        player = NullPlayer()
+        player.info = AudioFile({"~#length": 10})
+        scale = WaveformScale(player)
+        scale.compute_redraw_interval()
+        scale.compute_redraw_area()
+
+        with visible(scale):
+            scale.compute_redraw_interval()
+            scale.compute_redraw_area()
