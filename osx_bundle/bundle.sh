d50db37436f8fe687165b131a58ad5f207490e98
Christoph Reiter <reiter.christoph@gmail.com>
2017-07-03 18:51:25 +0200

    macOS bundle: include the QL source for sentry; remove some icon themes to save space

--- osx_bundle/bundle.sh
+++ osx_bundle/bundle.sh
@@ -1,68 +1,106 @@
 #!/bin/bash
+# Copyright 2016,2017 Christoph Reiter
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
-# pass the tag/revision to install, or install the current revision
+# Pass the tag/revision to install, or install the current revision
 # if nothing is passed
 
 set -e
 
 source env.sh
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-jhbuild run gtk-mac-bundler misc/bundle/app.bundle
 
-APP="$QL_OSXBUNDLE_BUNDLE_DEST/Application.app"
-APP_PREFIX="$APP"/Contents/Resources
-DMG_SETTINGS="misc/dmg_settings.py"
+function main {
+    local GIT_TAG=${1:-"master"}
 
-# clone this repo and install into the bundle
-CLONE="$QL_OSXBUNDLE_BUNDLE_DEST"/_temp_clone
-git clone ../ "$CLONE"
-if [ $# -eq 1 ]; then
-    (cd "$CLONE"; git checkout "$1";)
-fi
-jhbuild run "$CLONE"/quodlibet/setup.py install --prefix="$APP_PREFIX" --record="$QL_OSXBUNDLE_BUNDLE_DEST"/_install_log.txt
-rm -Rf "$CLONE"
+    jhbuild run gtk-mac-bundler misc/bundle/app.bundle
 
-# kill some useless files
-rm -f "$APP_PREFIX"/lib/python2.7/config/libpython2.7.a
-rm -Rf "$APP_PREFIX"/lib/python2.7/*/test
-rm -f "$APP"/Contents/MacOS/_launcher-bin
-rm -Rf "$APP_PREFIX"/include/
-./misc/prune_translations.py "$APP_PREFIX"/share/locale
+    APP="$QL_OSXBUNDLE_BUNDLE_DEST/Application.app"
+    APP_PREFIX="$APP"/Contents/Resources
 
-# only keep *.pyc
-find "$APP_PREFIX"/lib/python2.7 -name '*.pyo' -delete
+    # kill some useless files
+    rm -f "$APP_PREFIX"/lib/python2.7/config/libpython2.7.a
+    rm -Rf "$APP_PREFIX"/lib/python2.7/*/test
+    rm -f "$APP"/Contents/MacOS/_launcher-bin
+    rm -Rf "$APP_PREFIX"/include/
+    find "$APP_PREFIX"/lib/python2.7 -name '*.pyo' -delete
+    find "$APP_PREFIX"/lib/python2.7 -name '*.pyc' -delete
 
-(cd "$APP"/Contents/MacOS/ && ln -s _launcher quodlibet)
-(cd "$APP"/Contents/MacOS/ && ln -s _launcher exfalso)
-(cd "$APP"/Contents/MacOS/ && ln -s _launcher operon)
-(cd "$APP"/Contents/MacOS/ && ln -s _launcher run)
-(cd "$APP"/Contents/MacOS/ && ln -s _launcher gst-plugin-scanner)
+    # remove some larger icon theme files
+    rm -Rf "${APP_PREFIX}/share/icons/Adwaita/cursors"
+    rm -Rf "${APP_PREFIX}/share/icons/Adwaita/512x512"
+    rm -Rf "${APP_PREFIX}/share/icons/Adwaita/256x256"
+    rm -Rf "${APP_PREFIX}/share/icons/Adwaita/96x96"
+    rm -Rf "${APP_PREFIX}/share/icons/Adwaita/48x48"
+    jhbuild run gtk-update-icon-cache "${APP_PREFIX}/share/icons/Adwaita"
 
-EXFALSO="$QL_OSXBUNDLE_BUNDLE_DEST/ExFalso.app"
-EXFALSO_PREFIX="$EXFALSO"/Contents/Resources
-QUODLIBET="$QL_OSXBUNDLE_BUNDLE_DEST/QuodLibet.app"
-QUODLIBET_PREFIX="$QUODLIBET"/Contents/Resources
+    # compile the stdlib
+    jhbuild run python -m compileall -d "" -f "$APP_PREFIX"/lib/python2.7
+    # delete stdlib source
+    find "$APP_PREFIX"/lib/python2.7 -name '*.py' -delete
 
-cp -R "$APP" "$EXFALSO"
-mv "$APP" "$QUODLIBET"
+    # clone this repo and install into the bundle
+    CLONE="$QL_OSXBUNDLE_BUNDLE_DEST"/_temp_clone
+    git clone ../ "$CLONE"
+    (cd "$CLONE"; git checkout "$GIT_TAG")
+    jhbuild run "$CLONE"/quodlibet/setup.py install --prefix="$APP_PREFIX" \
+        --record="$QL_OSXBUNDLE_BUNDLE_DEST"/_install_log.txt
+    rm -Rf "$CLONE"
 
-echo 'BUILD_TYPE = u"osx-exfalso"' >> "$EXFALSO_PREFIX"/lib/python2.7/site-packages/quodlibet/build.py
-echo 'BUILD_TYPE = u"osx-quodlibet"' >> "$QUODLIBET_PREFIX"/lib/python2.7/site-packages/quodlibet/build.py
+    jhbuild run python ./misc/prune_translations.py "$APP_PREFIX"/share/locale
 
-jhbuild run python -m compileall -d "" -f "$EXFALSO_PREFIX"/lib/python2.7
-jhbuild run python -m compileall -d "" -f "$QUODLIBET_PREFIX"/lib/python2.7
-find "$EXFALSO_PREFIX"/lib/python2.7 -name '*.py' -delete
-find "$QUODLIBET_PREFIX"/lib/python2.7 -name '*.py' -delete
+    # create launchers
+    (cd "$APP"/Contents/MacOS/ && ln -s _launcher quodlibet)
+    (cd "$APP"/Contents/MacOS/ && ln -s _launcher exfalso)
+    (cd "$APP"/Contents/MacOS/ && ln -s _launcher operon)
+    (cd "$APP"/Contents/MacOS/ && ln -s _launcher run)
+    (cd "$APP"/Contents/MacOS/ && ln -s _launcher gst-plugin-scanner)
 
-VERSION=$("$QUODLIBET"/Contents/MacOS/run -c "import sys;import quodlibet.const;sys.stdout.write(quodlibet.const.VERSION)")
-./misc/create_info.py "quodlibet" "$VERSION" > "$QUODLIBET"/Contents/Info.plist
-./misc/create_info.py "exfalso" "$VERSION" > "$EXFALSO"/Contents/Info.plist
+    EXFALSO="$QL_OSXBUNDLE_BUNDLE_DEST/ExFalso.app"
+    EXFALSO_PREFIX="$EXFALSO"/Contents/Resources
+    QUODLIBET="$QL_OSXBUNDLE_BUNDLE_DEST/QuodLibet.app"
+    QUODLIBET_PREFIX="$QUODLIBET"/Contents/Resources
 
-./misc/list_content.py "$HOME/jhbuild_prefix" "$QUODLIBET" > "$QUODLIBET/Contents/Resources/content.txt"
-./misc/list_content.py "$HOME/jhbuild_prefix" "$EXFALSO" > "$EXFALSO/Contents/Resources/content.txt"
+    cp -R "$APP" "$EXFALSO"
+    mv "$APP" "$QUODLIBET"
 
-jhbuild run dmgbuild -s "$DMG_SETTINGS" -D app="$QUODLIBET" "Quod Libet $VERSION" "$QL_OSXBUNDLE_BUNDLE_DEST/QuodLibet-$VERSION.dmg"
-jhbuild run dmgbuild -s "$DMG_SETTINGS" -D app="$EXFALSO" "Ex Falso $VERSION" "$QL_OSXBUNDLE_BUNDLE_DEST/ExFalso-$VERSION.dmg"
+    echo 'BUILD_TYPE = u"osx-exfalso"' >> \
+        "$EXFALSO_PREFIX"/lib/python2.7/site-packages/quodlibet/build.py
+    echo 'BUILD_TYPE = u"osx-quodlibet"' >> \
+        "$QUODLIBET_PREFIX"/lib/python2.7/site-packages/quodlibet/build.py
 
-(cd "$QL_OSXBUNDLE_BUNDLE_DEST" && shasum -a256 "QuodLibet-$VERSION.dmg" > "QuodLibet-$VERSION.dmg.sha256")
-(cd "$QL_OSXBUNDLE_BUNDLE_DEST" && shasum -a256 "ExFalso-$VERSION.dmg" > "ExFalso-$VERSION.dmg.sha256")
+    # force compile again to get relative paths in pyc files and for the
+    # modified files
+    jhbuild run python -m compileall -d "" -f "$EXFALSO_PREFIX"/lib/python2.7
+    jhbuild run python -m compileall -d "" -f "$QUODLIBET_PREFIX"/lib/python2.7
+
+    VERSION=$("$QUODLIBET"/Contents/MacOS/run -c \
+        "import sys, quodlibet.const;sys.stdout.write(quodlibet.const.VERSION)")
+    jhbuild run python ./misc/create_info.py "quodlibet" "$VERSION" > \
+        "$QUODLIBET"/Contents/Info.plist
+    jhbuild run python ./misc/create_info.py "exfalso" "$VERSION" > \
+        "$EXFALSO"/Contents/Info.plist
+
+    jhbuild run python ./misc/list_content.py "$HOME/jhbuild_prefix" \
+        "$QUODLIBET" > "$QUODLIBET/Contents/Resources/content.txt"
+    jhbuild run python ./misc/list_content.py "$HOME/jhbuild_prefix" \
+        "$EXFALSO" > "$EXFALSO/Contents/Resources/content.txt"
+
+    DMG_SETTINGS="misc/dmg_settings.py"
+    jhbuild run dmgbuild -s "$DMG_SETTINGS" -D app="$QUODLIBET" \
+        "Quod Libet $VERSION" "$QL_OSXBUNDLE_BUNDLE_DEST/QuodLibet-$VERSION.dmg"
+    jhbuild run dmgbuild -s "$DMG_SETTINGS" -D app="$EXFALSO" \
+        "Ex Falso $VERSION" "$QL_OSXBUNDLE_BUNDLE_DEST/ExFalso-$VERSION.dmg"
+
+    (cd "$QL_OSXBUNDLE_BUNDLE_DEST" && \
+        shasum -a256 "QuodLibet-$VERSION.dmg" > "QuodLibet-$VERSION.dmg.sha256")
+    (cd "$QL_OSXBUNDLE_BUNDLE_DEST" && \
+        shasum -a256 "ExFalso-$VERSION.dmg" > "ExFalso-$VERSION.dmg.sha256")
+}
+
+main "$@";
