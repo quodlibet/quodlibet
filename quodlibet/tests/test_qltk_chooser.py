3cbd75137d3d9811c70fc669bf28b9eba3762201
Christoph Reiter <reiter.christoph@gmail.com>
2017-08-08 20:17:42 +0200

    tests: skip Tchooser on macOS
    it crashes from time to time

--- quodlibet/tests/test_qltk_chooser.py
+++ quodlibet/tests/test_qltk_chooser.py
@@ -13,11 +13,12 @@ from quodlibet.qltk.chooser import choose_files, get_current_dir, \
     set_current_dir, choose_folders, create_chooser_filter, \
     choose_target_file, choose_target_folder, with_response
 from quodlibet.qltk import gtk_version
+from quodlibet.util import is_osx
 
 from . import TestCase, skipIf
 
 
-@skipIf(gtk_version < (3, 16, 0), "crashy on older gtk+")
+@skipIf(gtk_version < (3, 16, 0) or is_osx(), "crashy on older gtk+ and macOS")
 class Tchooser(TestCase):
 
     def test_choose_files(self):
