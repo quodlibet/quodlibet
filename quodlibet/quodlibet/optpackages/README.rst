f2ad3c87080058cfba54ab871da832ee4acef90e
Christoph Reiter <reiter.christoph@gmail.com>
2017-11-04 12:02:35 +0100

    optpackages: Remove contextlib2; was only needed on py2

--- quodlibet/quodlibet/optpackages/README.rst
+++ quodlibet/quodlibet/optpackages/README.rst
@@ -3,10 +3,6 @@ and don't get distributed in the source tarball.
 
 Don't change anything here as these are just copies of upstream releases.
 
-contextlib2:
-    PYPI: https://pypi.python.org/pypi/contextlib2
-    License: "Python Software Foundation License"
-
 raven:
     PYPI: https://pypi.python.org/pypi/raven
     License: "BSD 3-Clause License"
--- quodlibet/quodlibet/optpackages/update.sh
+++ quodlibet/quodlibet/optpackages/update.sh
@@ -2,11 +2,6 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-rm -f "contextlib2.py"
-pip install --no-compile --no-deps --target="$DIR/tmp" "contextlib2==0.5.5"
-mv "$DIR/tmp/contextlib2.py" "$DIR"
-rm -R "$DIR/tmp"
-
 rm -Rf "raven"
 pip install --no-compile --no-deps --target="$DIR/tmp" "git+https://github.com/getsentry/raven-python.git@005d7fb0238a598529f85d11ddf272d2214408d8"
 mv "$DIR/tmp/raven" "$DIR"
