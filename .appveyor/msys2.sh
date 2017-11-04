08e71e385410b9f945cea94409c792f2405727cd
Christoph Reiter <reiter.christoph@gmail.com>
2017-11-04 16:05:37 +0100

    Integrate appveyor

---
+++ .appveyor/msys2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+export MSYS2_FC_CACHE_SKIP=1
+
+pacman --noconfirm -Suy
+
+pacman --noconfirm -S --needed \
+    git \
+    intltool \
+    mingw-w64-$MSYS2_ARCH-gdk-pixbuf2 \
+    mingw-w64-$MSYS2_ARCH-librsvg \
+    mingw-w64-$MSYS2_ARCH-gtk3 \
+    mingw-w64-$MSYS2_ARCH-libsoup \
+    mingw-w64-$MSYS2_ARCH-gstreamer \
+    mingw-w64-$MSYS2_ARCH-gst-plugins-base \
+    mingw-w64-$MSYS2_ARCH-gst-plugins-good \
+    mingw-w64-$MSYS2_ARCH-gst-plugins-bad \
+    mingw-w64-$MSYS2_ARCH-gst-libav \
+    mingw-w64-$MSYS2_ARCH-gst-plugins-ugly \
+    mingw-w64-$MSYS2_ARCH-python3 \
+    mingw-w64-$MSYS2_ARCH-python3-gobject \
+    mingw-w64-$MSYS2_ARCH-python3-cairo \
+    mingw-w64-$MSYS2_ARCH-python3-pip \
+    mingw-w64-$MSYS2_ARCH-python3-pytest \
+    mingw-w64-$MSYS2_ARCH-python3-certifi \
+    mingw-w64-$MSYS2_ARCH-python3-coverage
+
+pip3 install feedparser musicbrainzngs mutagen pycodestyle pyflakes codecov
+
+cd quodlibet
+MSYSTEM= python3 -R -bb -m coverage run --branch setup.py test
+MSYSTEM= python3 -m codecov
