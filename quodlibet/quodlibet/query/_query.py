a26437f49bb3515c8be0bb7aa603a8745897aa2c
Christoph Reiter <reiter.christoph@gmail.com>
2017-11-10 10:28:42 +0100

    More relicensing. See #2276

--- quodlibet/quodlibet/query/_query.py
+++ quodlibet/quodlibet/query/_query.py
@@ -4,8 +4,9 @@
 #                2016 Ryan Dellenbaugh
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
 from quodlibet import print_d
 from quodlibet.util.dprint import frame_info
