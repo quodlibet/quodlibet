9cb58eb1764ef8f5bed9e707e6927c2eafe8c4df
Christoph Reiter <reiter.christoph@gmail.com>
2017-11-16 16:36:33 +0100

    Relicensing progress

--- quodlibet/quodlibet/browsers/covergrid/main.py
+++ quodlibet/quodlibet/browsers/covergrid/main.py
@@ -5,8 +5,9 @@
 #           2009-2014 Christoph Reiter
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
 from __future__ import absolute_import
 
