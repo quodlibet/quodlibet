3cf4e9232b880d5365537de8781e5b57a5f5faf3
Christoph Reiter <reiter.christoph@gmail.com>
2018-01-23 14:08:11 +0100

    sync senf

--- quodlibet/quodlibet/packages/update.sh
+++ quodlibet/quodlibet/packages/update.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 rm -Rf "senf"
-pip install --system --no-compile --no-deps --target="$DIR/tmp" "senf==1.3.2"
+pip install --system --no-compile --no-deps --target="$DIR/tmp" "senf==1.3.4"
 mv "$DIR/tmp/senf" "$DIR"
 rm -R "$DIR/tmp"
 
