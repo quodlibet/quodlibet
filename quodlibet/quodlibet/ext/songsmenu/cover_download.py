032cf3996322a0ee4459f168e42e74028b1c489b
Nick B <nick@declension.net>
2018-03-03 19:19:17 +0000

    Cover Art Downloader: improved UI
     * Move to using a Dialog (with headerbar support)
     * Real-time preview resizing now supported
     * Improve layout / padding a bit
     * Still no actual saving though

--- quodlibet/quodlibet/ext/songsmenu/cover_download.py
+++ quodlibet/quodlibet/ext/songsmenu/cover_download.py
@@ -12,7 +12,7 @@ from quodlibet import _, app, print_d, print_w
 from quodlibet import qltk
 from quodlibet.plugins.songshelpers import any_song, is_a_file
 from quodlibet.plugins.songsmenu import SongsMenuPlugin
-from quodlibet.qltk import Icons, Button
+from quodlibet.qltk import Icons
 from quodlibet.qltk.paned import ConfigRVPaned
 from quodlibet.util import connect_destroy, format_size, escape
 from quodlibet.util.http import session
@@ -24,15 +24,16 @@ class ResizeWebImage(Gtk.Image):
         'info-known': (GObject.SignalFlags.RUN_LAST, None, (int, object))
     }
 
-    def __init__(self, url, preview_size=400, cancellable=None):
+    def __init__(self, url, preview_size, cancellable=None):
         super().__init__()
         self.preview_size = preview_size
         self.url = url
         self.cancellable = cancellable
         self.message = msg = Soup.Message.new('GET', self.url)
         session.send_async(msg, self.cancellable, self._sent, None)
-
-        self.set_size_request(preview_size + 24, preview_size + 24)
+        # TODO: drop this sizing hack
+        self.set_size_request(preview_size, preview_size)
+        self._pixbuf = None
 
     def _sent(self, session, task, data):
         try:
@@ -54,83 +55,141 @@ class ResizeWebImage(Gtk.Image):
             print_w('Failed sending {method} request to {uri} ({err})'.format(
                 method=self.message.method, uri=self.url, err=e))
 
-    def _finished(self, istream, result):
-        self._pixbuf = pb = GdkPixbuf.Pixbuf.new_from_stream_finish(result)
+    def resize(self, new_size=None):
+        if not self._pixbuf:
+            return
+        if new_size:
+            self.preview_size = new_size
+        pb = self._pixbuf
         resized = pb.scale_simple(self.preview_size, self.preview_size,
                                   GdkPixbuf.InterpType.BILINEAR)
         self.set_from_pixbuf(resized)
+        self.queue_resize()
+
+    def _finished(self, istream, result):
+        self._pixbuf = GdkPixbuf.Pixbuf.new_from_stream_finish(result)
+        self.resize()
         istream.close()
         self.emit('info-known', self.size, self._pixbuf.props)
         self.queue_draw()
 
 
-class CoverArtWindow(qltk.Window):
+class CoverArtWindow(qltk.Dialog):
+    SIZES = {
+        300: _("Small"),
+        500: _("Classic"),
+        600: _("Large"),
+        720: _("HD"),
+        1080: _("Full HD"),
+        1600: _("WQXGA"),
+        2160: _("4K UHD")
+    }
+    DEFAULT_SIZE = list(SIZES.keys())[0]
 
     def __init__(self, songs):
-        super().__init__(title="Cover Art Download")
-        self.set_default_size(1000, 800)
+        super().__init__(title="Cover Art Download", use_header_bar=True)
+        self.set_default_size(1200, 640)
         self.flow_box = box = Gtk.FlowBox()
-        box.set_row_spacing(12)
         self.model = model = Gio.ListStore()
 
         def update(img, size, props, item, frame):
             text = ("{} - {}x{}, <b>{}</b>".format(escape(item.source),
                     props.width, props.height, format_size(size)))
             frame.get_label_widget().set_markup(text)
+            frame.get_child().set_reveal_child(True)
 
         def create_widget(item, data=None):
-            img = ResizeWebImage(item.url)
+            img = ResizeWebImage(item.url, preview_size=self.DEFAULT_SIZE)
             text = (_("(Loading %(source)s (%(dimensions)s)…")
-                    % (item.source, item.dimensions))
+                    % {'source': item.source, 'dimensions': item.dimensions})
             frame = Gtk.Frame.new(text)
+            img.set_padding(12, 12)
+            frame.set_shadow_type(Gtk.ShadowType.NONE)
+            frame.set_border_width(12)
             img.connect('info-known', update, item, frame)
-            frame.add(img)
-            frame.set_label_align(0.5, 0.5)
+            reveal = Gtk.Revealer()
+            reveal.set_reveal_child(False)
+            reveal.props.transition_duration = 800
+            reveal.props.transition_type = Gtk.RevealerTransitionType.CROSSFADE
+            reveal.add(img)
+            frame.add(reveal)
+
+            frame.set_label_align(0.5, 1.0)
             return frame
 
         box.bind_model(model, create_widget, None)
         box.set_valign(Gtk.Align.START)
         box.set_max_children_per_line(4)
-        paned = ConfigRVPaned("plugins", "cover_art_pane_pos", 0.5)
+        paned = ConfigRVPaned("plugins", "cover_art_pane_pos", 0.25)
         paned.ensure_wide_handle()
-        self.add(paned)
         sw = Gtk.ScrolledWindow()
         sw.add(self.flow_box)
-        options_box = Gtk.VBox()
-        frame = Gtk.Frame(label=_("Options"))
-        options_box.pack_start(frame, True, False, 6)
-        self.button = Button(_("_Save"), Icons.DOCUMENT_SAVE_AS)
-        self.button.set_sensitive(False)
-        self.button.connect('clicked', self.__save)
-        options_box.pack_start(self.button, True, False, 6)
 
-        paned.pack1(sw, True, False)
-        paned.pack2(options_box, True, False)
+        paned.pack1(sw, True, True)
+        paned.pack2(self.create_options(), True, False)
+        self.vbox.pack_start(paned, True, True, 0)
 
         manager = app.cover_manager
-
-        def covers_found(manager, provider, results):
-            if not results:
-                print_d("No results from %s" % provider)
-                return
-            for result in results:
-                self.model.append(result)
-            self.show_all()
-
-        def finished(manager, songs):
-            print_d("Finished all searches")
-            if not self.model.get_n_items():
-                print_w("Nothing found from any sources")
-
-                self.button.set_sensitive(False)
-            else:
-                self.button.set_sensitive(True)
-
+        connect_destroy(manager, 'covers-found', self._covers_found)
+        connect_destroy(manager, 'searches-complete', self._finished)
         cancellable = self.__cancellable = Gio.Cancellable()
-        connect_destroy(manager, 'covers-found', covers_found)
-        connect_destroy(manager, 'searches-complete', finished)
+        self.show_all()
 
+        # Do the search
         manager.search_cover(cancellable, songs)
+
+    def _covers_found(self, manager, provider, results):
+        if not results:
+            print_d("No results from %s" % provider)
+            return
+        for result in results:
+            self.model.append(result)
+        self.show_all()
+
+    def _finished(self, manager, songs):
+        print_d("Finished all searches")
+        if not self.model.get_n_items():
+            print_w("Nothing found from any sources")
+            self.button.set_sensitive(False)
+        else:
+            self.button.set_sensitive(True)
+
+    def create_options(self):
+        frame = Gtk.Frame(label=_("Options"))
+        hbox = Gtk.HBox()
+        sizes = self.SIZES.keys()
+        slider = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL,
+                                          min(sizes), max(sizes), 20)
+        for size, name in self.SIZES.items():
+            slider.add_mark(size, Gtk.PositionType.BOTTOM, name)
+        slider.set_show_fill_level(False)
+        slider.set_value(self.DEFAULT_SIZE)
+
+        def format_size(slider, value):
+            return _("%(size)d ✕ %(size)d px") % {'size': value}
+
+        def slider_changed(slider):
+            new_size = slider.get_value()
+            try:
+                for child in self.flow_box.get_children():
+                    img = child.get_child().get_child().get_child()
+                    img.resize(new_size)
+            except AttributeError as e:
+                print_w("Couldn't set picture size(s) (%s)" % e)
+
+        slider.connect('format-value', format_size)
+        slider.connect('value-changed', slider_changed)
+        label = Gtk.Label(_("Preview size"))
+        label.set_mnemonic_widget(slider)
+        hbox.pack_start(label, False, False, 6)
+        hbox.pack_start(slider, True, True, 6)
+        frame.add(hbox)
+
+        self.button = self.add_icon_button(_("_Save"), Icons.DOCUMENT_SAVE,
+                                           Gtk.ResponseType.APPLY)
+        self.button.set_sensitive(False)
+        self.button.connect('clicked', self.__save)
+        return frame
 
     def __save(self, button):
         children = self.flow_box.get_selected_children()
@@ -151,4 +210,6 @@ class DownloadCoverArt(SongsMenuPlugin):
     plugin_handles = any_song(is_a_file)
 
     def plugin_album(self, songs):
-        return CoverArtWindow(songs).show()
+        dialog = CoverArtWindow(songs)
+        dialog.run()
+        dialog.destroy()
