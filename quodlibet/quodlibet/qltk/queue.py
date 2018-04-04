quodlibet/quodlibet

commit 336eb350acbed697b9900181d0e09b05319d4074

Queue stop once empty: do check at end of song instead

quodlibet/quodlibet/qltk/queue.py
@@ -130,7 +130,6 @@ class QueueExpander(Gtk.Expander):
 
         clear_item = MenuItem(_("_Clear Queue"), Icons.EDIT_CLEAR)
         menu.append(clear_item)
-        clear_item.connect("activate", self.__clear_queue)
 
         button = SmallMenuButton(
             SymbolicIconImage(Icons.EMBLEM_SYSTEM, Gtk.IconSize.MENU),
@@ -184,7 +183,7 @@ class QueueExpander(Gtk.Expander):
             state_icon, False)
 
         connect_destroy(
-            player, 'song-started', self.__update_queue_stop, self.queue.model)
+            player, 'song-ended', self.__update_queue_stop, self.queue.model)
 
         # to make the children clickable if mapped
         # ....no idea why, but works
@@ -223,14 +222,6 @@ class QueueExpander(Gtk.Expander):
         else:
             state_icon.stop()
 
-    def __clear_queue(self, activator):
-        self.model.clear()
-        stop_queue = config.getboolean("memory",
-                                       "queue_stop_once_empty",
-                                       False)
-        if stop_queue:
-            app.player_options.stop_after = True
-
     def __motion(self, wid, context, x, y, time):
         Gdk.drag_status(context, Gdk.DragAction.COPY, time)
         return True
@@ -260,12 +251,11 @@ class QueueExpander(Gtk.Expander):
         self.queue.model.order = (OrderShuffle() if is_shuffled
                                   else OrderInOrder())
 
-    def __update_queue_stop(self, player, song, model):
+    def __update_queue_stop(self, player, song, stopped, model):
         enabled = config.getboolean("memory", "queue_stop_once_empty", False)
         songs_left = len(model.get())
-        if enabled and songs_left == 1:
-            # Enable stop_after if this is the last song
-            app.player_options.stop_after = True
+        if enabled and songs_left == 0 and not stopped:
+            app.player.stop()
 
     def __expand(self, widget, prop, menu_button):
         expanded = self.get_expanded()

commit 6f16d501bcf6ae2d377598679def739524196090

queue: make clearing work again
It was broken in commit 336eb350acbed697b9900181d0e09b05319d4074

Fixes #2816

quodlibet/quodlibet/qltk/queue.py
@@ -130,6 +130,7 @@ class QueueExpander(Gtk.Expander):
 
         clear_item = MenuItem(_("_Clear Queue"), Icons.EDIT_CLEAR)
         menu.append(clear_item)
+        clear_item.connect("activate", self.__clear_queue)
 
         button = SmallMenuButton(
             SymbolicIconImage(Icons.EMBLEM_SYSTEM, Gtk.IconSize.MENU),
@@ -226,6 +227,9 @@ class QueueExpander(Gtk.Expander):
         else:
             state_icon.stop()
 
+    def __clear_queue(self, activator):
+        self.model.clear()
+
     def __motion(self, wid, context, x, y, time):
         Gdk.drag_status(context, Gdk.DragAction.COPY, time)
         return True
@@ -266,7 +270,7 @@ class QueueExpander(Gtk.Expander):
         songs_left = len(model.get())
         if songs_left == 0:
             self._last_queue_song = None
-        elif songs_left == 1:
+        else:
             self._last_queue_song = song
 
     def __expand(self, widget, prop, menu_button):