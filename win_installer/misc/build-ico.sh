328ac2ae8ef00eca3237a8f93a96d65738942996
Christoph Reiter <reiter.christoph@gmail.com>
2018-05-12 19:03:15 +0200

    wininst: use the prescaled images as ico source, rescaling makes them blurry

--- win_installer/misc/build-ico.sh
+++ win_installer/misc/build-ico.sh
@@ -3,9 +3,5 @@
 set -e
 
 for name in quodlibet exfalso; do
-    path="../../quodlibet/quodlibet/images/hicolor/256x256/apps/${name}.png"
-    convert -background transparent \
-        "${path}" \
-        -define icon:auto-resize=16,32,48,256 \
-        "${name}.ico"
+    convert ../../quodlibet/quodlibet/images/hicolor/*/apps/${name}.png "${name}.ico"
 done
