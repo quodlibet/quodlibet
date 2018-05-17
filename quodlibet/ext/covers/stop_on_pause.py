ce157485302b2836fcdbd6e57ee7badb610aad5b
David Morris <othalan@othalan.net>
2018-05-17 12:22:53 +0700

    Another style fix to pass tests

--- quodlibet/ext/covers/stop_on_pause.py
+++ quodlibet/ext/covers/stop_on_pause.py
@@ -22,11 +22,11 @@ class StopOnPause(EventPlugin, PluginConfigMixin):
     PLUGIN_NAME = _("Stop on Pause")
     PLUGIN_ICON = Icons.MEDIA_PLAYBACK_PAUSE
     PLUGIN_DESC = _("This plugin changes Pause to Stop/Play/Seek."
-                    "\n\n"
+                    "\n"
                     "This behavior is useful if Quod Libet fails to switch "
                     "audio devices when a new audio device is added to the "
                     "system (Bluetooth speaker, USB DAC, etc)."
-                    "\n\n"
+                    "\n"
                     "If the option for 'Only Seekable Sources' "
                     "is not checked, this plugin will ensure the position in "
                     "the current source is maintained.  Otherwise, playback "
