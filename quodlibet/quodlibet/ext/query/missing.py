7223c915d61c1daaad070afdcdb2d04552392ea2
Peter Strulo <peter.strulo@gmail.com>
2018-08-14 18:50:05 +0100

    Strip body of whitespace.

--- quodlibet/quodlibet/ext/query/missing.py
+++ quodlibet/quodlibet/ext/query/missing.py
@@ -18,7 +18,7 @@ class MissingQuery(QueryPlugin, PluginConfigMixin):
     key = 'missing'
 
     def search(self, data, body):
-        val = data.get(body, None)
+        val = data.get(body.strip(), None)
         return (val is None
                 or (self.config_get_bool("include_empty", True)
                     and val == ""))
