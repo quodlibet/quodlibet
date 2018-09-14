6bd7c98ea34eacaddf16b5e1e653facede8be5f3
Christoph Reiter <reiter.christoph@gmail.com>
2018-09-14 09:31:54 +0200

    osx bootstrap: update git urls for gnome

--- osx_bundle/bootstrap.sh
+++ osx_bundle/bootstrap.sh
@@ -14,9 +14,9 @@ rm -f "$HOME/.jhbuildrc-custom"
 JHBUILD_REVISION="fe1552ad15999f023b01bc009dabb1b1956cd9ac"
 
 mkdir -p "$HOME"
-git clone git://git.gnome.org/jhbuild "$QL_OSXBUNDLE_JHBUILD_DEST"
+git clone https://gitlab.gnome.org/GNOME/jhbuild.git "$QL_OSXBUNDLE_JHBUILD_DEST"
 (cd "$QL_OSXBUNDLE_JHBUILD_DEST" && git checkout "$JHBUILD_REVISION" && ./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
 cp misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
 cp misc/quodlibet-jhbuildrc-custom "$HOME/.jhbuildrc-custom"
-git clone git://git.gnome.org/gtk-mac-bundler "$QL_OSXBUNDLE_BUNDLER_DEST"
+git clone https://gitlab.gnome.org/GNOME/gtk-mac-bundler.git "$QL_OSXBUNDLE_BUNDLER_DEST"
 (cd "$QL_OSXBUNDLE_BUNDLER_DEST" && make install)
