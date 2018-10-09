03c3a0e741c79ed422b1dfb28b43e06c5bedd8d2
Fredrik Strupe <fredrik@strupe.net>
2018-10-09 22:07:24 +0200

    formats/id3: Allow USLT tag with non-empty lang or desc field
    There shouldn't be any reason to ignore these.
    
    The test might be a bit funky now, but should still make sense.

--- quodlibet/tests/test_formats__id3.py
+++ quodlibet/tests/test_formats__id3.py
@@ -177,17 +177,19 @@ class TID3FileMixin(object):
                    text=u'lyrics with non-empty lang'))
         f.save()
 
-        f = self.KIND(self.filename)
-        self.failUnlessEqual(f['lyrics'], u'lyrics')
-        f['lyrics'] = u'modified lyrics'
-        f.write()
-
         f = mutagen.File(self.filename)
-        self.failUnlessEqual(f.tags[u'USLT::\x00\x00\x00'], u'modified lyrics')
+        self.failUnlessEqual(f.tags[u'USLT::\x00\x00\x00'], u'lyrics')
         self.failUnlessEqual(f.tags[u'USLT:desc:\x00\x00\x00'],
                              u'lyrics with non-empty desc')
         self.failUnlessEqual(f.tags[u'USLT::xyz'],
                              u'lyrics with non-empty lang')
+
+        f = self.KIND(self.filename)
+        self.failUnlessEqual(f['lyrics'], u'lyrics\n'
+                                          u'lyrics with non-empty lang\n'
+                                          u'lyrics with non-empty desc')
+        f['lyrics'] = u'modified lyrics'
+        f.write()
 
         f = self.KIND(self.filename)
         self.failUnlessEqual(f['lyrics'], u'modified lyrics')
@@ -197,10 +199,6 @@ class TID3FileMixin(object):
         f = mutagen.File(self.filename)
         self.failIf('USLT' in f.tags,
                     'There should be no USLT tag when lyrics were deleted')
-        self.failUnlessEqual(f.tags[u'USLT:desc:\x00\x00\x00'],
-                             u'lyrics with non-empty desc')
-        self.failUnlessEqual(f.tags[u'USLT::xyz'],
-                             u'lyrics with non-empty lang')
 
         f = self.KIND(self.filename)
         self.failIf('lyrics' in f,
