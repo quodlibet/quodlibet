d530c55309e11f22f8768e36c79d35c25506bf40
Christoph Reiter <reiter.christoph@gmail.com>
2018-10-21 12:59:21 +0200

    macos: moduleset updates
    doesn't include gtk/pango or anything needing meson yet

--- osx_bundle/build.sh
+++ osx_bundle/build.sh
@@ -4,5 +4,11 @@ set -e
 
 source env.sh
 
+# install ninja
+BIN=$(jhbuild run bash -c 'echo $PREFIX/bin')
+curl -L -o ninja.zip "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip"
+unzip -o ninja.zip -d "$BIN"
+rm ninja.zip
+
 jhbuild build meta-bootstrap
 jhbuild build quodlibet
