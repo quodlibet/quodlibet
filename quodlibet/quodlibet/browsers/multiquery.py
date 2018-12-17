e629612d78a34428d1fa33438dcc0d9c0b48bb06
Peter Strulo <peter.strulo@gmail.com>
2018-12-17 20:00:38 +0000

    Added removing queries

--- quodlibet/quodlibet/browsers/multiquery.py
+++ quodlibet/quodlibet/browsers/multiquery.py
@@ -5,11 +5,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
+
 import operator
 from itertools import count
 from functools import reduce
 
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk
 
 from quodlibet import _
 from quodlibet.browsers.search import SearchBar
@@ -18,7 +19,9 @@ from quodlibet.query import Query
 
 
 class MultiQueryBrowser(SearchBar):
-    """A browser in which multiple queries are parsed and used to filter results"""
+    """A browser in which multiple queries are parsed
+    and used to filter results
+    """
 
     name = _("MultiQuery")
     accelerated_name = _("_MultiQuery")
@@ -36,12 +39,13 @@ class MultiQueryBrowser(SearchBar):
         self.pack_start(align, False, True, 0)
 
     def add_list_query(self, _):
-        q = ListQuery(self._get_text())
+        q = ListQuery(self._get_text(), self.activate)
         q.show()
         self._list_box.add(q)
         self._set_text("")
 
     def _get_songs(self):
+        # Gtk.ListBox doesn't seem to have a get_rows method?
         queries = []
         for i in count():
             lq = self._list_box.get_row_at_index(i)
@@ -58,12 +62,25 @@ class MultiQueryBrowser(SearchBar):
 class ListQuery(Gtk.ListBoxRow):
     """A ListBoxRow representing a query"""
 
-    def __init__(self, string):
+    def __init__(self, string, changed_callback):
         super().__init__(activatable=False, selectable=False)
-        lbl = Gtk.Label(string)
-        lbl.show()
-        self.add(lbl)
+
+        self.changed_callback = changed_callback
         self.query = Query(string)
+
+        hbox = Gtk.HBox()
+        hbox.pack_start(Gtk.Label(string, halign=Gtk.Align.START, margin=3),
+                        True, True, 0)
+        btn = Gtk.Button.new_from_icon_name("window-close",
+                                            Gtk.IconSize.BUTTON)
+        btn.connect('clicked', self.remove)
+        hbox.pack_start(btn, False, True, 0)
+        self.add(hbox)
+        self.show_all()
+
+    def remove(self, _):
+        self.destroy()
+        self.changed_callback()
 
 
 browsers = [MultiQueryBrowser]
