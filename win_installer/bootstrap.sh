76abfc55c6eb7e4500f7974b54c2fc8a89cf1a72
Christoph Reiter <reiter.christoph@gmail.com>
2019-02-05 19:17:22 +0100

    wininst: make bootstrap work with x86_64

--- win_installer/bootstrap.sh
+++ win_installer/bootstrap.sh
@@ -9,31 +9,42 @@
 set -e
 
 function main {
+
+    if [[ "$MSYSTEM" == "MINGW32" ]]; then
+        local MSYS2_ARCH="i686"
+    else
+        local MSYS2_ARCH="x86_64"
+    fi
+
     pacman --noconfirm -Suy
 
     pacman --noconfirm -S --needed \
         git \
-        mingw-w64-i686-gettext \
-        mingw-w64-i686-gdk-pixbuf2 \
-        mingw-w64-i686-librsvg \
-        mingw-w64-i686-gtk3 \
-        mingw-w64-i686-libsoup mingw-w64-i686-gstreamer \
-        mingw-w64-i686-gst-plugins-base \
-        mingw-w64-i686-gst-plugins-good mingw-w64-i686-libsrtp \
-        mingw-w64-i686-gst-plugins-bad mingw-w64-i686-gst-libav \
-        mingw-w64-i686-gst-plugins-ugly \
-        base-devel mingw-w64-i686-toolchain
+        base-devel \
+        mingw-w64-$MSYS2_ARCH-gettext \
+        mingw-w64-$MSYS2_ARCH-gdk-pixbuf2 \
+        mingw-w64-$MSYS2_ARCH-librsvg \
+        mingw-w64-$MSYS2_ARCH-gtk3 \
+        mingw-w64-$MSYS2_ARCH-libsoup \
+        mingw-w64-$MSYS2_ARCH-gstreamer \
+        mingw-w64-$MSYS2_ARCH-gst-plugins-base \
+        mingw-w64-$MSYS2_ARCH-gst-plugins-good \
+        mingw-w64-$MSYS2_ARCH-libsrtp \
+        mingw-w64-$MSYS2_ARCH-gst-plugins-bad \
+        mingw-w64-$MSYS2_ARCH-gst-libav \
+        mingw-w64-$MSYS2_ARCH-gst-plugins-ugly \
+        mingw-w64-$MSYS2_ARCH-toolchain
 
     pacman --noconfirm -S --needed \
-        mingw-w64-i686-python3 \
-        mingw-w64-i686-python3-gobject \
-        mingw-w64-i686-python3-cairo \
-        mingw-w64-i686-python3-pip \
-        mingw-w64-i686-python3-pytest \
-        mingw-w64-i686-python3-certifi \
-        mingw-w64-i686-python3-coverage \
-        mingw-w64-i686-python3-pycodestyle \
-        mingw-w64-i686-python3-pyflakes
+        mingw-w64-$MSYS2_ARCH-python3 \
+        mingw-w64-$MSYS2_ARCH-python3-gobject \
+        mingw-w64-$MSYS2_ARCH-python3-cairo \
+        mingw-w64-$MSYS2_ARCH-python3-pip \
+        mingw-w64-$MSYS2_ARCH-python3-pytest \
+        mingw-w64-$MSYS2_ARCH-python3-certifi \
+        mingw-w64-$MSYS2_ARCH-python3-coverage \
+        mingw-w64-$MSYS2_ARCH-python3-pycodestyle \
+        mingw-w64-$MSYS2_ARCH-python3-pyflakes
 
     pip3 install --user -U feedparser musicbrainzngs mutagen
 }
