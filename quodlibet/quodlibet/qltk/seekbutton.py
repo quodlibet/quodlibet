7db33fecb7f937434b10ac8dbfcea9a339edfb99
Tomasz Kot <tomasz.kot13@gmail.com>
2019-04-13 22:08:59 +0200

    Fix waveform scroll  #3149 (#3161)
    * Fix waveform scroll  #3149
    
    * Revert "Fix waveform scroll  #3149"
    
    This reverts commit 6ae029e8d6ab9e15fee9f452f48bfff8a24ca0e9.
    
    * Fix waveform scroll #3149. Change display time scroll behaviour.

--- quodlibet/quodlibet/qltk/seekbutton.py
+++ quodlibet/quodlibet/qltk/seekbutton.py
@@ -185,9 +185,9 @@ class HSlider(Gtk.Button):
         v = hscale.get_value()
         direction = event.direction
         if direction in [Gdk.ScrollDirection.DOWN, Gdk.ScrollDirection.RIGHT]:
-            v += adj.props.step_increment
-        elif direction in [Gdk.ScrollDirection.UP, Gdk.ScrollDirection.LEFT]:
             v -= adj.props.step_increment
+        elif direction in [Gdk.ScrollDirection.UP, Gdk.ScrollDirection.LEFT]:
+            v += adj.props.step_increment
         else:
             # newer Gdk.ScrollDirection.SMOOTH
             return
