eb0bc6ee1a89ba0d399633ce5a4566f4f59fdde3
Nick B <nick@declension.net>
2020-04-17 09:10:17 +0100

    Paned browser: format time tags (#3346)
     * Extract date logic from `DateColumn`
     * Tidy / add typing to existing code, a bit.
     * If pane config looks datelike, use a date formatter
    
    This closes #3346

---
+++ quodlibet/util/string/date.py
@@ -0,0 +1,34 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+from datetime import datetime
+from time import localtime, strftime
+from typing import Text, Optional
+
+
+def format_date(stamp: float, format_setting: Optional[Text] = None) -> Text:
+    """Formats a date either with the default format,
+     or the passed strftime-compatible format string"""
+    try:
+        date = datetime.fromtimestamp(stamp).date()
+    except (OverflowError, ValueError, OSError):
+        text = u""
+    else:
+        if format_setting:
+            format_ = format_setting
+        # use default behaviour-format
+        else:
+            today = datetime.now().date()
+            days = (today - date).days
+            if days == 0:
+                format_ = "%X"
+            elif days < 7:
+                format_ = "%A"
+            else:
+                format_ = "%x"
+
+        stamp = localtime(stamp)
+        text = strftime(format_, stamp)
+    return text
