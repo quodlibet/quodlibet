fa49dbbdbec9183b12737f2548aa35111cb685cf
Christoph Reiter <reiter.christoph@gmail.com>
2020-05-10 09:32:10 +0200

    CI: try to work around missing msys2 deps

--- .azure/msys2-install.sh
+++ .azure/msys2-install.sh
@@ -7,6 +7,10 @@ export MSYS2_FC_CACHE_SKIP=1
 pacman --noconfirm -Suy
 
 pacman --noconfirm -S --needed \
+    mingw-w64-$MSYS2_ARCH-libxml2 \
+    mingw-w64-$MSYS2_ARCH-brotli
+
+pacman --noconfirm -S --needed \
     git \
     mingw-w64-$MSYS2_ARCH-gettext \
     mingw-w64-$MSYS2_ARCH-gdk-pixbuf2 \
