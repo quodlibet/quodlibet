c5633bbc1f7e81c98961517d76bb2d4f1060b05e
Ironbalden <ironbalden@gmail.com>
2020-06-07 19:07:49 +0200

    add check to Missing.py, fix #3336 (#3382)

--- quodlibet/ext/query/missing.py
+++ quodlibet/ext/query/missing.py
@@ -17,7 +17,7 @@ class MissingQuery(QueryPlugin, PluginConfigMixin):
     key = 'missing'
 
     def search(self, data, body):
-        val = data.get(body.strip(), None)
+        val = data.get(body.strip() if body else None, None)
         return (val is None
                 or (self.config_get_bool("include_empty", True)
                     and val == ""))
