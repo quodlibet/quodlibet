85c739b1dd7cfdcc240e38c7287e30355ac27438
Nick B <nick@declension.net>
2020-09-30 08:25:30 +0100

    More test parameterisation:
     * Use Pytest-style fixtures and raw tests class
     * Parameterise plugins prefs test too, to allow multiple failures / visibility in test
     * Check we have _some_ plugins tested still
     * Mypy test failure improvements (newline was obscuring test problem, use asserts instead, assert in right directory for MyPy)
     * Use pathlib for new things
     * Parameterise by directory, thus fixing last bits #3432
    
     This closes #3432

--- tests/quality/test_mypy.py
+++ tests/quality/test_mypy.py
@@ -1,24 +1,26 @@
 # Copyright 2018 Christoph Reiter
+#           2020 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-import os
+from pathlib import Path
 
 import pytest
+
 api = pytest.importorskip("mypy.api")
 
 import quodlibet
 from quodlibet.util import get_module_dir
-from tests import TestCase
 
 
 @pytest.mark.quality
-class TMypy(TestCase):
+class TestMypy:
 
-    def test_all(self):
-        root = os.path.dirname(get_module_dir(quodlibet))
-        out, err, status = api.run([root])
+    def test_project(self):
+        root = Path(get_module_dir(quodlibet))
+        assert Path.cwd() == root.parent, "MyPy must be run from project root"
+        out, err, status = api.run([str(root)])
         assert status == 0, "Failed mypy checks: \n" + "\n".join([out, err])
