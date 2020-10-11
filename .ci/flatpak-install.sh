af2bed71ce76ee190e4b7332b9309518577d34b3
Nick B <nick@declension.net>
2020-10-11 16:01:04 +0100

    CI: install python3-pip explicitly for Flatpak
    Fixes #3469

--- .ci/flatpak-install.sh
+++ .ci/flatpak-install.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo add-apt-repository --yes ppa:alexlarsson/flatpak
 sudo apt-get update
-sudo apt-get install -y ca-certificates flatpak xvfb
+sudo apt-get install -y ca-certificates flatpak xvfb python3-pip
 
 flatpak remote-add --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --user gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
