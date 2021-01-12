22abc6b597a765cc55cb00ec7815d6df0fabea90
Nick B <nick@declension.net>
2021-01-12 22:26:52 +0000

    Playlists: Fix Mypy fsnative

--- quodlibet/library/playlist.py
+++ quodlibet/library/playlist.py
@@ -11,7 +11,7 @@ from quodlibet import print_d, print_w
 from quodlibet.formats import AudioFile
 from quodlibet.library.base import Library
 from quodlibet.util.collection import Playlist, XSPFBackedPlaylist, FileBackedPlaylist
-from senf import text2fsn, fsnative
+from senf import text2fsn, _fsnative
 
 _DEFAULT_PLAYLIST_DIR = text2fsn(os.path.join(quodlibet.get_user_dir(), "playlists"))
 """Directory for playlist files"""
@@ -25,7 +25,7 @@ class PlaylistLibrary(Library[str, Playlist]):
     the values are Playlist objects.
     """
 
-    def __init__(self, library: Library, pl_dir: fsnative = _DEFAULT_PLAYLIST_DIR):
+    def __init__(self, library: Library, pl_dir: _fsnative = _DEFAULT_PLAYLIST_DIR):
         self.librarian = None
         super().__init__(f"{type(self).__name__} for {library._name}")
         print_d(f"Initializing Playlist Library {self} to watch {library._name!r}")
