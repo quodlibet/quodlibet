1ee855a487824a1923b1a72905e430c488da3218
Nick B <nick@declension.net>
2021-03-12 18:03:23 +0000

    Fix import

--- tests/test_util_copool.py
+++ tests/test_util_copool.py
@@ -2,7 +2,6 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-from time import sleep
 
 import pytest
 
