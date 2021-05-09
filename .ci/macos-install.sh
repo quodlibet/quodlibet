e5ed077b0131d06ca876eea927a90a9a80c61b25
Nick Boultbee <declension@users.noreply.github.com>
2021-05-09 10:59:11 +0100

    Support flaky tests (#3695)
    * Support flaky tests
    
    * Add `flaky` as test dep
    * Mark copool tests as flaky
    * Update lots of CI things
    
    * Add flaky to Flatpak testing too

--- .ci/macos-install.sh
+++ .ci/macos-install.sh
@@ -7,4 +7,4 @@ hdiutil attach -noverify -readonly -mountpoint _mount ql.dmg
 cp -R _mount/QuodLibet.app "$TMPDIR/_app"
 hdiutil detach -force _mount
 RUN="$TMPDIR/_app/Contents/MacOS/run"
-$RUN -m pip install flake8 pytest pytest-faulthandler "coverage[toml]"
+$RUN -m pip install flake8 pytest pytest-faulthandler flaky "coverage[toml]"
