2c8d054cf72487ff7e41483a25da64dff89ceed6
Nick B <nick@declension.net>
2021-06-05 10:37:09 +0100

    Update header too

--- quodlibet/browsers/soundcloud/api.py
+++ quodlibet/browsers/soundcloud/api.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Nick Boultbee
+# Copyright 2016-21 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
