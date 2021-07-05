3285eaf3c9de839e365e035ab31071f3fd977787
Nick B <nick@declension.net>
2021-07-05 15:31:53 +0100

    CI: Fedora: Normalise Docker usage
     * Add steps in Github like other jobs

--- .github/workflows/fedora33/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Fix for hanging flake8 test
-# https://github.com/quodlibet/quodlibet/issues/3539#issuecomment-767389459
-poetry config virtualenvs.in-project true
-
-poetry install -E plugins
-PYTEST_ADDOPTS=-rxXs poetry run python3 setup.py test
