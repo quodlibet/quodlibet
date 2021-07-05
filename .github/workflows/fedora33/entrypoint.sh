7664dd5632c15fcac639430e3f174fd02525979a
Nick B <nick@declension.net>
2021-07-05 17:47:59 +0100

    CI: Fedora: even more revert

--- .github/workflows/fedora33/entrypoint.sh
+++ .github/workflows/fedora33/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+# Fix for hanging flake8 test
+# https://github.com/quodlibet/quodlibet/issues/3539#issuecomment-767389459
+poetry config virtualenvs.in-project true
+
 poetry install -E plugins
 export PYTEST_ADDOPTS=-rxXs
 sudo -u user poetry run setup.py test
