c4b505a3c656d12526b4d02293b1920eb32b69a1
Nick B <nick@declension.net>
2021-07-11 16:54:55 +0100

    CI: Fedora: simplify entrypoint more

--- .github/workflows/fedora33/entrypoint.sh
+++ .github/workflows/fedora33/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 # Leave source locally, otherwise some tests go mad (via get_module_dir)...
 sudo -u user poetry install --no-root -E plugins
 
-sudo -u user PYTEST_ADDOPTS='-rxXs -m "not quality"' poetry run setup.py test
+sudo -u user PYTEST_ADDOPTS='-rxXs -m "not quality"' poetry run python3 setup.py test
