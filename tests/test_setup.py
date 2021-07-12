quodlibet/quodlibet

commit 3ea15c04daa1fa28e8a3d59ecbaf0c9b8e2f26a3

Refactor gettext code (#3737)
* Refactor gettext code

 * Use `pathlib` paths everywhere. It's neater now and we get better checking
 * Add lots of typing, mypy does some work for us now
 * Use f-strings for great win
 * Fix some inappropriate test naming...
 * Improve some old / incorrect docstrings
 * Add some tests around `setup.py` PO commands (this broke them initially, but no tests broke)

* Shorter tracebacks in testing

* Revert over-zelous renaming

* gettext: A few more Path conversion

* gettext: Only check quodlibet/ dir (#3736)

For some reason other `.venv` is included (in Fedora...)

* setup_test: only test po commands with gettext

* gettext: dump CLI command on failure

* CI: Fedora: normalise Docker usage slightly

 * Enforce user at Docker level not script
 * Exit on failure
 * Use same pytest options as other jobs

* CI: Fedora: try root user

* CI: Fedora: Normalise Docker usage

 * Add steps in Github like other jobs

* CI: Fedora: Fix Github action YML

* Revert "CI: Fedora: Fix Github action YML"

This reverts commit 503d4050

* Revert "CI: Fedora: Normalise Docker usage"

This reverts commit 3285eaf3

* CI: debug permissions

* CI: Fedora: back to non-root

* CI: Fedora: fix

* CI: Fedora: fix

* CI: Fedora: more revert, sigh

* CI: Fedora: more revert, sigh

* CI: Fedora: even more revert

* CI: Fedora: revert with debug

* CI: Fedora: ignore quality tests

* CI: Fedora: ignore quality tests (fix)

* CI: Fedora: bit more debugging

* Gettext: test using source paths

* Fedora - don't install ql into venv
* Nicer debug on failure though

* CI: Fedora: more debugging

* Gettext: ignore non-file paths in processed POTFILES.in

* Gettext: Fix skipping of dotfiles

* Gettext: list all paths on failure

* Gettext: re-allow directories in potfiles

* Fix import

* Revert

* CI: Fedora: revert to using Poetry venv outside root

gdist/po.py
@@ -1,5 +1,6 @@
 # Copyright 2007 Joe Wreschnig
 #           2009-2010,2012-2016 Christoph Reiter
+#           2021 Nick Boultbee
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -28,9 +29,11 @@ gettext message catalogs.
 
 import os
 import shutil
+from pathlib import Path
 from tempfile import mkstemp
 from distutils.errors import DistutilsOptionError
 from distutils.dep_util import newer_group, newer
+from typing import Optional
 
 from .util import Command
 from . import gettextutil
@@ -48,12 +51,13 @@ class po_stats(Command):
         pass
 
     def run(self):
-        po_directory = self.distribution.po_directory
+        po_directory = Path(self.distribution.po_directory)
 
         with gettextutil.create_pot(po_directory) as pot_path:
             res = []
             for language in gettextutil.list_languages(po_directory):
                 fd, temp_path = mkstemp(".po")
+                temp_path = Path(temp_path)
                 try:
                     os.close(fd)
                     po_path = gettextutil.get_po_path(po_directory, language)
@@ -61,7 +65,7 @@ class po_stats(Command):
                     output = gettextutil.po_stats(temp_path)
                     res.append((language, output))
                 finally:
-                    os.remove(temp_path)
+                    temp_path.unlink()
 
         stats = []
         for po, r in res:
@@ -98,12 +102,12 @@ class update_po(Command):
         except gettextutil.GettextError as e:
             raise SystemExit(e)
 
-        po_directory = self.distribution.po_directory
+        po_directory = Path(self.distribution.po_directory)
 
         langs = gettextutil.list_languages(po_directory)
         if self.lang is not None:
             if self.lang not in langs:
-                raise SystemExit("Error: %r not found" % self.lang)
+                raise SystemExit(f"Error: {self.lang} not found")
             else:
                 langs = [self.lang]
 
@@ -133,11 +137,11 @@ class create_po(Command):
         except gettextutil.GettextError as e:
             raise SystemExit(e)
 
-        po_directory = self.distribution.po_directory
+        po_directory = Path(self.distribution.po_directory)
         po_path = gettextutil.get_po_path(po_directory, self.lang)
         with gettextutil.create_pot(po_directory) as pot_path:
             gettextutil.create_po(pot_path, po_path)
-            print("Created %r" % os.path.abspath(po_path))
+            print(f"Created {po_path.absolute()}")
 
 
 class create_pot(Command):
@@ -158,12 +162,12 @@ class create_pot(Command):
             raise SystemExit(e)
 
         po_package = self.distribution.po_package
-        po_directory = self.distribution.po_directory
+        po_directory = Path(self.distribution.po_directory)
         with gettextutil.create_pot(po_directory) as pot_path:
-            dest = os.path.join(po_directory, po_package + ".pot")
+            dest = po_directory / f"{po_package}.pot"
             shutil.copy(pot_path, dest)
             strip_pot_date(dest)
-            print("Created %r" % os.path.abspath(dest))
+            print(f"Created {dest.absolute()}")
 
 
 def strip_pot_date(path):
@@ -188,14 +192,14 @@ class build_po(Command):
 
     def initialize_options(self):
         self.build_base = None
-        self.po_build_dir = None
+        self.po_build_dir: Optional[Path] = None
 
     def finalize_options(self):
         self.set_undefined_options('build', ('build_base', 'build_base'))
-        self.po_build_dir = os.path.join(self.build_base, '_po_build')
+        self.po_build_dir = Path(self.build_base) / '_po_build'
 
     def run(self):
-        po_directory = self.distribution.po_directory
+        po_directory = Path(self.distribution.po_directory)
         langs = gettextutil.list_languages(po_directory)
 
         pot_deps = gettextutil.get_pot_dependencies(po_directory)
@@ -203,13 +207,13 @@ class build_po(Command):
         for language in langs:
             po_path = gettextutil.get_po_path(po_directory, language)
             out_path = gettextutil.get_po_path(self.po_build_dir, language)
-            if newer_group(pot_deps + [po_path], out_path):
+            if newer_group([str(p) for p in pot_deps + [po_path]], str(out_path)):
                 to_build.append((po_path, out_path))
 
         if not to_build:
             return
 
-        self.mkpath(self.po_build_dir)
+        self.mkpath(str(self.po_build_dir))
         with gettextutil.create_pot(po_directory) as pot_path:
             for po_path, out_path in to_build:
                 gettextutil.update_po(pot_path, po_path, out_path)
@@ -232,9 +236,9 @@ class build_mo(Command):
     ]
 
     def initialize_options(self):
-        self.build_base = None
+        self.build_base: Optional[Path] = None
         self.lang = None
-        self.po_build_dir = None
+        self.po_build_dir: Optional[Path] = None
 
     def finalize_options(self):
         self.set_undefined_options('build', ('build_base', 'build_base'))
@@ -253,15 +257,15 @@ class build_mo(Command):
             else:
                 langs = [self.lang]
 
-        basepath = os.path.join(self.build_base, 'share', 'locale')
+        basepath = self.build_base / "share" / "locale"
         for language in langs:
-            fullpath = os.path.join(basepath, language, "LC_MESSAGES")
-            destpath = os.path.join(fullpath, po_package + ".mo")
+            fullpath = basepath / language / "LC_MESSAGES"
+            destpath = fullpath / f"{po_package}.mo"
 
-            self.mkpath(fullpath)
+            self.mkpath(str(fullpath))
 
             po_path = gettextutil.get_po_path(self.po_build_dir, language)
-            if newer(po_path, destpath):
+            if newer(str(po_path), str(destpath)):
                 gettextutil.compile_po(po_path, destpath)
 
 

tests/test_setup.py
@@ -1,0 +1,77 @@
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import shutil
+from pathlib import Path
+from tempfile import mkdtemp
+
+import pytest
+from _pytest.fixtures import fixture
+
+import quodlibet
+from gdist import create_po, GDistribution, create_pot, update_po, po_stats, build_po
+from quodlibet.util import get_module_dir
+from tests.test_po import has_gettext_util
+
+SRC_FILE = Path(get_module_dir(quodlibet)).parent / "quodlibet.py"
+
+
+@fixture
+def dist(temp_po_dir) -> GDistribution:
+    dist = GDistribution()
+    dist.po_directory = str(temp_po_dir)
+    return dist
+
+
+@fixture
+def temp_po_dir() -> Path:
+    out_path = Path(mkdtemp())
+    po_path = out_path / "po"
+    po_path.mkdir()
+    with open(po_path / "POTFILES.in", "w") as f:
+        f.write(f"{SRC_FILE.name}\n")
+    shutil.copy(SRC_FILE, out_path / SRC_FILE.name)
+    return po_path
+
+
+@pytest.mark.skipif(not has_gettext_util(), reason="no gettext")
+class TestPoCommands:
+    def test_create_po_command(self, dist):
+        cmd = create_po(dist)
+        cmd.lang = "fr_FR"
+        cmd.run()
+
+    def test_create_pot_command(self, dist):
+        cmd = create_pot(dist)
+        cmd.run()
+
+    def test_update_po_command(self, dist, temp_po_dir):
+        (temp_po_dir / "en_GB.po").touch()
+        cmd = update_po(dist)
+        cmd.lang = "en_GB"
+        cmd.run()
+
+    def test_po_stats_command(self, dist, temp_po_dir):
+        cmd = po_stats(dist)
+        cmd.lang = "en_GB"
+        cmd.run()
+
+    def test_build_po(self, dist):
+        cmd = build_po(dist)
+        cmd.run()

.github/workflows/fedora33/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
-# Fix for hanging flake8 test
-# https://github.com/quodlibet/quodlibet/issues/3539#issuecomment-767389459
-poetry config virtualenvs.in-project true
+# Leave source locally, otherwise some tests go mad (via get_module_dir)...
+sudo -u user poetry install --no-root -E plugins
 
-poetry install -E plugins
-sudo -u user poetry run python3 setup.py test
+sudo -u user PYTEST_ADDOPTS='-rxXs -m "not quality"' poetry run python3 setup.py test

gdist/gettextutil.py
@@ -1,5 +1,5 @@
 # Copyright 2015-2017 Christoph Reiter
-#                2019 Nick Boultbee
+#             2019-21 Nick Boultbee
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -21,7 +21,6 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
-import glob
 import subprocess
 import contextlib
 import fnmatch
@@ -30,8 +29,12 @@ import shutil
 import functools
 import warnings
 
-# pattern -> (language, [keywords])
-XGETTEXT_CONFIG = {
+from pathlib import Path
+from typing import List, Optional, Tuple, Iterable, Dict
+
+QL_SRC_DIR = "quodlibet"
+
+XGETTEXT_CONFIG: Dict[str, Tuple[str, List[str]]] = {
     "*.py": ("Python", [
         "", "_", "N_", "C_:1c,2", "NC_:1c,2", "Q_", "pgettext:1c,2",
         "npgettext:1c,2,3", "numeric_phrase:1,2", "dgettext:2",
@@ -41,6 +44,7 @@ XGETTEXT_CONFIG = {
     "*.desktop": ("Desktop", [
         "", "Name", "GenericName", "Comment", "Keywords"]),
 }
+"""Dict of pattern -> (language, [keywords])"""
 
 
 class GettextError(Exception):
@@ -51,7 +55,7 @@ class GettextWarning(Warning):
     pass
 
 
-def _read_potfiles(src_root, potfiles):
+def _read_potfiles(src_root: Path, potfiles: Path) -> List[Path]:
     """Returns a list of paths for a POTFILES.in file"""
 
     paths = []
@@ -60,32 +64,33 @@ def _read_potfiles(src_root, potfiles):
             line = line.strip()
             if not line or line.startswith("#"):
                 continue
-            paths.append(os.path.normpath(os.path.join(src_root, line)))
+            paths.append((src_root / line).resolve())
     return paths
 
 
-def _write_potfiles(src_root, potfiles, paths):
+def _write_potfiles(src_root: Path, potfiles: Path, paths: Iterable[Path]):
     with open(potfiles, "w", encoding="utf-8") as h:
         for path in paths:
-            path = os.path.relpath(path, src_root)
-            h.write(path + "\n")
+            path = src_root / path
+            h.write(str(path) + "\n")
 
 
-def _src_root(po_dir):
-    return os.path.normpath(os.path.join(po_dir, ".."))
+def _src_root(po_dir: Path) -> Path:
+    assert isinstance(po_dir, Path)
+    return po_dir.parent.resolve()
 
 
-def get_pot_dependencies(po_dir):
+def get_pot_dependencies(po_dir: Path) -> List[Path]:
     """Returns a list of paths that are used as input for the .pot file"""
 
     src_root = _src_root(po_dir)
-    potfiles_path = os.path.join(po_dir, "POTFILES.in")
+    potfiles_path = po_dir / "POTFILES.in"
     return _read_potfiles(src_root, potfiles_path)
 
 
-def _get_pattern(path):
+def _get_pattern(path: Path) -> Optional[str]:
     for pattern in XGETTEXT_CONFIG:
-        match_part = os.path.basename(path)
+        match_part = path.name
         if match_part.endswith(".in"):
             match_part = match_part.rsplit(".", 1)[0]
         if fnmatch.fnmatch(match_part, pattern):
@@ -93,7 +98,11 @@ def _get_pattern(path):
     return None
 
 
-def _create_pot(potfiles_path, src_root):
+def _create_pot(potfiles_path: Path, src_root: Path) -> Path:
+    """Create a POT file for the specified POs and source code
+
+        :returns: the output path
+    """
     potfiles = _read_potfiles(src_root, potfiles_path)
 
     groups = {}
@@ -102,7 +111,7 @@ def _create_pot(potfiles_path, src_root):
         if pattern is not None:
             groups.setdefault(pattern, []).append(path)
         else:
-            raise ValueError("Unknown filetype: " + path)
+            raise ValueError(f"Unknown filetype: {path!s}")
 
     specs = []
     for pattern, paths in groups.items():
@@ -112,6 +121,7 @@ def _create_pot(potfiles_path, src_root):
     specs.sort(reverse=True)
 
     fd, out_path = tempfile.mkstemp(".pot")
+    out_path = Path(out_path)
     try:
         os.close(fd)
         for language, keywords, paths in specs:
@@ -126,14 +136,16 @@ def _create_pot(potfiles_path, src_root):
                     args.append("-k")
 
             fd, potfiles_in = tempfile.mkstemp()
+            potfiles_in = Path(potfiles_in)
             try:
                 os.close(fd)
                 _write_potfiles(src_root, potfiles_in, paths)
 
                 args = ["xgettext", "--from-code=utf-8", "--add-comments",
-                        "--files-from=" + potfiles_in,
-                        "--directory=" + src_root,
-                        "--output=" + out_path, "--force-po",
+                        "--files-from=" + str(potfiles_in),
+                        "--directory=" + str(src_root),
+                        "--output=" + str(out_path),
+                        "--force-po",
                         "--join-existing"] + args
 
                 p = subprocess.Popen(
@@ -142,25 +154,26 @@ def _create_pot(potfiles_path, src_root):
                     universal_newlines=True)
                 stdout, stderr = p.communicate()
                 if p.returncode != 0:
-                    raise GettextError(stderr
-                                       or ("Got error: %d" % p.returncode))
+                    path_strs = ", ".join(str(p) for p in paths)
+                    msg = f"Error running `{' '.join(args)}`:\nPaths: {path_strs}...\n"
+                    msg += stderr or ("Got error: %d" % p.returncode)
+                    raise GettextError(msg)
                 if stderr:
                     warnings.warn(stderr, GettextWarning)
             finally:
-                os.unlink(potfiles_in)
+                potfiles_in.unlink()
     except Exception:
-        os.unlink(out_path)
+        out_path.unlink()
         raise
 
     return out_path
 
 
 @contextlib.contextmanager
-def create_pot(po_dir):
+def create_pot(po_path: Path):
     """Temporarily creates a .pot file in a temp directory."""
-
-    src_root = _src_root(po_dir)
-    potfiles_path = os.path.join(po_dir, "POTFILES.in")
+    src_root = _src_root(po_path)
+    potfiles_path = po_path / "POTFILES.in"
     pot_path = _create_pot(potfiles_path, src_root)
     try:
         yield pot_path
@@ -168,39 +181,39 @@ def create_pot(po_dir):
         os.unlink(pot_path)
 
 
-def update_linguas(po_dir):
+def update_linguas(po_path: Path) -> None:
     """Create a LINGUAS file in po_dir"""
 
-    linguas = os.path.join(po_dir, "LINGUAS")
+    linguas = po_path / "LINGUAS"
     with open(linguas, "w", encoding="utf-8") as h:
-        for l in list_languages(po_dir):
+        for l in list_languages(po_path):
             h.write(l + "\n")
 
 
-def list_languages(po_dir):
+def list_languages(po_path: Path) -> List[str]:
     """Returns a list of available language codes"""
 
-    po_files = glob.glob(os.path.join(po_dir, "*.po"))
-    return sorted([os.path.basename(po[:-3]) for po in po_files])
+    po_files = po_path.glob("*.po")
+    return sorted([os.path.basename(str(po)[:-3]) for po in po_files])
 
 
-def compile_po(po_path, target_file):
+def compile_po(po_path: Path, target_file: Path):
     """Creates an .mo from a .po"""
 
     try:
         subprocess.check_output(
-            ["msgfmt", "-o", target_file, po_path],
+            ["msgfmt", "-o", str(target_file), str(po_path)],
             universal_newlines=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         raise GettextError(e.output)
 
 
-def po_stats(po_path):
+def po_stats(po_path: Path):
     """Returns a string containing translation statistics"""
 
     try:
         return subprocess.check_output(
-            ["msgfmt", "--statistics", po_path, "-o", os.devnull],
+            ["msgfmt", "--statistics", str(po_path), "-o", os.devnull],
             universal_newlines=True, stderr=subprocess.STDOUT).strip()
     except subprocess.CalledProcessError as e:
         raise GettextError(e.output)
@@ -226,18 +239,17 @@ def merge_file(po_dir, file_type, source_file, target_file):
         raise GettextError(e.output)
 
 
-def get_po_path(po_dir, lang_code):
+def get_po_path(po_path: Path, lang_code: str) -> Path:
     """The default path to the .po file for a given language code"""
 
-    return os.path.join(po_dir, lang_code + ".po")
+    return po_path / (lang_code + ".po")
 
 
-def update_po(pot_path, po_path, out_path=None):
+def update_po(pot_path: Path, po_path: Path, out_path: Optional[Path] = None) -> None:
     """Update .po at po_path based on .pot at po_path.
 
     If out_path is given will not touch po_path and write to out_path instead.
 
-    Returns the path written to.
     Raises GettextError on error.
     """
 
@@ -246,13 +258,13 @@ def update_po(pot_path, po_path, out_path=None):
 
     try:
         subprocess.check_output(
-            ["msgmerge", "-o", out_path, po_path, pot_path],
+            ["msgmerge", "-o", str(out_path), str(po_path), str(pot_path)],
             universal_newlines=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         raise GettextError(e.output)
 
 
-def check_po(po_path, ignore_header=False):
+def check_po(po_path: Path, ignore_header=False):
     """Makes sure the .po is well formed
 
     Raises GettextError if not
@@ -261,13 +273,13 @@ def check_po(po_path, ignore_header=False):
     check_arg = "--check" if not ignore_header else "--check-format"
     try:
         subprocess.check_output(
-            ["msgfmt", check_arg, "--check-domain", po_path, "-o", os.devnull],
+            ["msgfmt", check_arg, "--check-domain", str(po_path), "-o", os.devnull],
             stderr=subprocess.STDOUT, universal_newlines=True)
     except subprocess.CalledProcessError as e:
         raise GettextError(e.output)
 
 
-def check_pot(pot_path):
+def check_pot(pot_path: Path) -> None:
     """Makes sure that the .pot is well formed
 
     Raises GettextError if not
@@ -276,8 +288,9 @@ def check_pot(pot_path):
     # msgfmt doesn't like .pot files, but we can create a dummy .po
     # and test that instead.
     fd, po_path = tempfile.mkstemp(".po")
+    po_path = Path(po_path)
     os.close(fd)
-    os.unlink(po_path)
+    po_path.unlink()
     create_po(pot_path, po_path)
 
     try:
@@ -286,58 +299,59 @@ def check_pot(pot_path):
         os.remove(po_path)
 
 
-def create_po(pot_path, po_path):
+def create_po(pot_path: Path, po_path: Path) -> None:
     """Create a new <po_path> file based on <pot_path>
 
-    Returns the path to the new po file or raise GettextError
-    in case something went wrong or the file already exists.
+    :raises GettextError: in case something went wrong or the file already exists.
     """
 
-    if os.path.exists(po_path):
-        raise GettextError("%r already exists" % po_path)
+    if po_path.exists():
+        raise GettextError(f"{po_path!s} already exists")
 
-    if not os.path.exists(pot_path):
-        raise GettextError("%r missing" % pot_path)
+    if not pot_path.exists():
+        raise GettextError(f"{pot_path!s} missing")
 
     try:
         subprocess.check_output(
-            ["msginit", "--no-translator", "-i", pot_path, "-o", po_path],
+            ["msginit", "--no-translator", "-i", str(pot_path), "-o", str(po_path)],
             universal_newlines=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         raise GettextError(e.output)
 
-    if not os.path.exists(po_path):
-        raise GettextError(
-            "something went wrong; %r didn't get created" % po_path)
+    if not po_path.exists():
+        raise GettextError(f"something went wrong; {po_path!s} didn't get created")
 
     update_po(pot_path, po_path)
 
 
-def get_missing(po_dir):
-    """Returns a list of file information for translatable strings
-    found in files not listed in POTFILES.in and not skipped in
-    POTFILES.skip.
+def get_missing(po_dir: Path) -> Iterable[str]:
+    """Gets missing strings
+
+       :returns: a list of file information for translatable strings
+                 found in files not listed in POTFILES.in
+                 and not skipped in POTFILES.skip.
     """
 
     src_root = _src_root(po_dir)
-    potfiles_path = os.path.join(po_dir, "POTFILES.in")
-    skip_path = os.path.join(po_dir, "POTFILES.skip")
+    potfiles_path = po_dir / "POTFILES.in"
+    skip_path = po_dir / "POTFILES.skip"
 
     # Generate a set of paths of files which are not marked translatable
     # and not skipped
-    pot_files = {os.path.relpath(p, src_root)
+    pot_files = {p.relative_to(src_root)
                  for p in _read_potfiles(src_root, potfiles_path)}
-    skip_files = {os.path.relpath(p, src_root)
+    skip_files = {p.relative_to(src_root)
                   for p in _read_potfiles(src_root, skip_path)}
     not_translatable = set()
     for root, dirs, files in os.walk(src_root):
+        root = Path(root)
         for dirname in dirs:
-            dirpath = os.path.relpath(os.path.join(root, dirname), src_root)
-            if dirpath in skip_files or dirpath.startswith("."):
+            dirpath = (root / dirname).relative_to(src_root)
+            if dirpath in skip_files or dirname.startswith("."):
                 dirs.remove(dirname)
 
         for name in files:
-            path = os.path.relpath(os.path.join(root, name), src_root)
+            path = (root / name).relative_to(src_root)
             if path not in pot_files and path not in skip_files:
                 not_translatable.add(path)
 
@@ -346,6 +360,7 @@ def get_missing(po_dir):
 
     # Filter out any files not containing translations
     fd, temp_path = tempfile.mkstemp("POTFILES.in")
+    temp_path = Path(temp_path)
     try:
         os.close(fd)
         _write_potfiles(src_root, temp_path, not_translatable)
@@ -360,13 +375,13 @@ def get_missing(po_dir):
                     infos.update(line.split()[1:])
             return sorted(infos)
         finally:
-            os.unlink(pot_path)
+            pot_path.unlink()
     finally:
-        os.unlink(temp_path)
+        temp_path.unlink()
 
 
-def _get_xgettext_version():
-    """Returns a version tuple e.g. (0, 19, 3) or GettextError"""
+def _get_xgettext_version() -> Tuple:
+    """:returns: a version tuple e.g. (0, 19, 3) or GettextError"""
 
     try:
         result = subprocess.check_output(["xgettext", "--version"])
@@ -380,10 +395,10 @@ def _get_xgettext_version():
 
 
 @functools.lru_cache(None)
-def check_version():
-    """Raises GettextError in case the required gettext programs are missing
+def check_version() -> None:
+    """Check Gettext version.
+    :raises GettextError: (with message) if required gettext programs are missing
 
-    Tries to include a helpful error message..
     """
 
     required_programs = ["xgettext", "msgmerge", "msgfmt"]

setup.cfg
@@ -25,6 +25,8 @@ ignore_errors = True
 
 [tool:pytest]
 junit_suite_name = quodlibet
+# Pytest long traces are way too long in bulk
+addopts = --tb=short
 filterwarnings =
     ignore::DeprecationWarning
 markers =

commit 92605bfc61e83e62409547e6501c289e05afbcfb

Gettext: Correct / convert string paths to Path as now expected
* Broken in refactoring in #3737
* Add / correct type hints
* Add integration tests for these commands too

Fixes #3743

gdist/po.py
@@ -191,7 +191,7 @@ class build_po(Command):
     user_options = []
 
     def initialize_options(self):
-        self.build_base = None
+        self.build_base: Optional[str] = None
         self.po_build_dir: Optional[Path] = None
 
     def finalize_options(self):
@@ -236,9 +236,9 @@ class build_mo(Command):
     ]
 
     def initialize_options(self):
-        self.build_base: Optional[Path] = None
+        self.build_base: Optional[str] = None
         self.lang = None
-        self.po_build_dir: Optional[Path] = None
+        self.po_build_dir: Optional[str] = None
 
     def finalize_options(self):
         self.set_undefined_options('build', ('build_base', 'build_base'))
@@ -249,22 +249,23 @@ class build_mo(Command):
         self.run_command("build_po")
 
         po_package = self.distribution.po_package
+        po_build_path = self.po_build_dir and Path(self.po_build_dir)
 
-        langs = gettextutil.list_languages(self.po_build_dir)
+        langs = gettextutil.list_languages(po_build_path)
         if self.lang is not None:
             if self.lang not in langs:
                 raise SystemExit("Error: %r not found" % self.lang)
             else:
                 langs = [self.lang]
 
-        basepath = self.build_base / "share" / "locale"
+        basepath = Path(self.build_base) / "share" / "locale"
         for language in langs:
             fullpath = basepath / language / "LC_MESSAGES"
             destpath = fullpath / f"{po_package}.mo"
 
             self.mkpath(str(fullpath))
 
-            po_path = gettextutil.get_po_path(self.po_build_dir, language)
+            po_path = gettextutil.get_po_path(po_build_path, language)
             if newer(str(po_path), str(destpath)):
                 gettextutil.compile_po(po_path, destpath)
 

tests/test_setup.py
@@ -25,7 +25,8 @@ import pytest
 from _pytest.fixtures import fixture
 
 import quodlibet
-from gdist import create_po, GDistribution, create_pot, update_po, po_stats, build_po
+from gdist import (create_po, GDistribution, create_pot, update_po, po_stats, build_po,
+                   build_mo, install_mo)
 from quodlibet.util import get_module_dir
 from tests.test_po import has_gettext_util
 
@@ -47,7 +48,8 @@ def temp_po_dir() -> Path:
     with open(po_path / "POTFILES.in", "w") as f:
         f.write(f"{SRC_FILE.name}\n")
     shutil.copy(SRC_FILE, out_path / SRC_FILE.name)
-    return po_path
+    yield po_path
+    shutil.rmtree(po_path)
 
 
 @pytest.mark.skipif(not has_gettext_util(), reason="no gettext")
@@ -75,3 +77,22 @@ class TestPoCommands:
     def test_build_po(self, dist):
         cmd = build_po(dist)
         cmd.run()
+
+    def test_build_mo(self, dist, temp_po_dir):
+        cmd = build_mo(dist)
+        # Probably should create another dir, but this will do for now
+        cmd.build_base = str(temp_po_dir)
+        cmd.po_build_dir = str(temp_po_dir)
+        cmd.run()
+
+    def test_install_mo(self, dist, temp_po_dir):
+        cmd = install_mo(dist)
+        cmd.build_base = str(temp_po_dir)
+        out = mkdtemp()
+        # These need to exist for the command to succeed
+        (Path(out) / "share" / "locale").mkdir(parents=True)
+        (temp_po_dir / "share" / "locale").mkdir(parents=True)
+
+        cmd.install_dir = out
+        cmd.run()
+        shutil.rmtree(out)