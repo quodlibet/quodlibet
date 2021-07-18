c1f7f297c7e04924af8de9aa5070c4bab8210ca1
Christoph Reiter <reiter.christoph@gmail.com>
2021-07-18 17:09:38 +0200

    macos: a round of jhbuild package updates
    still lots of things missing
    
    We now depend on rust being installed globally

--- dev-utils/osx_bundle/build.sh
+++ dev-utils/osx_bundle/build.sh
@@ -4,6 +4,8 @@ set -e
 
 source env.sh
 
+cargo -V || (echo "Requires rust"; exit 1)
+
 # install ninja
 BIN=$(jhbuild run bash -c 'echo $PREFIX/bin')
 curl -L -o ninja.zip "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip"
