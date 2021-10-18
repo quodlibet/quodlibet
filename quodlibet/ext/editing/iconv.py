d59b604ea33b878ce55cb05b1d7ed33d5f14681d
Sergey <schernikov@gmail.com>
2021-10-18 11:55:32 -0700

    Update quodlibet/ext/editing/iconv.py
    Co-authored-by: Nick Boultbee <declension@users.noreply.github.com>

--- quodlibet/ext/editing/iconv.py
+++ quodlibet/ext/editing/iconv.py
@@ -70,4 +70,5 @@ class Iconv(EditTagsPlugin):
         try:
             return [(tag, self.__value)]
         except AttributeError:
-            return []  # make it different from [(tag, value)] so the menu entry is enabled
+            # make it different from [(tag, value)] so the menu entry is enabled
+            return []
