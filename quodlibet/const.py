7e82d050c765a1d6f3ee84009461aa07acd64b58
Christoph Reiter <reiter.christoph@gmail.com>
2022-03-29 19:03:09 +0200

    release prep

--- quodlibet/const.py
+++ quodlibet/const.py
@@ -63,7 +63,7 @@ class MinVersions:
     GSTREAMER = Version("GStreamer", 1, 8)
 
 
-VERSION_TUPLE = Version("", 4, 5, -1)
+VERSION_TUPLE = Version("", 4, 5, 0)
 VERSION = str(VERSION_TUPLE)
 
 # entry point for the user guide / wiki
