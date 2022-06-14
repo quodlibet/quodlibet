988fca71664d38806531766d01290a0338068d4d
Nick B <nick@declension.net>
2022-06-14 21:43:55 +0100

    Fix some Bash warnings in Win installer
    Thanks to shellcheck really...

--- dev-utils/win_installer/rebuild.sh
+++ dev-utils/win_installer/rebuild.sh
@@ -18,7 +18,7 @@ function main {
     [[ -d "${BUILD_ROOT}" ]] && (echo "${BUILD_ROOT} already exists"; exit 1)
 
     # started from the wrong env -> switch
-    if [ $(echo "$MSYSTEM" | tr '[A-Z]' '[a-z]') != "$MINGW" ]; then
+    if [ "$(echo "$MSYSTEM" | tr 'A-Z' 'a-z')" != "$MINGW" ]; then
         "/${MINGW}.exe" "$0"
         exit $?
     fi
