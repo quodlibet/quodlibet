d0cb65def5babf8cf95751646bc43c230db036f1
Nick B <nick@declension.net>
2022-07-23 10:28:14 +0100

    Operon Linux tests are still very flaky...

--- tests/test_operon.py
+++ tests/test_operon.py
@@ -357,7 +357,7 @@ class TOperonEdit(TOperonBase):
         os.environ["VISUAL"] = "false"
         self.check_false(["edit", self.f], False, True)
 
-    @pytest.mark.flaky(max_runs=4, min_passes=2)
+    @pytest.mark.flaky(max_runs=4, min_passes=1)
     def test_dry_run(self):
         if os.name == "nt" or sys.platform == "darwin":
             return
@@ -378,7 +378,7 @@ class TOperonEdit(TOperonBase):
         self.assertEqual(sorted(old_items), sorted(realitems(self.s)))
 
     @skipIf(is_windows() or is_osx(), "Linux only, uses truncate")
-    @pytest.mark.flaky(max_runs=4, min_passes=2)
+    @pytest.mark.flaky(max_runs=4, min_passes=1)
     def test_remove_all(self):
 
         os.environ["VISUAL"] = "truncate -s 0"
