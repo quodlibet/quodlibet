4af0c5553d5483486d068efb294b0ee90d03a4cc
Nick B <nick@declension.net>
2022-08-03 22:33:13 +0100

    Internet Radio: update buttons
     * Move to bottom, consistent with all other browsers now
     * Move to a flowbox, to allow smaller scaling (like Playlist)

--- quodlibet/browsers/iradio.py
+++ quodlibet/browsers/iradio.py
@@ -1,5 +1,5 @@
 # Copyright 2011 Joe Wreschnig, Christoph Reiter
-#      2013-2020 Nick Boultbee
+#      2013-2022 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -46,9 +46,7 @@ from quodlibet.util import print_w
 from quodlibet.qltk.views import AllTreeView
 from quodlibet.qltk.searchbar import SearchBarBox
 from quodlibet.qltk.completion import LibraryTagCompletion
-from quodlibet.qltk.x import MenuItem, Align, ScrolledWindow
-from quodlibet.qltk.x import SymbolicIconImage
-from quodlibet.qltk.menubutton import MenuButton
+from quodlibet.qltk.x import MenuItem, Align, ScrolledWindow, Button
 
 STATION_LIST_URL = \
     "https://quodlibet.github.io/radio/radiolist.bz2"
@@ -569,20 +567,6 @@ class InternetRadio(Browser, util.InstanceTracker):
                                                  accel_group=self.accelerators)
         search.connect('query-changed', self.__filter_changed)
 
-        menu = Gtk.Menu()
-        new_item = MenuItem(_(u"_New Station…"), Icons.LIST_ADD)
-        new_item.connect('activate', self.__add)
-        menu.append(new_item)
-        update_item = MenuItem(_("_Update Stations"), Icons.VIEW_REFRESH)
-        update_item.connect('activate', self.__update)
-        menu.append(update_item)
-        menu.show_all()
-
-        button = MenuButton(
-            SymbolicIconImage(Icons.EMBLEM_SYSTEM, Gtk.IconSize.MENU),
-            arrow=True)
-        button.set_menu(menu)
-
         def focus(widget, *args):
             qltk.get_top_parent(widget).songlist.grab_focus()
         search.connect('focus-out', focus)
@@ -599,12 +583,10 @@ class InternetRadio(Browser, util.InstanceTracker):
         scrolled_window.add(view)
         model = Gtk.ListStore(int, str, str, str)
 
-        model.append(row=[self.TYPE_ALL, Icons.FOLDER, "__all",
-                          _("All Stations")])
+        model.append(row=[self.TYPE_ALL, Icons.FOLDER, "__all", _("All Stations")])
         model.append(row=[self.TYPE_SEP, Icons.FOLDER, "", ""])
         # Translators: Favorite radio stations
-        model.append(row=[self.TYPE_FAV, Icons.FOLDER, "__fav",
-                          _("Favorites")])
+        model.append(row=[self.TYPE_FAV, Icons.FOLDER, "__fav", _("Favorites")])
         model.append(row=[self.TYPE_SEP, Icons.FOLDER, "", ""])
 
         filters = self.filters
@@ -647,7 +629,6 @@ class InternetRadio(Browser, util.InstanceTracker):
 
         box = Gtk.HBox(spacing=6)
         box.pack_start(search, True, True, 0)
-        box.pack_start(button, False, True, 0)
         self._searchbox = Align(box, left=0, right=6, top=0)
         self._searchbox.show_all()
 
@@ -661,15 +642,27 @@ class InternetRadio(Browser, util.InstanceTracker):
             self.qbar.show()
 
         pane = qltk.ConfigRHPaned("browsers", "internetradio_pos", 0.4)
-        pane.show()
-        pane.pack1(scrolled_window, resize=False, shrink=False)
+        vb = Gtk.VBox(spacing=0)
+        vb.pack_start(scrolled_window, True, True, 0)
+        fb = Gtk.FlowBox()
+        fb.set_column_spacing(3)
+        fb.set_homogeneous(True)
+        new_station = Button(_(u"_Add Station…"), Icons.LIST_ADD)
+        new_station.connect('activate', self.__add)
+        update_station = Button(_("_Update Stations"), Icons.VIEW_REFRESH)
+        update_station.connect('activate', self.__update)
+        fb.insert(new_station, 1)
+        fb.insert(update_station, 2)
+        vb.pack_end(Align(fb, left=3), False, False, 3)
+        pane.pack1(vb, resize=False, shrink=False)
+        pane.show_all()
+
         songbox = Gtk.VBox(spacing=6)
         songbox.pack_start(self._searchbox, False, True, 0)
         self._songpane_container = Gtk.VBox()
-        self._songpane_container.show()
         songbox.pack_start(self._songpane_container, True, True, 0)
         songbox.pack_start(self.qbar, False, True, 0)
-        songbox.show()
+        songbox.show_all()
         pane.pack2(songbox, resize=True, shrink=False)
         self.pack_start(pane, True, True, 0)
         self.show()
