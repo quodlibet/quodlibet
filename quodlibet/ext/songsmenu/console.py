3ec403847f1c9ec0511999e542bff316b29464c8
Adam Fontenot <adam.m.fontenot@gmail.com>
2022-09-27 00:20:14 -0700

    Fix crash in console when autocompleting
    Some objects (like custom classes) don't have a defined __repr__.
    When an object like this is the default argument of a function,
    we currently receive an uncaught TypeError exception.
    
    Instead, use `repr` to get the string representation of the object.
    When __repr__ isn't defined, it falls back to returning a
    reasonable representaion of most obects.

--- quodlibet/ext/songsmenu/console.py
+++ quodlibet/ext/songsmenu/console.py
@@ -523,9 +523,8 @@ class PythonConsole(Gtk.ScrolledWindow):
                             if i < noargs:
                                 sargs.append(spec.args[i])
                             else:
-                                sargs.append(spec.args[i] + '=' +
-                                             spec.defaults[i -
-                                                           noargs].__repr__())
+                                default_arg = repr(spec.defaults[i - noargs])
+                                sargs.append(spec.args[i] + '=' + default_arg)
 
                         details = " ({})".format(", ".join(sargs))
                         comp.append((name, details))
