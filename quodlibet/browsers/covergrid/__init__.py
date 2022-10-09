bf0fd817a846e6de06e276dfc502c477885be765
Nick Boultbee <declension@users.noreply.github.com>
2022-10-09 10:47:14 +0100

    Covergrid init cleanup (#4163)

--- quodlibet/browsers/covergrid/__init__.py
+++ quodlibet/browsers/covergrid/__init__.py
@@ -1,14 +1,13 @@
 # Copyright 2013 Christoph Reiter
+#           2022 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-from __future__ import absolute_import
 
-from .main import CoverGrid, AlbumTagCompletion
+from .main import CoverGrid
 
 
-AlbumTagCompletion
 browsers = [CoverGrid]
