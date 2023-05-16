ae4dd1f66d48945b61f2f3bcbe41140e6e78d19b
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-16 20:09:23 +0200

    osx: bootstrap ninja as well

--- dev-utils/osx_bundle/build.sh
+++ dev-utils/osx_bundle/build.sh
@@ -7,12 +7,5 @@ source env.sh
 
 cargo -V || (echo "Requires rust"; exit 1)
 
-# install ninja
-# shellcheck disable=SC2016
-BIN=$(jhbuild run bash -c 'echo $PREFIX/bin')
-curl -L -o ninja.zip "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip"
-unzip -o ninja.zip -d "$BIN"
-rm ninja.zip
-
 jhbuild build meta-bootstrap
 jhbuild build quodlibet
