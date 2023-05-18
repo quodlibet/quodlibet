80a9eb2e44ecb2064b93db0565dd398c6dd9070c
Nick B <nick@declension.net>
2023-05-18 18:00:54 +0100

    Flatpak: remove host things, reinstall certs

--- .ci/flatpak-install.sh
+++ .ci/flatpak-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt-get update -qq
-sudo apt-get install -qq -y flatpak xvfb libsoup-3.0-0
+sudo apt-get install -qq -y flatpak xvfb
 sudo apt-get install --reinstall ca-certificates
 
 flatpak remote-add --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
