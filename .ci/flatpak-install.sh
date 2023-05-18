44a2e2f4f7d94f9755d1f217bb583e4e4394e1c5
Nick B <nick@declension.net>
2023-05-18 17:52:06 +0100

    Flatpak: Reinstall ca-certificates

--- .ci/flatpak-install.sh
+++ .ci/flatpak-install.sh
@@ -3,7 +3,8 @@
 set -e
 
 sudo apt-get update -qq
-sudo apt-get install -qq -y ca-certificates flatpak xvfb libsoup-3.0-0
+sudo apt-get install -qq -y flatpak xvfb libsoup-3.0-0
+sudo apt-get install --reinstall ca-certificates
 
 flatpak remote-add --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub io.github.quodlibet.QuodLibet
