bfff79ce96b7730923d27c9b6ebfbfdb1c75b380
Nick B <nick@declension.net>
2023-08-21 21:27:45 +0100

    version bump

--- quodlibet/const.py
+++ quodlibet/const.py
@@ -63,7 +63,7 @@ class MinVersions:
     GSTREAMER = Version("GStreamer", 1, 8)
 
 
-VERSION_TUPLE = Version("", 4, 6, -1)
+VERSION_TUPLE = Version("", 4, 7, -1)
 VERSION = str(VERSION_TUPLE)
 
 QL_NAMESPACE = "https://quodlibet.github.io"
