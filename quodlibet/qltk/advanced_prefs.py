85002a56ed6293792deb20cd92f1cb130b94f811
Jej <jej@users.noreply.github.com>
2023-10-24 09:03:04 +0200

    Convert checkboxes to switches

--- quodlibet/qltk/advanced_prefs.py
+++ quodlibet/qltk/advanced_prefs.py
@@ -55,21 +55,24 @@ def boolean_config(section, option, label, tooltip):
         config.reset(section, option)
         button.set_active(config.getboolean(section, option))
 
-    def __toggled(self, section, option):
-        config.set(section, option, str(bool(self.get_active())).lower())
+    def __toggled(switch, state, section, option):
+        config.set(section, option, str(bool(switch.get_active())).lower())
 
     default = config.getboolean(section, option)
-    button = Gtk.CheckButton()
+    button = Gtk.Switch()
     button.set_active(config.getboolean(section, option, default))
     button.set_tooltip_text(tooltip)
-    button.connect("toggled", __toggled, section, option)
+    button.connect("notify::active", __toggled, section, option)
+    button_box = Gtk.VBox(homogeneous=True)
+    button_box.pack_start(button, False, False, 0)
+
     revert = Gtk.Button()
     revert.add(Gtk.Image.new_from_icon_name(Icons.DOCUMENT_REVERT, Gtk.IconSize.BUTTON))
     revert.connect("clicked", on_reverted)
 
     lbl = Gtk.Label(label=label, use_underline=True)
     lbl.set_mnemonic_widget(button)
-    return lbl, button, revert
+    return lbl, button_box, revert
 
 
 def int_config(section, option, label, tooltip):
@@ -236,14 +239,13 @@ class AdvancedPreferencesPane():
         for (row, (label, widget, button)) in enumerate(rows):
             label.set_alignment(1.0, 0.5)
             table.attach(label, 0, 1, row, row + 1, xoptions=Gtk.AttachOptions.FILL)
-            if isinstance(widget, Gtk.CheckButton):
-                xoptions = Gtk.AttachOptions.FILL
-                widget.set_alignment(0.0, 0.5)
-                table.attach(widget, 1, 2, row, row + 1, xoptions=xoptions)
+            if isinstance(widget, Gtk.VBox):
                 # This stops checkbox from expanding too big, or shrinking text entries
                 blank = Gtk.Label()
-                table.attach(blank, 2, 3, row, row + 1,
+                table.attach(blank, 1, 2, row, row + 1,
                              xoptions=Gtk.AttachOptions.EXPAND)
+                table.attach(widget, 2, 3, row, row + 1,
+                             xoptions=Gtk.AttachOptions.SHRINK)
             else:
                 xoptions = Gtk.AttachOptions.FILL
                 table.attach(widget, 1, 3, row, row + 1, xoptions=xoptions)
