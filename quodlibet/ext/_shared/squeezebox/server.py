e60d0e1d9fadfe2fdba8d7da455c3c89568d4197
Nick Boultbee <declension@users.noreply.github.com>
2024-02-04 16:47:49 +0000

    Squeezebox plugins: fail nicely if telnetlib is missing (#4480)
    TODO: port code (or delete plugins)
    
    Co-authored-by: Nick Boultbee <boultbee@arrival.com>

--- quodlibet/ext/_shared/squeezebox/server.py
+++ quodlibet/ext/_shared/squeezebox/server.py
@@ -1,11 +1,17 @@
-# Copyright 2014-2023 Nick Boultbee
+# Copyright 2014-2024 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
+from quodlibet.plugins import MissingModulePluginError
 
-from telnetlib import Telnet
+try:
+    # TODO: port Telnet code to raw socket
+    from telnetlib import Telnet
+except ImportError as e:
+    raise MissingModulePluginError("telnetlib", "Not supported in Python 3.13+") from e
+
 import time
 from urllib.parse import quote, unquote
 
