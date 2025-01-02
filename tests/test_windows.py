407874988e321df7e381ec6367e762b676a164ac
Nick B <nick@declension.net>
2025-01-02 21:42:22 +0000

    Manual

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -1,4 +1,5 @@
 # Copyright 2013 Christoph Reiter
+#           2025 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -6,6 +7,8 @@
 # (at your option) any later version.
 
 import os
+
+import pytest
 
 from tests import TestCase, get_data_path, skipUnless
 
@@ -39,19 +42,18 @@ class TWindows(TestCase):
         path = get_data_path("test.lnk")
         d = windows.get_link_target(path)
         assert isinstance(d, str)
-        self.assertEqual(
-            normalize_path(d), normalize_path("C:\\Windows\\explorer.exe"))
+        assert normalize_path(d) == normalize_path("C:\\Windows\\explorer.exe")
 
     def test_get_link_target_unicode(self):
         path = get_data_path("test2.lnk")
         d = windows.get_link_target(path)
         assert isinstance(d, str)
         if is_wine():
-            # wine doesn't support unicode paths here..
-            self.assertEqual(os.path.basename(d), "\xe1??.txt")
+            # wine doesn't support unicode paths here...
+            assert os.path.basename(d) == "\xe1??.txt"
         else:
-            self.assertEqual(os.path.basename(d), "\xe1\U00016826.txt")
+            assert os.path.basename(d) == "\xe1\U00016826.txt"
 
     def test_get_link_target_non_exist(self):
-        with self.assertRaises(WindowsError):
+        with pytest.raises(WindowsError):
             windows.get_link_target("nopenope.lnk")
