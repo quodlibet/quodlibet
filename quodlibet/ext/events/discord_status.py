eb0723dc35472cf7586cda27b48c792d2ebf267c
W. Connor Yates <self@wcyates.xyz>
2025-11-03 23:51:01 -0500

    Rework Discord Rich Presence plugin
    Add music progress slider, use ActivityType.LISTENING, change pause
    behavior and display

--- quodlibet/ext/events/discord_status.py
+++ quodlibet/ext/events/discord_status.py
@@ -7,23 +7,30 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-from quodlibet import _, app, print_e
+from time import time
+
+from quodlibet import _, app
 from quodlibet.plugins import PluginConfig, ConfProp
 from quodlibet.plugins.events import EventPlugin
 from quodlibet.pattern import Pattern
+from quodlibet.formats import AudioFile
 
 from gi.repository import Gtk
 
 try:
-    from pypresence import Presence, InvalidID, DiscordNotFound
-except ImportError as e:
-    from quodlibet.plugins import MissingModulePluginError
-
-    raise MissingModulePluginError("pypresence") from e
+    from pypresence import (
+            Presence, InvalidID, DiscordNotFound,
+            ActivityType, StatusDisplayType
+        )
+except ImportError:
+    from quodlibet.plugins import MissingModulePluginException
+    raise MissingModulePluginException("pypresence")
 
 # The below resources are from/uploaded-to the Discord Application portal.
-QL_DISCORD_RP_ID = "974521025356242984"
-QL_LARGE_IMAGE = "io-github-quodlibet-quodlibet"
+DISCORD_APP_ID = '974521025356242984'
+QL_LOGO_IMAGE_URL = "io-github-quodlibet-quodlibet"
+
+QL_HOMEPAGE_LINK = "https://github.com/quodlibet/quodlibet"
 
 VERSION = "1.0"
 
@@ -45,35 +52,71 @@ discord_status_config = DiscordStatusConfig()
 class DiscordStatusMessage(EventPlugin):
     PLUGIN_ID = _("Discord status message")
     PLUGIN_NAME = _("Discord Status Message")
-    PLUGIN_DESC = _(
-        "Change your Discord status message according to what "
-        "you're currently listening to."
-    )
+    PLUGIN_DESC = _("Change your Discord status message according to what "
+                    "you're currently listening to.")
     VERSION = VERSION
 
     def __init__(self):
-        self.song = None
-        self.discordrp = None
+        self.song: AudioFile = None
+        self.details: str = None
+        self.state: str = None
+        try:
+            self.discordrp = Presence(DISCORD_APP_ID, pipe=0)
+            self.discordrp.connect()
+        except (DiscordNotFound, ConnectionRefusedError):
+            self.discordrp = None
 
-    def update_discordrp(self, details, state=None):
+    def update_discordrp(self,
+                         time_start: int | None = None,
+                         time_end: int | None = None) -> None:
+        """
+        Update the Discord Rich Presence via the open Discord instance, if
+        possible.
+
+        :param time_start: Optional timestamp in seconds from Unix epoch
+            representing the time the activity would have started from.
+            In this plugin, it represents the position/progress of the song
+            subtracted from the system time at the time that the song is
+            unpaused.
+            (i.e. the timestamp for when the song would have started from)
+        :type time_start: int or None
+        :param time_end: Optional timestamp in seconds from Unix epoch
+            representing the time the activity would end by.
+            In this plugin, it represents the remaining time of the song added
+            to the system time at the time that the song is unpaused.
+            (i.e. the timestamp for when the song would be expected to end)
+        :type time_end: int or None
+        :rtype: None
+        """
+        # The connection can be lost/closed if Discord is closed while the
+        # plugin is active.
+        # The connection can fail to be established when Discord is not open.
         if not self.discordrp:
             try:
-                self.discordrp = Presence(QL_DISCORD_RP_ID, pipe=0)
+                self.discordrp = Presence(DISCORD_APP_ID, pipe=0)
                 self.discordrp.connect()
-            except (DiscordNotFound, ConnectionRefusedError) as e:
-                print_e(f"Got error connecting to Discord ({e!r})")
+            except (DiscordNotFound, ConnectionRefusedError):
                 self.discordrp = None
+                return
 
-        if self.discordrp:
-            try:
-                self.discordrp.update(
-                    details=details, state=state, large_image=QL_LARGE_IMAGE
-                )
-            except InvalidID:
-                # XXX Discord was closed?
-                self.discordrp = None
+        try:
+            self.discordrp.update(
+                name=app.name,
+                details=self.details,
+                state=self.state,
+                start=time_start,
+                end=time_end,
+                status_display_type=StatusDisplayType.DETAILS,
+                activity_type=ActivityType.LISTENING,
+                large_image=QL_LOGO_IMAGE_URL,
+                #large_url=QL_HOMEPAGE_LINK,
+                large_text=app.name,
+            )
+        except InvalidID:
+            # XXX Discord was closed?
+            self.discordrp = None
 
-    def handle_play(self):
+    def update_details(self):
         if self.song:
             details = Pattern(discord_status_config.rp_line1) % self.song
             state = Pattern(discord_status_config.rp_line2) % self.song
@@ -81,24 +124,60 @@ class DiscordStatusMessage(EventPlugin):
             # The details and state fields must be atleast 2 characters.
             if len(details) < 2:
                 details = None
+            elif app.player.paused:
+                details = details + " " + _("(Paused)")
 
             if len(state) < 2:
                 state = None
 
-            self.update_discordrp(details, state)
+            self.state = state
+            self.details = details
 
     def handle_paused(self):
-        self.update_discordrp(details=_("Paused"))
+        if self.discordrp:
+            if self.song is not None:
+                self.update_details()
+                self.update_discordrp()
+            else:
+                self.discordrp.clear()
 
     def handle_unpaused(self):
         if not self.song:
             self.song = app.player.song
-        self.handle_play()
+        position = app.player.get_position() / 1000
+        ts_now = int(time()) - position
+        ts_before = ts_now
+        ts_left = ts_now + self.song["~#length"]
+        self.update_details()
+        self.update_discordrp(time_start=ts_before, time_end=ts_left)
 
-    def plugin_on_song_started(self, song):
-        self.song = song
+    def plugin_on_seek(self, song: AudioFile, msec: int):
         if not app.player.paused:
-            self.handle_play()
+            position = app.player.get_position() / 1000
+            ts_now = round(time()) - position
+            ts_before = ts_now
+            ts_left = ts_now + self.song["~#length"]
+            self.update_discordrp(time_start=ts_before, time_end=ts_left)
+
+    def plugin_on_song_started(self, song: AudioFile):
+        self.song = song
+        if song is not None:
+            if not app.player.paused:
+                # a new song is being played
+                ts_now = round(time())
+                ts_before = ts_now
+                ts_left = ts_now + self.song["~#length"]
+
+                self.update_details()
+                self.update_discordrp(ts_before, ts_left)
+            else:
+                # this branch can execute when the app just launched
+                # (i.e. song from previous session is loaded, but paused)
+                self.handle_paused()
+        else:
+            # this branch can execute when the last song of the queue ends
+            # (i.e. song is removed from player w/o a substitute)
+            self.handle_paused()
 
     def plugin_on_paused(self):
         self.handle_paused()
@@ -125,23 +204,22 @@ class DiscordStatusMessage(EventPlugin):
         def rp_line1_changed(entry):
             discord_status_config.rp_line1 = entry.get_text()
             if not app.player.paused:
-                self.handle_play()
+                self.plugin_on_unpaused()
 
         def rp_line2_changed(entry):
             discord_status_config.rp_line2 = entry.get_text()
             if not app.player.paused:
-                self.handle_play()
+                self.plugin_on_unpaused()
 
         status_line1_box = Gtk.HBox(spacing=6)
         status_line1_box.set_border_width(3)
 
         status_line1 = Gtk.Entry()
         status_line1.set_text(discord_status_config.rp_line1)
-        status_line1.connect("changed", rp_line1_changed)
+        status_line1.connect('changed', rp_line1_changed)
 
-        status_line1_box.pack_start(
-            Gtk.Label(label=_("Status Line #1")), False, True, 0
-        )
+        status_line1_box.pack_start(Gtk.Label(label=_("Status Line #1")),
+                                    False, True, 0)
         status_line1_box.pack_start(status_line1, True, True, 0)
 
         status_line2_box = Gtk.HBox(spacing=3)
@@ -149,11 +227,10 @@ class DiscordStatusMessage(EventPlugin):
 
         status_line2 = Gtk.Entry()
         status_line2.set_text(discord_status_config.rp_line2)
-        status_line2.connect("changed", rp_line2_changed)
+        status_line2.connect('changed', rp_line2_changed)
 
-        status_line2_box.pack_start(
-            Gtk.Label(label=_("Status Line #2")), False, True, 0
-        )
+        status_line2_box.pack_start(Gtk.Label(label=_('Status Line #2')),
+                                    False, True, 0)
         status_line2_box.pack_start(status_line2, True, True, 0)
 
         vb.pack_start(status_line1_box, True, True, 0)
